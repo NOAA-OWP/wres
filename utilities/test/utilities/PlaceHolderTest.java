df4c8172baf113b8ca088f06933fc6d4f462fcce
Jesse Bickel <jesse.bickel@noreply.gov>
2017-02-09 10:43:19 -0600

    Build runs a bare-bones test.
    Have to specify specific test dirs since they vary across subprojects.

--- utilities/test/utilities/PlaceHolderTest.java
+++ utilities/test/utilities/PlaceHolderTest.java
@@ -1,6 +1,13 @@
 package utilities;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class PlaceHolderTest
 {
-
+	@Test
+	public void sampleTest()
+	{
+		assertEquals(true,true);
+	}		
 }
