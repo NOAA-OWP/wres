59740d6e679f78f704b4db0b7f0ce805015cb011
James Brown <james.d.brown@noreply.gov>
2017-02-15 21:11:00 +0000

    Returned the (rebuilt) EVS.jar (no logging tools) and the EVSIOTest.java. Updated the .gitignore to allow *.jar Updated the build.gradle to add the EVS.jar dependency

---
+++ utilities/test/owp/util/EVSIOTest.java
@@ -0,0 +1,284 @@
+package owp.util;
+
+//EVS dependencies
+import evs.data.*;
+import evs.io.*;
+import evs.io.netcdf.*;
+import evs.io.nws.*;
+import evs.io.xml.pixml.*;
+import evs.utilities.*;
+import evs.utilities.matrix.*;
+
+//Java dependencies
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+//JUnit dependencies
+import org.junit.*;
+
+/**
+ * Test class for EVS IO.
+ * 
+ * @author evs@hydrosolved.com
+ */
+
+public class EVSIOTest {
+    
+    /**
+     * Reads a test dataset of observations in PI-XML format and prints them to
+     * standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readPIObservations() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/CBNK1_MAP.xml";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("CBNK1", "MAP");
+        v.add(getMe);
+        //Construct the reader
+        PublishedInterfaceIO reader = new PublishedInterfaceIO(true);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), false, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();  
+    }
+    
+    /**
+     * Reads a test dataset of ensemble forecasts in PI-XML format and prints 
+     * them to standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readPIForecasts() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/1988010112_WALN6DEL_hefs_export.xml";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("WALN6TOT", "SQIN");
+        v.add(getMe);
+        //Construct the reader
+        PublishedInterfaceIO reader = new PublishedInterfaceIO(true);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), true, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }    
+    
+    /**
+     * Reads a test dataset of observations in ASCII format and prints them to
+     * standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readASCIIObservations() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/01608500_TEST.OBS";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("01608500", "MAP");
+        v.add(getMe);
+        //Construct the reader
+        ASCIIFileIO reader = new ASCIIFileIO("yyyyMMddHH", " ");
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), false, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }
+    
+    /**
+     * Reads a test dataset of ensemble forecasts in ASCII format and prints 
+     * them to standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readASCIIForecasts() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/01608500_TEST.FCST";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("01608500", "MAP");
+        v.add(getMe);
+        //Construct the reader
+        ASCIIFileIO reader = new ASCIIFileIO("yyyyMMddHH", " ");
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), true, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }    
+    
+    /**
+     * Reads a test dataset of observations in NetCDF format and prints them to
+     * standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readNetCDFObservations() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/2011010312_observed_discharge_timeseries.nc";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("JNSV2", "QINE");
+        v.add(getMe);
+        //Construct the reader
+        NetCDFFileIO reader = new NetCDFFileIO(new ObservedNetCDFReader(
+                new NetCDFObservedHandler()),
+                false);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), false, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, (DoubleMatrix2D)data.
+                getSubmatrixByRow(0, 10),"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }
+    
+    /**
+     * Reads a test dataset of ensemble forecasts in NetCDF format and prints 
+     * them to standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readNetCDFForecasts() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/1990110112_AEnKF_discharge_timeseries.nc";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("JNSV2", "QINE");
+        v.add(getMe);
+        //Construct the reader
+        NetCDFFileIO reader = new NetCDFFileIO(new EnsembleNetCDFReader(
+                new NetCDFEnsembleHandler()),
+                false);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), true, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, (DoubleMatrix2D)data.
+                getSubmatrixByRow(0, 10),"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }    
+    
+    /**
+     * Reads a test dataset of observations in NWS Datacard format and prints 
+     * them to standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readDatacardObservations() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/QUAO2.MAT.OBS";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("QUAO2", "MAT");
+        v.add(getMe);
+        //Construct the reader
+        OHDFileIO reader = new OHDFileIO(InputDataIO.NWSCARD);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), false, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }
+    
+    /**
+     * Reads a test dataset of ensemble forecasts in NWS Datacard format and
+     * prints them to standard out.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void readDatacardForecasts() throws IOException {
+        //Locate the test data
+        String resource = "testdata/evsiotests/20030701QUAO2.MAT";
+        File f = new File(resource); 
+        if(!f.exists()) {
+        	throw new IOException("Could not locate test data '"+f+"'.");
+        }
+        //Identify the precise data to read
+        ArrayList<VUIdentifier> v = new ArrayList<>();
+        VUIdentifier getMe = new VUIdentifier("QUAO2", "MAT");
+        v.add(getMe);
+        //Construct the reader
+        OHDFileIO reader = new OHDFileIO(InputDataIO.NWSCARD);
+        //Read the data into the store
+        reader.read(new File[]{f}, v,
+                TimeZone.getTimeZone("UTC"), true, null, -999,new IOState());
+        //Obtain the data from the store
+        DoubleMatrix2D data = reader.getData(getMe).get(0);
+        //Print some of the data to standard out
+        StringUtilities.printWithDates(0, data,"yyyyMMddHH");
+        //Get the double[][]
+        //double[][] d = data.toArray();
+    }     
+    
+    
+    
+}
