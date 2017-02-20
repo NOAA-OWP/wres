e86dadb2eb949aec2498044406077ef94023d1a4
James Brown <james.d.brown@noreply.gov>
2017-02-20 20:29:01 +0000

    Adding unversioned superclass, Identifier.java.

---
+++ wres-core/src/wres/configcontrol/config/Identifier.java
@@ -0,0 +1,19 @@
+package wres.configcontrol.config;
+
+/**
+ * Interface for an immutable identifier.
+ * 
+ * @author james.brown@hydrosolved.com
+ */
+public interface Identifier
+{
+
+    /**
+     * Returns the string identifier.
+     * 
+     * @return the identifier
+     */
+
+    String getID();
+
+}
