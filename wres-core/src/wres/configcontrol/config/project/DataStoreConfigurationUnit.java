6cc0800998b42d984fe2ef192383495d0f46c1fb
James Brown <james.d.brown@noreply.gov>
2017-02-27 20:50:24 +0000

    Eliminated several references to PairStore, as the verification pairs are unlikely to be stored separately from the verification inputs, but constructed on-the-fly or dynamically. However, several aspects of the pairing will need to be configured, so PairingConfigurationUnit.java remains.

--- wres-core/src/wres/configcontrol/config/project/DataStoreConfigurationUnit.java
+++ wres-core/src/wres/configcontrol/config/project/DataStoreConfigurationUnit.java
@@ -6,11 +6,10 @@ import wres.configcontrol.config.SimpleIdentifier;
 /**
  * An elementary block of configuration for a data store, including:
  * <ul>
- * <li>A {@link wres.configcontrol.datamodel.spacetimeobject.SpaceTimeObjectStore}, which stores the elementary
- * space-time datasets for verification;</li>
- * <li>A {@link wres.configcontrol.datamodel.spacetimeobject.PairStore}, which stores paired datasets; and</li>
+ * <li>A {@link wres.configcontrol.datamodel.spacetimeobject.SpaceTimeObjectStore}, which stores the input datasets for
+ * verification; and</li>
  * <li>A {@link wres.configcontrol.datamodel.spacetimeobject.VerificationResultStore}, which stores verification
- * results.</li>
+ * outputs.</li>
  * </ul>
  * 
  * @author james.brown@hydrosolved.com
