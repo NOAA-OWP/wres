470ddb6cb66dda4e491162f8e7b41b3a7e7c5822
Chris Tubbs <christopher.tubbs@noreply.gov>
2017-03-10 09:07:11 -0600

    Added Definition for the UnitConversion table.

---
+++ wres-core/SQL/UnitConversion.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE UnitConversion
+(
+	from_unit SMALLINT,
+	to_unit SMALLINT,
+	factor SMALLINT,
+	CONSTRAINT from_measurementunit_fk FOREIGN KEY (from_unit)
+		REFERENCES MeasurementUnit(measurementunit_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT to_measurementunit_fk FOREIGN KEY (to_unit)
+		REFERENCES MeasurementUnit(measurementunit_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE INDEX unitconversion_measurementunit_idx
+	ON UnitConversion
+	USING btree
+	(from_unit, to_unit);
+
+INSERT INTO UnitConversion(from_unit, to_unit, factor)
+VALUES (1, 1, 1),
+	(1, 2, 1),
+	(2, 1, 1),
+	(2, 2, 1);
