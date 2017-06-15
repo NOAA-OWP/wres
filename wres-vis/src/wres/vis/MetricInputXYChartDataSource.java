c38a51a51e7561a1e91375575a62b3bbbbda4f33
Hank.Herr <Hank.Herr@noreply.gov>
2017-06-15 15:32:55 -0400

    Placing initial code in wres-vis
    Included libraries in libs which will be phased out as I use Maven
    central

---
+++ wres-vis/src/wres/vis/MetricInputXYChartDataSource.java
@@ -0,0 +1,110 @@
+package wres.vis;
+
+import org.jfree.data.xy.XYDataset;
+
+import ohd.hseb.charter.ChartConstants;
+import ohd.hseb.charter.datasource.DefaultXYChartDataSource;
+import ohd.hseb.charter.datasource.XYChartDataSource;
+import ohd.hseb.charter.datasource.XYChartDataSourceException;
+import ohd.hseb.charter.parameters.DataSourceDrawingParameters;
+import ohd.hseb.charter.parameters.SeriesDrawingParameters;
+import wres.datamodel.metric.MetricInput;
+import wres.engine.statistics.metric.inputs.SingleValuedPairs;
+
+/**
+ * An {@link XYChartDataSource} that makes use of a {@link MetricInput}.
+ * 
+ * @author Hank.Herr
+ */
+public class MetricInputXYChartDataSource extends DefaultXYChartDataSource
+{
+    private final MetricInput metricInput;
+
+    /**
+     * @param orderIndex The data source order index within the plotted chart.
+     * @param input The {@link MetricInput} for which to display a chart.
+     */
+    public MetricInputXYChartDataSource(final int orderIndex, final MetricInput input)
+    {
+        metricInput = input;
+        buildInitialParameters(orderIndex);
+
+        //TODO This stuff needs to come from meta data for MetricInput!!!
+        this.setXAxisType(ChartConstants.AXIS_IS_NUMERICAL);
+        this.setComputedDataType(ChartConstants.AXIS_IS_NUMERICAL);
+        this.setSourceNameInTable("TEST TABLE NAME");
+        this.setUnitsString("TEST UNITS");
+        this.setDomainHeader("TEST DOMAIN COL");
+    }
+
+    /**
+     * Called in the constructor, it calls {@link #constructAllSeriesDrawingParameters(int)} and initializes the data
+     * source drawing parameters.
+     */
+    private void buildInitialParameters(final int dataSourceOrderIndex)
+    {
+        getDefaultFullySpecifiedDataSourceDrawingParameters().setDataSourceOrderIndex(dataSourceOrderIndex);
+        getDefaultFullySpecifiedDataSourceDrawingParameters().setPlotterName("LineAndScatter");
+        getDefaultFullySpecifiedDataSourceDrawingParameters().setSubPlotIndex(0);
+        getDefaultFullySpecifiedDataSourceDrawingParameters().setYAxisIndex(0);
+
+        //Initialize the series parameters to be fully defined.
+        constructAllSeriesDrawingParameters(metricInput.size());
+        for(int i = 0; i < metricInput.size(); i++)
+        {
+            final SeriesDrawingParameters seriesParms = this.getDefaultFullySpecifiedDataSourceDrawingParameters()
+                                                            .getSeriesDrawingParametersForSeriesIndex(i);
+
+            //Need fully specified parameters since these are not overrides.  
+            seriesParms.setupDefaultParameters();
+
+            //Override the default legend names in order to number them starting with 0.
+            seriesParms.setNameInLegend("Series " + i);
+            if(i == 0)
+            {
+                seriesParms.setNameInLegend("Series First");
+            }
+        }
+    }
+
+    @Override
+    public XYChartDataSource returnNewInstanceWithCopyOfInitialParameters() throws XYChartDataSourceException
+    {
+        final MetricInputXYChartDataSource copy = new MetricInputXYChartDataSource(getDataSourceOrderIndex(),
+                                                                                   metricInput);
+
+        copy.copyTheseParametersIntoDataSource(this);
+        return copy;
+    }
+
+    @Override
+    protected XYDataset buildXYDataset(final DataSourceDrawingParameters parameters) throws XYChartDataSourceException
+    {
+        SingleValuedPairsXYDataset dataSet;
+
+        //Depending on answer to the question at the top of SingleValuedPairs, we may just call 
+        //metricInput.buildXYDataset() to acquire the data set and then set the legend names below.
+        //Then we wouldn't need this cheezy instanceof if-clause.
+        if(metricInput instanceof SingleValuedPairs)
+        {
+            dataSet = new SingleValuedPairsXYDataset((SingleValuedPairs)metricInput);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Class of MetricInput not understood.");
+        }
+
+        //Set the legend names based on the passed in override parameters.
+        //Legend names are set in the dataSet itself, which is why this must be done when the dataSet is created.
+        //I know... I don't like it either.
+        for(int i = 0; i < dataSet.getSeriesCount(); i++)
+        {
+            dataSet.setLegendName(i,
+                                  parameters.getArguments()
+                                            .replaceArgumentsInString(parameters.getSeriesDrawingParametersForSeriesIndex(i)
+                                                                                .getArgumentReplacedNameInLegend()));
+        }
+
+        return dataSet;
+    }
+}
---
+++ wres-vis/src/wres/vis/WRESArgumentProcessor.java
@@ -0,0 +1,33 @@
+package wres.vis;
+
+import ohd.hseb.hefs.utils.arguments.DefaultArgumentsProcessor;
+import ohd.hseb.hefs.utils.plugins.UniqueGenericParameterList;
+
+/**
+ * WRES implementation of {@link DefaultArgumentsProcessor}. After constructing, arguments can be populated by calling
+ * {@link UniqueGenericParameterList#addParameter(String, String)} on the return value of {@link #getArguments()}.
+ * 
+ * @author Hank.Herr
+ */
+public class WRESArgumentProcessor extends DefaultArgumentsProcessor
+{
+    //TODO This needs to be fleshed out.  Specifically, the constructor should take some kind of input to the charting process that 
+    //includes metadata and then map that metadata to arguments included here so that the XML template can use those arguments.
+    public WRESArgumentProcessor()
+    {
+        super();
+
+        //Add arguments here.
+
+        //See the ExporterArgumentsProcessor for how to added functions arguments if needed, here.
+    }
+
+    /**
+     * Convenience wrapper on {@link UniqueGenericParameterList#addParameter(String, String)} for the return of
+     * {@link #getArguments()}.
+     */
+    public void addArgument(final String key, final String value)
+    {
+        getArguments().addParameter(key, value);
+    }
+}
