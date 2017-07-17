ba5216801169f725388947c42b4d25d164bc5bef
Chris Tubbs <christopher.tubbs@noreply.gov>
2017-07-17 09:25:06 -0500

    The project settings class now pulls in the new ProjectConfig class

--- wres-io/src/wres/io/config/ProjectSettings.java
+++ wres-io/src/wres/io/config/ProjectSettings.java
@@ -1,20 +1,26 @@
 package wres.io.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wres.config.generated.DestinationConfig;
+import wres.config.generated.ObjectFactory;
+import wres.config.generated.ProjectConfig;
+import wres.io.config.specification.ProjectSpecification;
+import wres.io.reading.XMLReader;
+import wres.util.Collections;
+import wres.util.Strings;
+import wres.util.XML;
+
+import javax.xml.bind.*;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.stream.XMLStreamReader;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import wres.io.config.specification.ProjectSpecification;
-import wres.io.reading.XMLReader;
-import wres.util.Collections;
-import wres.util.XML;
 
 /**
  * Provides access to projects determining the configuration of metric execution
@@ -23,6 +29,7 @@ import wres.util.XML;
 public final class ProjectSettings extends XMLReader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectSettings.class);
+    private static final List<ProjectConfig> CONFIGS = new ArrayList<>();
 
     /**
      *  The underlying storage structure for the project configurations
@@ -40,12 +47,17 @@ public final class ProjectSettings extends XMLReader {
 		try
 		{
 			loadProjects();
+			loadConfigs();
 		}
 		catch (IOException ioe)
 		{
 			LOGGER.error("Could not load project files.", ioe);
 		}
-	}
+        catch (JAXBException e) {
+            LOGGER.error("The JAXB config was not loaded correctly.");
+            LOGGER.error(Strings.getStackTrace(e));
+        }
+    }
 	
 	/**
 	 * Loads all project configurations defined within a directory
@@ -70,6 +82,71 @@ public final class ProjectSettings extends XMLReader {
 			parse();
 		}
 	}
+
+	private void loadConfigs() throws JAXBException {
+        final String configLocation = "nonsrc/config_possibility.xml";
+        ProjectConfig projectConfig;
+
+        try
+        {
+            File xmlFile = new File(configLocation);
+            Source xmlSource = new StreamSource(xmlFile);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            jaxbUnmarshaller.setEventHandler(validationEvent -> {
+                if (LOGGER.isDebugEnabled())
+                {
+                    LOGGER.debug("Severity: {}", validationEvent.getSeverity());
+                    LOGGER.debug("Location: {}", validationEvent.getLocator());
+                    LOGGER.debug("Message: {}", validationEvent.getMessage());
+                }
+                return true;
+            });
+
+            JAXBElement<ProjectConfig> wrappedConfig = jaxbUnmarshaller.unmarshal(xmlSource, ProjectConfig.class);
+
+            projectConfig = wrappedConfig.getValue();
+            ProjectSettings.CONFIGS.add(projectConfig);
+
+            LOGGER.debug("ProjectConfig: {}", projectConfig);
+            if (LOGGER.isDebugEnabled())
+            {
+                LOGGER.debug("JAXBContext: {}", jaxbContext);
+                LOGGER.debug("Unmarshaller: {}", jaxbUnmarshaller);
+                LOGGER.debug("ProjectConfig.getName(): {}", projectConfig.getLabel());
+                LOGGER.debug("ProjectConfig metric 0: {}", projectConfig.getOutputs().getMetric().get(0).getValue());
+                LOGGER.debug("ProjectConfig forecast variable: {}", projectConfig.getInputs());
+
+                for (DestinationConfig d : projectConfig.getOutputs().getDestination())
+                {
+                    LOGGER.debug("Location of destination {} is line {} col {}",
+                                 d,
+                                 d.sourceLocation().getLineNumber(),
+                                 d.sourceLocation().getColumnNumber());
+
+                    if (d.getConfig() != null)
+                    {
+                        DestinationConfig.Config conf = d.getConfig();
+                        LOGGER.debug("Location of config for {} is line {} col {}",
+                                     d,
+                                     conf.sourceLocation().getLineNumber(),
+                                     conf.sourceLocation().getColumnNumber());
+                    }
+                }
+            }
+        }
+        catch (JAXBException je)
+        {
+            LOGGER.error("Could not parse file {}:", configLocation, je);
+            throw je;
+        }
+        catch (NumberFormatException nfe)
+        {
+            LOGGER.error("A value in the file {} was unable to be converted to a number.", configLocation, nfe);
+            throw nfe;
+        }
+    }
 	
 	/**
 	 * @return The collection of all configured projects
