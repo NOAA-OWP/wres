79feed1ed0accbac23c3b1cac6cc55da72cc033d
Jesse Bickel <jesse.bickel@noreply.gov>
2017-07-17 11:18:52 -0500

    Make some pair data comparable, improve toString. Refs #34242

---
+++ wres-datamodel/test/wres/datamodel/ComparisonTest.java
@@ -0,0 +1,62 @@
+package wres.datamodel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jesse on 7/17/17.
+ */
+public class ComparisonTest
+{
+    @Test
+    public void fancyPairWithEqualValuesIsEqualTest()
+    {
+        double[] firstArr = {1.0, 2.0};
+        double[] secondArr = {1.0, 2.0};
+        PairOfDoubleAndVectorOfDoubles firstPair = DataFactory.pairOf(3.0, firstArr);
+        PairOfDoubleAndVectorOfDoubles secondPair = DataFactory.pairOf(3.0, secondArr);
+        assertTrue("Expect pairs to be equal",
+                   firstPair.compareTo(secondPair) == 0);
+        assertTrue("Expect pairs to be equal",
+                   secondPair.compareTo(firstPair) == 0);
+    }
+
+    @Test
+    public void fancyPairFirstValueLessThanSecond()
+    {
+        double[] emptyArr = {};
+        PairOfDoubleAndVectorOfDoubles firstPair = DataFactory.pairOf(1.0, emptyArr);
+        PairOfDoubleAndVectorOfDoubles secondPair = DataFactory.pairOf(2.0, emptyArr);
+        assertTrue("Expect first pair to be less than second",
+                   firstPair.compareTo(secondPair) < 0);
+        assertTrue("Expect second pair to be more than first",
+                   secondPair.compareTo(firstPair) > 0);
+    }
+
+    @Test
+    public void fancyPairEmptyArrayIsLessThanNonEmptyTest()
+    {
+        double[] emptyArr = {};
+        double[] nonEmptyArr = {1.0};
+        PairOfDoubleAndVectorOfDoubles emptyPair = DataFactory.pairOf(2.0, emptyArr);
+        PairOfDoubleAndVectorOfDoubles nonEmptyPair = DataFactory.pairOf(2.0, nonEmptyArr);
+        assertTrue("Expect empty array pair to be less than nonEmpty",
+                   emptyPair.compareTo(nonEmptyPair) < 0);
+        assertTrue("Expect nonEmpty array pair to be more than empty",
+                   nonEmptyPair.compareTo(emptyPair) > 0);
+    }
+
+    @Test
+    public void fancyPairEqualLengthButDifferentValuesInArrayTest()
+    {
+        double[] firstArr = {1.0, 2.0};
+        double[] secondArr = {1.0, 3.0};
+        PairOfDoubleAndVectorOfDoubles firstPair = DataFactory.pairOf(4.0, firstArr);
+        PairOfDoubleAndVectorOfDoubles secondPair = DataFactory.pairOf(4.0, secondArr);
+        assertTrue("Expect first array pair to be less than second",
+                   firstPair.compareTo(secondPair) < 0);
+        assertTrue("Expect second array pair to be more than first",
+                secondPair.compareTo(firstPair) > 0);
+    }
+}
