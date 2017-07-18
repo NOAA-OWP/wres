1a842d1ac036f721d2ac6da6d938f6ff45315c22
Hank.Herr <Hank.Herr@noreply.gov>
2017-07-18 08:12:05 -0400

    Changes for wres-vis
    Updated pairs chart test to use nicer looking data; moved templates to
    dist/lib/conf

--- wres-vis/src/wres/vis/ScalarOutputByThresholdLeadXYChartDataSource.java
+++ wres-vis/src/wres/vis/ScalarOutputByThresholdLeadXYChartDataSource.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 import org.jfree.data.xy.XYDataset;
 
+import com.google.common.base.Strings;
+
 import ohd.hseb.charter.ChartConstants;
 import ohd.hseb.charter.datasource.DefaultXYChartDataSource;
 import ohd.hseb.charter.datasource.XYChartDataSource;
@@ -39,7 +41,7 @@ public class ScalarOutputByThresholdLeadXYChartDataSource extends DefaultXYChart
     {
         Objects.requireNonNull(input, "Specify a non-null input dataset for building the chart data source.");
         this.input = input;
-        buildInitialParameters(orderIndex);    
+        buildInitialParameters(orderIndex);
         this.setXAxisType(ChartConstants.AXIS_IS_NUMERICAL);
         this.setComputedDataType(ChartConstants.AXIS_IS_NUMERICAL);
     }
@@ -54,7 +56,7 @@ public class ScalarOutputByThresholdLeadXYChartDataSource extends DefaultXYChart
         getDefaultFullySpecifiedDataSourceDrawingParameters().setPlotterName("LineAndScatter");
         getDefaultFullySpecifiedDataSourceDrawingParameters().setSubPlotIndex(0);
         getDefaultFullySpecifiedDataSourceDrawingParameters().setYAxisIndex(0);
-        
+
         //TODO Need to ensure that the arguments used below are standard arguments created in the factory that generates images!
         getDefaultFullySpecifiedDataSourceDrawingParameters().setDefaultDomainAxisTitle("THRESHOLD VALUE@inputUnitsText@");
         getDefaultFullySpecifiedDataSourceDrawingParameters().setDefaultRangeAxisTitle("@metricShortName@@outputUnitsText@");
@@ -79,13 +81,29 @@ public class ScalarOutputByThresholdLeadXYChartDataSource extends DefaultXYChart
         copy.copyTheseParametersIntoDataSource(this);
         return copy;
     }
-    
+
     @Override
     protected XYDataset buildXYDataset(final DataSourceDrawingParameters parameters) throws XYChartDataSourceException
     {
         //Legend items are thresholds, and they are fully defined in the input data source, so an override is unlikely 
         //to make sense here. 
-        return new ScalarOutputByThresholdLeadXYDataset(input);
-    }    
+        final ScalarOutputByThresholdLeadXYDataset dataSet = new ScalarOutputByThresholdLeadXYDataset(input);
+
+        //Set the legend names based on the passed in parameters, which are fully processed.
+        //Legend names are set in the dataSet itself, which is why this must be done when the dataSet is created.
+        //I know... I don't like it either.
+        for(int i = 0; i < dataSet.getSeriesCount(); i++)
+        {
+            if(!Strings.isNullOrEmpty(parameters.getSeriesDrawingParametersForSeriesIndex(i).getNameInLegend()))
+            {
+                dataSet.setLegendName(i,
+                                      parameters.getArguments()
+                                                .replaceArgumentsInString(parameters.getSeriesDrawingParametersForSeriesIndex(i)
+                                                                                    .getArgumentReplacedNameInLegend()));
+            }
+        }
+
+        return dataSet;
+    }
 
 }
