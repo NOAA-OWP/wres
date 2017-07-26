d7b1dbce72cf75019de78c8803969deaedbf0ee3
Jesse Bickel <jesse.bickel@noreply.gov>
2017-07-26 13:59:58 -0500

    .equals and .hashCode on SafePairOfDoubles.

--- wres-datamodel/test/wres/datamodel/SafeVectorOfDoublesTest.java
+++ wres-datamodel/test/wres/datamodel/SafeVectorOfDoublesTest.java
@@ -2,7 +2,7 @@ package wres.datamodel;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SafeVectorOfDoublesTest
