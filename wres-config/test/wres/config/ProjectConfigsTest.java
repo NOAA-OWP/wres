7f6fa8b52b9d3684d8ed4bac1f629408c2fbd09d
Jesse Bickel <jesse.bickel@noreply.gov>
2017-08-07 14:15:13 -0500

    Test value threshold config. Return unmodifiable. Refs #35803

--- wres-config/test/wres/config/ProjectConfigsTest.java
+++ wres-config/test/wres/config/ProjectConfigsTest.java
@@ -6,6 +6,7 @@ import org.junit.rules.ExpectedException;
 import wres.config.generated.LeftOrRightOrBaseline;
 import wres.config.generated.ProbabilityThresholdConfig;
 import wres.config.generated.ThresholdOperator;
+import wres.config.generated.ValueThresholdConfig;
 
 import java.util.List;
 
@@ -85,5 +86,39 @@ public class ProjectConfigsTest
 
         exception.expect( ProjectConfigException.class );
         ProjectConfigs.parseProbabilities( config );
+    }
+
+    @Test
+    public void testSaneValueList() throws ProjectConfigException
+    {
+        String values = "200, 50.0,-8";
+
+        LeftOrRightOrBaseline applyToLeft = LeftOrRightOrBaseline.fromValue( "left" );
+        ThresholdOperator operator = ThresholdOperator.fromValue( "less than" );
+        ValueThresholdConfig config =
+                new ValueThresholdConfig( applyToLeft, values, operator );
+
+        List<Double> results = ProjectConfigs.parseValues( config );
+
+        assertTrue( "Expected 0.2 to be in first position.",
+                0 == Double.compare( 200, results.get( 0 ) ) );
+        assertTrue( "Expected 0.5 to be in second position.",
+                0 == Double.compare( 50, results.get( 1 ) ) );
+        assertTrue( "Expected 0.8 to be in first position.",
+                0 == Double.compare( -8, results.get( 2 ) ) );
+    }
+
+    @Test
+    public void testProblematicValueList() throws ProjectConfigException
+    {
+        String values = "schnitzelbank";
+
+        LeftOrRightOrBaseline applyToLeft = LeftOrRightOrBaseline.fromValue( "left" );
+        ThresholdOperator operator = ThresholdOperator.fromValue( "less than" );
+        ValueThresholdConfig config =
+                new ValueThresholdConfig( applyToLeft, values, operator );
+
+        exception.expect( ProjectConfigException.class );
+        ProjectConfigs.parseValues( config );
     }
 }
