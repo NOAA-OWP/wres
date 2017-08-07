fe4dca4a294bc36a26864da2324b9c7da6045b52
Jesse Bickel <jesse.bickel@noreply.gov>
2017-08-07 13:06:52 -0500

    More complete test coverage. Documentation. Refs #35803

--- wres-config/src/wres/config/ProjectConfigs.java
+++ wres-config/src/wres/config/ProjectConfigs.java
@@ -6,8 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Provides static methods that help with ProjectConfig and its children.
+ */
 public class ProjectConfigs
 {
+    private ProjectConfigs()
+    {
+        // Prevent construction, this is a static helper class.
+    }
+
+    /**
+     * Parse and validate probabilities from a given ProbabilityThresholdConfig
+     * @param p the threshold configuration to parse
+     * @return the list of Doubles found
+     * @throws ProjectConfigException when unparseable configuration is given
+     * @throws NullPointerException when p is null
+     */
     public static List<Double> parseProbabilities( ProbabilityThresholdConfig p )
             throws ProjectConfigException
     {
@@ -36,7 +51,9 @@ public class ProjectConfigs
         }
         catch ( NumberFormatException s )
         {
-            throw new ProjectConfigException( p, "Failed to parse probabilities.", s);
+            throw new ProjectConfigException( p,
+                                              "Failed to parse probabilities.",
+                                              s );
         }
         return result;
     }
