e723e9626807a0bb1c66bdd6163452ed5c35b330
James Brown <james.d.brown@noreply.gov>
2017-08-08 13:40:41 +0100

    Checked in missing MetricConfigurationException.java

---
+++ wres-metrics/src/wres/engine/statistics/metric/MetricConfigurationException.java
@@ -0,0 +1,52 @@
+package wres.engine.statistics.metric;
+
+/**
+ * A checked exception associated with a metric calculation.
+ * 
+ * @author james.brown@hydrosolved.com
+ * @version 0.1
+ * @since 0.1
+ */
+
+public final class MetricConfigurationException extends Exception
+{
+
+    /**
+     * Serial identifier.
+     */
+    
+    private static final long serialVersionUID = 6790246291277821024L;
+    
+    /**
+     * Constructs an {@link MetricConfigurationException} with no message.
+     */
+
+    public MetricConfigurationException()
+    {
+        super();
+    }
+
+    /**
+     * Constructs a {@link MetricConfigurationException} with the specified message.
+     * 
+     * @param message the message.
+     */
+
+    public MetricConfigurationException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Constructs a {@link MetricConfigurationException} with the specified message.
+     * 
+     * @param message the message.
+     * @param cause the cause of the exception
+     */
+
+    public MetricConfigurationException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+}
