d5a97604aae63a8e2d4aacefe945724a863316a3
Hank.Herr <Hank.Herr@noreply.gov>
2017-08-11 11:50:03 -0400

    Javadoc fixed

--- wres-vis/src/wres/vis/WRESAbstractXYDataset.java
+++ wres-vis/src/wres/vis/WRESAbstractXYDataset.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.AbstractXYDataset;
 import org.jfree.data.xy.XYDataset;
+
 /**
  * Base class for all {@link XYDataset} subclasses implemented for wres-vis. It provides a mechanism for specifying and
  * acquiring default legend names which {@link JFreeChart} acquires via the data set.<br>
@@ -15,10 +16,11 @@ import org.jfree.data.xy.XYDataset;
  * <br>
  * 2. Override {@link #preparePlotData(Object)} in order to prepare the plot data. In the simplest case, this should
  * pass through its argument to {@link #setPlotData(Object)}. In more complicated cases, this may need to prepare a data
- * storage container and pass that data storage container to {@link #setPlotData(Object)}.<br>
+ * storage container and pass that data storage container to {@link #setPlotData(Object)}. Be sure to call
+ * {@link #setPlotData(Object)}.<br>
  * <br>
- * 3. Define the constructor to accept an instance of the appropriate raw input data (metric output data) and pass it to
- * the abstract constructor herein. Perform any actions required afterward, such as, in the case for
+ * 3. Define the constructor to accept an instance of the appropriate raw input data (generic type U) and pass it to the
+ * abstract constructor herein. Perform any actions required afterward, such as, in the case for
  * {@link ScalarOutputByLeadThresholdXYDataset}, setting override legend names.<br>
  * <br>
  * 4. Implement the other {@link AbstractXYDataset} required methods appropriately. Note that the implementation of
@@ -26,14 +28,16 @@ import org.jfree.data.xy.XYDataset;
  * {@link #isLegendNameOverridden(int)} returns true.
  * 
  * @author Hank.Herr
- * @param <T> The data type of the plot data which is stored internally within {@link #plotData}, referred to vie {@link #getPlotData()}, and referred to as needed in subclasses.
- * @param <U> The raw data from which the plot data will be prepared and which is passed into the constructor method, {@link WRESAbstractXYDataset#WRESAbstractXYDataset(Object)}.
- * Most often, this will be identical to the plot data generic type.
+ * @param <T> The data type of the plot data which is stored internally within {@link #plotData}, referred to vie
+ *            {@link #getPlotData()}, and referred to as needed in subclasses.
+ * @param <U> The raw data from which the plot data will be prepared and which is passed into the constructor method,
+ *            {@link WRESAbstractXYDataset#WRESAbstractXYDataset(Object)}. Most often, this will be identical to the
+ *            plot data generic type.
  */
 public abstract class WRESAbstractXYDataset<T, U> extends AbstractXYDataset
 {
     /**
-     * Generic storage container for the data.
+     * Generic storage container for the data to plot which is derived from the raw data passed into constructor.
      */
     private T plotData;
 
@@ -64,9 +68,9 @@ public abstract class WRESAbstractXYDataset<T, U> extends AbstractXYDataset
 
     /**
      * Called during construction, this must prepare the plot data (from the point of view of the subclass and its
-     * corresponding {@link WRESAbstractXYDataset}) from the raw data, stored as an {@link Object}, that is passed into
-     * the constructor of this abstract class. It must then call {@link #setPlotData(Object)} so that it is stored
-     * herein.
+     * corresponding {@link WRESAbstractXYDataset}) from the raw data provided (the same data passed into the
+     * constructor {@link WRESAbstractXYDataset#WRESAbstractXYDataset(Object)}). It must then call
+     * {@link #setPlotData(Object)} so that it is stored herein.
      */
     protected abstract void preparePlotData(U rawData);
 
