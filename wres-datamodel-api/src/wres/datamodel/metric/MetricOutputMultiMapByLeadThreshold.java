67c5d09e7c2ae7de70f424df1f5b2488614e9527
James Brown <james.d.brown@noreply.gov>
2017-08-18 13:22:23 +0100

    Started to generalize the top-level containers in the datamodel for additional types beyond DR1.

--- wres-datamodel-api/src/wres/datamodel/metric/MetricOutputMultiMapByLeadThreshold.java
+++ wres-datamodel-api/src/wres/datamodel/metric/MetricOutputMultiMapByLeadThreshold.java
@@ -1,10 +1,6 @@
 package wres.datamodel.metric;
 
-import java.util.Collection;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.function.BiConsumer;
 
 /**
  * A map of {@link MetricOutputMapByLeadThreshold} stored by metric identifier. Implements the same read-only API as the
@@ -16,143 +12,19 @@ import java.util.function.BiConsumer;
  * @since 0.1
  */
 
-public interface MetricOutputMultiMapByLeadThreshold<T extends MetricOutput<?>>
+public interface MetricOutputMultiMapByLeadThreshold<S extends MetricOutput<?>>
+extends MetricOutputMultiMap<MetricOutputMapByLeadThreshold<S>>
 {
-
-    /**
-     * Returns a {@link MetricOutputMapByLeadThreshold} corresponding to the input identifiers or null
-     * 
-     * @param key the key
-     * @return the mapping or null
-     */
-
-    default MetricOutputMapByLeadThreshold<T> get(final MapBiKey<MetricConstants, MetricConstants> key)
-    {
-        return get(key.getFirstKey(), key.getSecondKey());
-    }
-
-    /**
-     * Convenience method that returns the {@link MetricOutputMapByLeadThreshold} associated with the specified metric
-     * identifier and {@link MetricConstants#MAIN} for the metric component identifier.
-     * 
-     * @param metricID the metric identifier
-     * @return the output for the specified key or null
-     */
-
-    default MetricOutputMapByLeadThreshold<T> get(final MetricConstants metricID)
-    {
-        return get(metricID, MetricConstants.MAIN);
-    }
-
-    /**
-     * Returns true if the map is empty, false otherwise.
-     * 
-     * @return true if the map is empty, false otherwise
-     */
-
-    default boolean isEmpty()
-    {
-        return size() > 0;
-    }
-
-    /**
-     * Consume each pair in the map.
-     * 
-     * @param consumer the consumer
-     */
-
-    default void forEach(BiConsumer<MapBiKey<MetricConstants, MetricConstants>, MetricOutputMapByLeadThreshold<T>> consumer)
-    {
-        for(Entry<MapBiKey<MetricConstants, MetricConstants>, MetricOutputMapByLeadThreshold<T>> entry: entrySet())
-        {
-            consumer.accept(entry.getKey(), entry.getValue());
-        }
-    }
-
-    /**
-     * Returns a builder.
-     * 
-     * @return a builder
-     */
-
-    Builder<T> builder();
-
-    /**
-     * Returns a {@link MetricOutputMap} corresponding to the input identifiers or null
-     * 
-     * @param metricID the metric identifier
-     * @param componentID the metric component identifier
-     * @return the mapping or null
-     */
-
-    MetricOutputMapByLeadThreshold<T> get(MetricConstants metricID, MetricConstants componentID);
-
-    /**
-     * Returns true if the mapping contains the specified key, false otherwise.
-     * 
-     * @param key the key
-     * @return true if the map contains the key, false otherwise
-     */
-
-    boolean containsKey(MapBiKey<MetricConstants, MetricConstants> key);
-
-    /**
-     * Returns true if the mapping contains the specified value, false otherwise.
-     * 
-     * @param value the value
-     * @return true if the map contains the value, false otherwise
-     */
-
-    boolean containsValue(MetricOutputMapByLeadThreshold<T> value);
-
-    /**
-     * Returns a collection view of the values in the map.
-     * 
-     * @return a collection view of the values
-     */
-
-    Collection<MetricOutputMapByLeadThreshold<T>> values();
-
-    /**
-     * Returns a view of the keys in the map for iteration.
-     * 
-     * @return a view of the keys
-     */
-
-    Set<MapBiKey<MetricConstants, MetricConstants>> keySet();
-
-    /**
-     * Returns a view of the entries in the map for iteration.
-     * 
-     * @return a view of the map entries
-     */
-
-    Set<Entry<MapBiKey<MetricConstants, MetricConstants>, MetricOutputMapByLeadThreshold<T>>> entrySet();
-
-    /**
-     * Returns the number of element in the map.
-     * 
-     * @return the size of the mapping
-     */
-
-    int size();
 
     /**
      * A builder.
      *
-     * @param <T> the type of output to store
+     * @param <S> the type of output to store
      */
 
-    interface Builder<T extends MetricOutput<?>>
+    interface Builder<S extends MetricOutput<?>>
+    extends MetricOutputMultiMap.Builder<MetricOutputMapByLeadThreshold<S>>
     {
-
-        /**
-         * Returns the built store.
-         * 
-         * @return the result store
-         */
-
-        MetricOutputMultiMapByLeadThreshold<T> build();
 
         /**
          * Adds a new result for a collection of metrics to the internal store.
@@ -162,22 +34,12 @@ public interface MetricOutputMultiMapByLeadThreshold<T extends MetricOutput<?>>
          * @return the builder
          */
 
-        default Builder<T> put(MapBiKey<Integer, Threshold> key, MetricOutputMapByMetric<T> result)
+        default Builder<S> put(MapBiKey<Integer, Threshold> key, MetricOutputMapByMetric<S> result)
         {
             put(key.getFirstKey(), key.getSecondKey(), result);
             return this;
         }
-        
-        /**
-         * Adds a new result to the internal store.
-         * 
-         * @param key the key
-         * @param result the result
-         * @return the builder
-         */
 
-        Builder<T> put(MapBiKey<MetricConstants, MetricConstants> key, MetricOutputMapByLeadThreshold<T> result);                 
-        
         /**
          * Adds a new result for a collection of metrics to the internal store.
          * 
@@ -187,7 +49,7 @@ public interface MetricOutputMultiMapByLeadThreshold<T extends MetricOutput<?>>
          * @return the builder
          */
 
-        Builder<T> put(int leadTime, Threshold threshold, MetricOutputMapByMetric<T> result);
+        Builder<S> put(int leadTime, Threshold threshold, MetricOutputMapByMetric<S> result);
 
     }
 
