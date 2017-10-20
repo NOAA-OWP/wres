3ea18f2613d052d348517dcfca2d35e8bab8db54
Jesse Bickel <jesse.bickel@noreply.gov>
2017-10-20 12:01:08 -0500

    Error for PI-XML and zoneOffset to be specified in one source
    When the user specifies both PI-XML and a zoneOffset, that is an error since
    WRES requires PI-XML to have a zone offset inside the data.
    
    Ingest-time checking will still occur for PI-XML data because specifying the
    format of the data is optional and could be auto-detected in the future as well.
    
    Fixes #38154

--- test/wres/config/ValidationTest.java
+++ test/wres/config/ValidationTest.java
@@ -1,6 +1,77 @@
 package wres.config;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import org.xml.sax.Locator;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import wres.config.generated.DataSourceConfig;
+import wres.config.generated.Format;
+import wres.io.config.ProjectConfigPlus;
+
 public class ValidationTest
 {
-    // Validation tests go here. Placeholder for now.
+
+    @Test
+    public void specifyingOffsetForPIXMLFailsValidation()
+            throws IOException
+    {
+        ProjectConfigPlus mockProjectConfigPlus = mock( ProjectConfigPlus.class );
+        Path mockPath = Paths.get("fake.xml");
+        when(mockProjectConfigPlus.getPath()).thenReturn( mockPath );
+
+        Locator mockSourceLocator =
+                new Locator() {
+                    @Override
+                    public String getPublicId()
+                    {
+                        return "fakePublicId";
+                    }
+
+                    @Override
+                    public String getSystemId()
+                    {
+                        return "fakeSystemId";
+                    }
+
+                    @Override
+                    public int getLineNumber()
+                    {
+                        return 1;
+                    }
+
+                    @Override
+                    public int getColumnNumber()
+                    {
+                        return 1;
+                    }
+                };
+
+        DataSourceConfig.Source source =
+                new DataSourceConfig.Source(
+                        null,
+                        Format.PI_XML,
+                        null,
+                        null,
+                        "-0500",
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                );
+
+        source.setSourceLocation( mockSourceLocator );
+
+        boolean result = Validation.isDateConfigValid( mockProjectConfigPlus,
+                                                       source );
+        assertFalse( "Expected validation failure when offset for "
+                     + "PI-XML data is specified.", result );
+    }
 }
