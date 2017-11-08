f0d57055e92e2d76895c8694d70e34abff5470aa
James Brown <james.d.brown@noreply.gov>
2017-11-08 16:46:16 +0000

    Fixing a Javadoc error in TimeSeries.

--- wres-datamodel-api/src/wres/datamodel/time/TimeSeries.java
+++ wres-datamodel-api/src/wres/datamodel/time/TimeSeries.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 import wres.datamodel.inputs.pairs.Pair;
 
 /**
- * <p>A representation of one or more time-series, each of which contains one or more <@link Pair> of times and values. 
+ * <p>A representation of one or more time-series, each of which contains one or more {@link Pair} of times and values. 
  * In this context, a time is an instant on the UTC timeline, which is represented by an {@link Instant}. Additionally,
  * each time-series has a basis time, which is also represented by an {@link Instant}. If a time-series is not 
  * explicitly associated with a basis time, the earliest time is assumed to be the basis time.</p>
@@ -18,7 +18,7 @@ import wres.datamodel.inputs.pairs.Pair;
  * 
  * <p>In this context, the need to represent one or more values for each time is significant, since it implies that 
  * a concrete implementation cannot assume a unique key for each value unless {@link #isRegular()} returns 
- * <code>true</code></p>
+ * <code>true</code>.</p>
  * 
  * <p><b>Implementation Requirements:</b></p>
  * 
@@ -42,16 +42,15 @@ public interface TimeSeries<T>
     Iterator<Pair<Instant,T>> timeIterator();
     
     /**
-     * Returns a nested {@link Iterator} where the nested iterator traverses the pairs of times and values associated
-     * with a time-series that has a common basis/reference time. For time-series with a single basis time (e.g. observed
-     * time-series), this will return a single nested iterator. For forecast time-series, it will return as many nested 
-     * iterators as discrete forecasts in the time-series container. The nested iterators are returned in time order 
-     * from the earliest basis time to the latest basis time on the UTC timeline.
+     * Returns an {@link Iterator} over the atomic time-series by basis/issue time. For time-series with a single 
+     * basis time (e.g. observed time-series), this will return one atomic time-series. For forecast time-series, it 
+     * will return as many atomic time-series as discrete forecasts in the time-series container. The time-series are 
+     * returned in time order from the earliest basis time to the latest basis time.
      * 
-     * @return a nested iterator over the time-series
+     * @return an iterator over the atomic time-series
      */
     
-    Iterator<Iterator<Pair<Instant,T>>> basisTimeIterator();
+    Iterator<TimeSeries<T>> basisTimeIterator();
     
     /**
      * Returns true if the time-series has a regular spacing of values with a single value for each time.
