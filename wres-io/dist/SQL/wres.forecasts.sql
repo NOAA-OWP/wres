8f3efa7e286183255d5acd398395ce539da3b267
Chris Tubbs <christopher.tubbs@noreply.gov>
2017-12-18 11:20:36 -0600

    Added script for forecast view

---
+++ wres-io/dist/SQL/wres.forecasts.sql
@@ -0,0 +1,19 @@
+﻿-- View: wres.forecasts
+
+-- DROP VIEW wres.forecasts;
+
+CREATE OR REPLACE VIEW wres.forecasts AS 
+SELECT  VBF.feature_id,
+	VBF.variable_id,
+	TS.initialization_date AS basis_time,
+	TS.initialization_date + INTERVAL '1 HOUR' * FV.lead AS valid_time,
+	FV.lead,
+	FV.forecasted_value,
+	TS.ensemble_id,
+	TS.measurementunit_id,
+	TS.timeseries_id
+FROM wres.TimeSeries TS
+INNER JOIN wres.ForecastValue FV
+	ON FV.timeseries_id = TS.timeseries_id
+INNER JOIN wres.VariableByFeature VBF
+	ON VBF.variableposition_id = TS.variableposition_id;
