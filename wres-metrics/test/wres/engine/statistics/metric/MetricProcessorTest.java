486cef9cdcc19e8762559a9a7d78de2ed1578972
James Brown <james.d.brown@noreply.gov>
2018-01-03 10:20:24 +0000

    Adds validation for configured outputs to Validation.java.
    Fixes #45006
    
    Ensures that pooling window configurations produce chart outputs.
    
    Refs #44868

--- wres-metrics/test/wres/engine/statistics/metric/MetricProcessorTest.java
+++ wres-metrics/test/wres/engine/statistics/metric/MetricProcessorTest.java
@@ -1,5 +1,6 @@
 package wres.engine.statistics.metric;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -7,6 +8,7 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
+import wres.config.generated.PairConfig;
 import wres.config.generated.ProjectConfig;
 import wres.datamodel.DataFactory;
 import wres.datamodel.DefaultDataFactory;
@@ -33,22 +35,22 @@ public final class MetricProcessorTest
     public void test1WillStoreMetricOutput()
     {
         final DataFactory metIn = DefaultDataFactory.getInstance();
-        String configPath = "testinput/metricProcessorSingleValuedPairsTest/test4AllValid.xml";
+        String configPath = "testinput/metricProcessorTest/test1AllValid.xml";
         try
         {
             ProjectConfig config = ProjectConfigPlus.from( Paths.get( configPath ) ).getProjectConfig();
             MetricProcessor<MetricOutputForProjectByTimeAndThreshold> trueProcessor =
                     MetricFactory.getInstance( metIn )
                                  .getMetricProcessorByTime( config,
-                                                                MetricOutputGroup.values() );
+                                                            MetricOutputGroup.values() );
             MetricProcessor<MetricOutputForProjectByTimeAndThreshold> falseProcessor =
                     MetricFactory.getInstance( metIn )
                                  .getMetricProcessorByTime( config );
             //Check for storage
             assertTrue( "Expected a metric processor that stores metric outputs.",
-                        trueProcessor.willStoreMetricOutput() == true );
-            assertTrue( "Expected a metric processor that does not store metric outputs.",
-                        falseProcessor.willStoreMetricOutput() == false );
+                        trueProcessor.willStoreMetricOutput() );
+            assertFalse( "Expected a metric processor that does not store metric outputs.",
+                         falseProcessor.willStoreMetricOutput() );
         }
         catch ( Exception e )
         {
@@ -72,27 +74,27 @@ public final class MetricProcessorTest
     public void test2HasMetrics()
     {
         final DataFactory metIn = DefaultDataFactory.getInstance();
-        String configPath = "testinput/metricProcessorSingleValuedPairsTest/test4AllValid.xml";
+        String configPath = "testinput/metricProcessorTest/test1AllValid.xml";
         try
         {
             ProjectConfig config = ProjectConfigPlus.from( Paths.get( configPath ) ).getProjectConfig();
             MetricProcessor<MetricOutputForProjectByTimeAndThreshold> processor =
                     MetricFactory.getInstance( metIn )
                                  .getMetricProcessorByTime( config,
-                                                                MetricOutputGroup.values() );
+                                                            MetricOutputGroup.values() );
             //Check for existence of metrics
             assertTrue( "Expected metrics for '" + MetricInputGroup.SINGLE_VALUED
                         + "' and '"
                         + MetricOutputGroup.SCALAR
                         + ".",
-                        processor.hasMetrics( MetricInputGroup.SINGLE_VALUED, MetricOutputGroup.SCALAR ) == true );
+                        processor.hasMetrics( MetricInputGroup.SINGLE_VALUED, MetricOutputGroup.SCALAR ) );
             assertTrue( "Expected metrics for '" + MetricInputGroup.SINGLE_VALUED
                         + "'.",
-                        processor.hasMetrics( MetricInputGroup.SINGLE_VALUED ) == true );
+                        processor.hasMetrics( MetricInputGroup.SINGLE_VALUED ) );
             assertTrue( "Expected metrics for '" + MetricOutputGroup.SCALAR
                         + ".",
-                        processor.hasMetrics( MetricOutputGroup.SCALAR ) == true );
-            assertTrue( "Expected threshold metrics.", processor.hasThresholdMetrics() == true );
+                        processor.hasMetrics( MetricOutputGroup.SCALAR ) );
+            assertTrue( "Expected threshold metrics.", processor.hasThresholdMetrics() );
         }
         catch ( Exception e )
         {
@@ -100,5 +102,73 @@ public final class MetricProcessorTest
         }
     }
 
+    /**
+     * Tests that non-score metrics are disallowed when configuring "all valid" metrics in combination with 
+     * {@link PairConfig#getPoolingWindow()} that is not null. Uses the configuration in 
+     * testinput/metricProcessorTest/test3SingleValued.xml and 
+     * testinput/metricProcessorTest/test3Ensemble.xml.
+     */
+
+    @Test
+    public void test3DisallowNonScores()
+    {
+        final DataFactory metIn = DefaultDataFactory.getInstance();
+        //Single-valued case
+        String configPathSingleValued = "testinput/metricProcessorTest/test3SingleValued.xml";
+        try
+        {
+            ProjectConfig config = ProjectConfigPlus.from( Paths.get( configPathSingleValued ) ).getProjectConfig();
+            MetricProcessor<MetricOutputForProjectByTimeAndThreshold> processor =
+                    MetricFactory.getInstance( metIn )
+                                 .getMetricProcessorByTime( config,
+                                                            MetricOutputGroup.values() );
+            //Check that score metrics are defined 
+            assertTrue( "Expected metrics for '" + MetricOutputGroup.SCALAR
+                        + "'.",
+                        processor.hasMetrics( MetricOutputGroup.SCALAR ) );
+            //Check that no non-score metrics are defined
+            for ( MetricOutputGroup next : MetricOutputGroup.values() )
+            {
+                if ( !next.equals( MetricOutputGroup.SCALAR ) && !next.equals( MetricOutputGroup.VECTOR ) )
+                {
+                    assertFalse( "Did not expect metrics for '" + next
+                                 + "'.",
+                                 processor.hasMetrics( next ) );
+                }
+            }
+        }
+        catch ( Exception e )
+        {
+            fail( "Unexpected exception on processing project configuration '" + configPathSingleValued + "'." );
+        }
+        //Ensemble case
+        String configPathEnsemble = "testinput/metricProcessorTest/test3Ensemble.xml";
+        try
+        {
+            ProjectConfig config = ProjectConfigPlus.from( Paths.get( configPathEnsemble ) ).getProjectConfig();
+            MetricProcessor<MetricOutputForProjectByTimeAndThreshold> processor =
+                    MetricFactory.getInstance( metIn )
+                                 .getMetricProcessorByTime( config,
+                                                            MetricOutputGroup.values() );
+            //Check that score metrics are defined 
+            assertTrue( "Expected metrics for '" + MetricOutputGroup.SCALAR
+                        + "'.",
+                        processor.hasMetrics( MetricOutputGroup.SCALAR ) );
+            //Check that no non-score metrics are defined
+            for ( MetricOutputGroup next : MetricOutputGroup.values() )
+            {
+                if ( !next.equals( MetricOutputGroup.SCALAR ) && !next.equals( MetricOutputGroup.VECTOR ) )
+                {
+                    assertFalse( "Did not expect metrics for '" + next
+                                 + "'.",
+                                 processor.hasMetrics( next ) );
+                }
+            }
+        }
+        catch ( Exception e )
+        {
+            fail( "Unexpected exception on processing project configuration '" + configPathSingleValued + "'." );
+        }
+    }
 
 }
