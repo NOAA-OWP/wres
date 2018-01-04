ca02d17c52192fc60057cc30dfc13078e9128141
Hank.Herr <Hank.Herr@noreply.gov>
2018-01-04 08:31:59 -0500

    Implemented shape rotation scheme in all data sources and templates
    Refs #44873

--- wres-vis/src/wres/vis/SingleValuedPairsXYChartDataSource.java
+++ wres-vis/src/wres/vis/SingleValuedPairsXYChartDataSource.java
@@ -23,6 +23,8 @@ public class SingleValuedPairsXYChartDataSource extends WRESXYChartDataSource<Si
         //TODO Need to ensure that these arguments are available here.  How best to do so?
         getDefaultFullySpecifiedDataSourceDrawingParameters().setDefaultDomainAxisTitle("@domainAxisLabelPrefix@@inputUnitsLabelSuffix@");
         getDefaultFullySpecifiedDataSourceDrawingParameters().setDefaultRangeAxisTitle("@rangeAxisLabelPrefix@@inputUnitsLabelSuffix@");
+        WRESTools.applyDefaultJFreeChartColorSequence( getDefaultFullySpecifiedDataSourceDrawingParameters() );
+        WRESTools.applyDefaultJFreeChartShapeSequence( getDefaultFullySpecifiedDataSourceDrawingParameters() );
     }
 
     @Override
