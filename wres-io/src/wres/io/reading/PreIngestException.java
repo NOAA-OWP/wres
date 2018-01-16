f15d0a649a9eb8551c2220178f244e35de3a4c2f
Jesse Bickel <jesse.bickel@noreply.gov>
2018-01-16 17:58:52 -0600

    More precise exception handling/translation/propagation
    Also use of .toFile and exists based on the File rather than Path.
    
    Thanks, SonarQube
    
    Refs #45523

---
+++ wres-io/src/wres/io/reading/PreIngestException.java
@@ -0,0 +1,14 @@
+package wres.io.reading;
+
+/**
+ * For when we cannot proceed with even starting the ingest due to catastrophic
+ * failure. For example, hashing the file failed, or communication with the
+ * database failed while getting ready to ingest.
+ */
+public class PreIngestException extends RuntimeException
+{
+    public PreIngestException( String message, Exception cause )
+    {
+        super( message, cause );
+    }
+}
