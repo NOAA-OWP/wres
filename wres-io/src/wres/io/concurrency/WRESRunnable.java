04f7d8ffda47591f1a35bd04b02f3298b3049be7
Jesse Bickel <jesse.bickel@noreply.gov>
2018-01-16 14:30:54 -0600

    Propagate exceptions from WRESRunnable
    An alternative would be to move to purely WRESCallable and remove both classes
    WRESRunnable and WRESRunnableException.
    
    Renamed a variable to follow the conventions, thanks SonarQube.
    
    Refs #45523

--- wres-io/src/wres/io/concurrency/WRESRunnable.java
+++ wres-io/src/wres/io/concurrency/WRESRunnable.java
@@ -1,17 +1,28 @@
 package wres.io.concurrency;
 
+import java.sql.SQLException;
+
 /**
  * Created by ctubbs on 7/19/17.
  */
 public abstract class WRESRunnable extends WRESTask implements Runnable
 {
     @Override
-    public final void run ()
+    public final void run()
     {
         this.executeOnRun();
-        this.execute();
+
+        try
+        {
+            this.execute();
+        }
+        catch ( SQLException se )
+        {
+            throw new WRESRunnableException( "Task failed:", se );
+        }
+
         this.executeOnComplete();
     }
 
-    protected abstract void execute();
+    protected abstract void execute() throws SQLException;
 }
