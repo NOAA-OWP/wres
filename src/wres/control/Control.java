39088c67327248792339c7c5ee9aa2210f67081a
Jesse Bickel <jesse.bickel@noreply.gov>
2018-01-17 16:58:54 -0600

    Finalize split of WresProcessingException from Control.java, preserving history
    Refs #45523

--- src/wres/control/Control.java
+++ src/wres/control/Control.java
@@ -1269,25 +1269,6 @@ public class Control implements Function<String[], Integer>
         }
     }
 
-    /**
-     * An exception representing that execution of a step failed.
-     * Needed because Java 8 Function world does not
-     * deal kindly with checked Exceptions.
-     */
-    private static class WresProcessingException extends RuntimeException
-    {
-        private static final long serialVersionUID = 6988169716259295343L;
-
-        WresProcessingException( String message )
-        {
-            super( message );
-        }
-        
-        WresProcessingException( String message, Throwable cause )
-        {
-            super( message, cause );
-        }
-    }
 
     /**
      * Composes a list of {@link CompletableFuture} so that execution completes when all futures are completed normally
