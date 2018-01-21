733bdf3869027748d8a0f304ec0f9870cd90a608
James Brown <james.d.brown@noreply.gov>
2018-01-21 21:26:21 +0000

    Adding DoubleScoreOutput.
    Refs #45681
    Refs #45523
    Refs #44778

---
+++ wres-datamodel-api/src/wres/datamodel/outputs/DoubleScoreOutput.java
@@ -0,0 +1,13 @@
+package wres.datamodel.outputs;
+
+/**
+ * A metric output that comprises a {@link Double}.
+ * 
+ * @author james.brown@hydrosolved.com
+ * @version 0.1
+ * @since 0.1
+ */
+
+public interface DoubleScoreOutput extends ScoreOutput<Double>
+{
+}
