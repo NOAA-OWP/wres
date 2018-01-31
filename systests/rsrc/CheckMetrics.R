9e29a5e62b56b95cd5811f21cb3ccc96103d32b3
James Brown <james.brown@noreply.gov>
2018-01-31 21:14:36 +0000

    Adds an R script to compute metric results from benchmark pairs.
    Refs #45174

---
+++ systests/rsrc/CheckMetrics.R
@@ -0,0 +1,199 @@
+# Import libraries silently
+suppressMessages( suppressWarnings( library( data.table ) ) )
+suppressMessages( suppressWarnings( library( hydroGOF ) ) ) 
+
+##########################################################################################
+#
+# A function that generates one or more named metrics at one threshold and for all 
+# features in the input pairs.
+#
+# pairs     The paired file
+# threshold The real valued threshold, which applies to the left for
+#           continuous measures. This is currently assumed to be >=
+# ...	      The list of metrics to compute
+#   
+##########################################################################################
+
+generateAllMetricsForAllFeatures <- function( pairs, threshold, ... ) 
+{
+      # Read, skip dates, fill header row, which may not be the first row
+	data <- fread( pairs, fill = TRUE, drop = "V2", stringsAsFactors = FALSE )
+	data <- data[ data$V1!="Feature", ]
+	# Convert columns with header information to numeric type
+	data[,4] <- sapply( data[,4],as.numeric )
+	data[,5] <- sapply( data[,5],as.numeric )
+	# Find the features
+      features <- unique(data$V1)
+	# Iterate through the features and generate the metrics for each one
+	for( i in 1: length(features) )
+	{
+		results <- generateAllMetricsForOneFeature ( data[ data$V1==features[i], ], threshold, ... )
+		print( paste( "Metric results for '", features[[i]], "':", sep="" ) )
+		print( results )		
+	} 
+}
+
+##########################################################################################
+#
+# A function that generates one or more named metrics at one threshold and for one 
+# feature. Expects a subset of pairs with only one feature.
+#
+# pairs     The pairs
+# threshold The real valued threshold, which applies to the left for
+#           continuous measures
+# ...	      The list of metrics to compute
+# Returns   The list of metric results, one for each metric
+#   
+##########################################################################################
+
+generateAllMetricsForOneFeature <- function( pairs, threshold, ... ) 
+{
+	# Validate
+	if( length( unique( pairs$V1 ) ) >1 )
+	{
+		stop( " Expected pairs for one feature only. ")
+	}
+	# Iterate through the metrics
+	metrics <- list( ... )
+	# Results to return in a named list
+	results<-list( length(metrics))
+	names(results)<-metrics
+	for ( i in 1: length( metrics ) )
+	{
+		results[[i]]=generateOneMetricForOneFeature( pairs, threshold, metrics[[i]] )
+	} 
+	results
+}
+
+##########################################################################################
+#
+# A function that generates one named metric at one threshold and for one feature.
+# Expects a subset of pairs with only one feature.
+#
+# pairs     The pairs
+# threshold The real valued threshold, which applies to the left for
+#           continuous measures
+# metric    The metric to compute
+# Returns   A data.table of metric results with two columns: window number and metric name
+#   
+##########################################################################################
+
+generateOneMetricForOneFeature <- function( pairs, threshold, metric ) 
+{
+	# Iterate through each window
+	windows <- unique( pairs$V4 )
+	metricToCompute <- getMetric( metric )
+	metric.results<-data.frame( matrix(ncol = 2, nrow = length( windows ) ) )
+	colnames(metric.results) <- c( "Window number", metric )
+	for (i in 1: length (windows) )
+	{
+		# Subset the data by window
+		nextWindow <- pairs[ pairs$V4 == windows[[i]],]
+
+		# Transform the pairs in a metric-appropriate way
+		nextWindow <- suppressWarnings( transformPairs( nextWindow, metric, threshold ) )
+		nextLeft <- unlist( nextWindow[,4] )  # Observations in column 4
+		nextRight <- unlist( nextWindow[,5] )
+		# Compute the mean if the right has more than one column, i.e. ensemble mean
+		if( ncol(pairs) > 5 )
+		{	
+			nextRight <- rowMeans( data.frame( nextWindow[,5:ncol(nextWindow)] ) )
+		}
+		metric.results[i,1] = windows[i]
+		metric.results[i,2] = metricToCompute( nextRight, nextLeft )
+	}
+	metric.results
+}
+
+##########################################################################################
+#
+# Applies a threshold to a set of pairs in a metric appropriate way. For continuous 
+# measures, the pairs are subset by left value. For dichotomous measures, the pairs 
+# are classified according to threshold.
+#
+# pairs     The pairs
+# metric    The named metric
+# Returns   A the transformed pairs
+#  
+##########################################################################################
+
+transformPairs <- function( pairs, metric, threshold )
+{
+	if( tolower( metric ) == "mean error" )    # Add more continuous possibilities here
+	{
+		# All data
+		if( is.na( threshold ) )
+		{
+			pairs
+		}
+		else 
+		{
+	      	# Observations in column 4, or V5
+			pairs[ pairs$V5 >= threshold , ]     # Assumed >=
+		}
+	}
+	else 	
+	{
+		stop( paste( "Could not find metric: ", metric ) )
+	}	
+}
+
+##########################################################################################
+#
+# A function that returns a named metric function and deals with the threshold 
+# specification in a metric appropriate way. For continuous measures, subset by left
+# value. For dichotomous measures classify using the threshold.
+#
+# metric    The named metric
+# Returns   A named metric function
+#  
+##########################################################################################
+
+getMetric <- function( metric )
+{
+	# Find metric
+	if( tolower( metric ) == "mean error" )
+	{
+		me
+	}
+	else 	
+	{
+		stop( paste( "Could not find metric: ", metric ) )
+	}
+} 
+
+##########################################################################################
+#
+# Main function that accepts command line arguments in this order:
+#
+# Path to pairs.csv file (sorted or unsorted)
+# Metric name	
+# Threshold
+#
+##########################################################################################
+
+main <- function()
+{
+	# Get the trailing command line args
+	args <-commandArgs(TRUE)
+	if(length(args) != 3 )
+	{
+		stop("Usage: path-to-pairs metric threshold")
+	}
+
+	# Echo
+	print( paste("Pairs: ", args[1], sep="" ) )
+	print( paste("Metric: ", args[2], sep="" ) )
+	print( paste("Threshold: ", args[3], sep="" ) )
+
+	# Generate the mean error for a particular threshold. NA = all data
+	generateAllMetricsForAllFeatures( args[1], as.numeric( args[3] ), args[2] )
+}
+main()
+
+
+
+
+
+
+
