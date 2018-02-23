f2e872f1bd7c975a41bd82a14198006b562b87f4
Chris Tubbs <christopher.tubbs@noreply.gov>
2018-02-23 15:44:48 -0600

    Updated the TrackRequests script to filter out admin scripts we don't have control over and refactored the scaling script.

--- wres-io/dist/SQL/TrackRequests.sql
+++ wres-io/dist/SQL/TrackRequests.sql
@@ -3,4 +3,5 @@ from pg_stat_activity
 WHERE client_port != -1
 	AND application_name = 'PostgreSQL JDBC Driver'
 	AND query != 'SHOW TRANSACTION ISOLATION LEVEL'
+    AND query NOT LIKE 'SELECT e.typdelim%'
 ORDER BY state_change
