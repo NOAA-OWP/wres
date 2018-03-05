263a9adbb8d1fb1c9a9e350e2f6914fe78f4dd49
Jesse Bickel <jesse.bickel@noreply.gov>
2018-03-05 16:37:59 -0600

    Write dimensions and variables to NetCDF files
    NetCDF requires a lot of up-front information at file creation time. So for the
    current draft, it is assumed that this will be calculated somewhere and passed
    in, or possibly pushed back into the writer to do that research. Control or
    one of its Processors will likely have more information than the lowly writer.
    
    Also some refactoring and better javadocs.
    
    Currently exposes an issue with cdm-4.6.11 due to a bug in cdm and also our
    exclusion rules for extra transitive jars.
    
    Refs #46739

--- wres-io/src/wres/io/writing/netcdf/NetcdfDoubleScoreWriter.java
+++ wres-io/src/wres/io/writing/netcdf/NetcdfDoubleScoreWriter.java
@@ -1,39 +1,112 @@
 package wres.io.writing.netcdf;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ucar.ma2.DataType;
+import ucar.nc2.Attribute;
+import ucar.nc2.Dimension;
+import ucar.nc2.NetcdfFileWriter;
+import ucar.nc2.Variable;
 
 import wres.config.ProjectConfigException;
+import wres.config.generated.DestinationConfig;
+import wres.config.generated.DestinationType;
 import wres.config.generated.ProjectConfig;
 import wres.datamodel.MetricConstants;
 import wres.datamodel.outputs.DoubleScoreOutput;
 import wres.datamodel.outputs.MapKey;
+import wres.datamodel.outputs.MetricOutput;
 import wres.datamodel.outputs.MetricOutputMapByTimeAndThreshold;
 import wres.datamodel.outputs.MetricOutputMultiMapByTimeAndThreshold;
+import wres.io.writing.WriteException;
+import wres.io.writing.WriterHelper;
 
 /**
- * Consumes {@link DoubleScoreOutput} into an output file in NetCDF format.
- * 
- * @author jesse
- * @author james.brown@hydrosolved.com
- * @version 0.1
- * @since 1.0
+ * Consumes {@link DoubleScoreOutput} and writes one or more NetCDF files.
  */
 
-public class NetcdfDoubleScoreWriter extends NetcdfWriter<DoubleScoreOutput>
+public class NetcdfDoubleScoreWriter implements NetcdfWriter<DoubleScoreOutput>, AutoCloseable
 {
+    /** The Logger */
+    private static final Logger LOGGER = LoggerFactory.getLogger( NetcdfDoubleScoreWriter.class );
+
+    /** The file name prefix to use if project name is not specified */
+    private static final String DEFAULT_FILE_NAME = "WRES_SCORE_METRICS";
+
+
+    /**
+     * List of output files to write.
+     */
+
+    private final List<NetcdfFileWriter> files;
+
+
+    /**
+     * Use static utility methods "of", etc. for construction.
+     * @param projectConfig the project configuration
+     * @param featureCount the count of features to write (netCDF lib needs it)
+     * @param timeStepCount the count of times to write (netCDF lib needs it)
+     * @param leadCount the count of leads to write (netCDF lib needs it)
+     * @param thresholdCount the count of thresholds to write (netCDF lib needs it)
+     * @param metrics the metric names to write (netCDF lib needs it)
+     * @throws ProjectConfigException if the project configuration is not valid for writing
+     */
+
+    private NetcdfDoubleScoreWriter( ProjectConfig projectConfig,
+                                     int featureCount,
+                                     int timeStepCount,
+                                     int leadCount,
+                                     int thresholdCount,
+                                     List<String> metrics )
+            throws ProjectConfigException, IOException
+    {
+        WriterHelper.validateProjectForWriting( projectConfig );
+        this.files = NetcdfDoubleScoreWriter.initializeFiles( projectConfig,
+                                                              featureCount,
+                                                              timeStepCount,
+                                                              leadCount,
+                                                              thresholdCount,
+                                                              metrics );
+    }
+
 
     /**
      * Returns a writer of {@link DoubleScoreOutput}.
      * 
      * @param projectConfig the project configuration
      * @return a writer
+     * @param featureCount the count of features to write (netCDF lib needs it)
+     * @param timeStepCount the count of times to write (netCDF lib needs it)
+     * @param leadCount the count of lead times to write (netCDF lib needs it)
+     * @param thresholdCount the count of thresholds to write (netCDF lib needs it)
+     * @param metrics the metric names to write (netCDF lib needs it)
      * @throws ProjectConfigException if the project configuration is not valid for writing
+     * @throws IOException when creation or mutation of the netcdf file fails
      */
 
-    public static NetcdfDoubleScoreWriter of( ProjectConfig projectConfig ) throws ProjectConfigException
+    public static NetcdfDoubleScoreWriter of( ProjectConfig projectConfig,
+                                              int featureCount,
+                                              int timeStepCount,
+                                              int leadCount,
+                                              int thresholdCount,
+                                              List<String> metrics )
+            throws ProjectConfigException, IOException
     {
-        return new NetcdfDoubleScoreWriter( projectConfig );
+        return new NetcdfDoubleScoreWriter( projectConfig,
+                                            featureCount,
+                                            timeStepCount,
+                                            leadCount,
+                                            thresholdCount,
+                                            metrics );
     }
+
 
     /**
      * Consumes a map of {@link DoubleScoreOutput} into a NetCDF file.
@@ -48,7 +121,6 @@ public class NetcdfDoubleScoreWriter extends NetcdfWriter<DoubleScoreOutput>
         // Iterate through the metrics
         for ( final Entry<MapKey<MetricConstants>, MetricOutputMapByTimeAndThreshold<DoubleScoreOutput>> e : output.entrySet() )
         {
-
             // This is the metric to write
             MetricConstants myMetricToWrite = e.getKey().getKey();
             
@@ -72,20 +144,275 @@ public class NetcdfDoubleScoreWriter extends NetcdfWriter<DoubleScoreOutput>
             // process-by-feature processing works.
 
             // TODO: mutate the netcdf file
+            for ( NetcdfFileWriter writer : this.getFiles() )
+            {
+                try
+                {
+                    LOGGER.info( "Writing to netCDF..." );
+                    Variable ncVariable = writer.findVariable( myMetricToWrite.toString() );
+
+                    if ( ncVariable != null )
+                    {
+                        NetcdfDoubleScoreWriter.writeMetric( writer,
+                                                             myMetricToWrite,
+                                                             e.getValue() );
+                    }
+
+                    // Flush probably not necessary, but helpful for debugging
+                    writer.flush();
+                }
+                catch ( IOException ioe )
+                {
+                    // Because java 8's function-land has trouble with checked
+                    // Exceptions, use one of our unchecked ones. Feel free to
+                    // change which unchecked Exception this is.
+                    throw new WriteException( "Failed to write to NetCDF file.",
+                                              ioe );
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void close()
+            throws IOException
+    {
+        for ( NetcdfFileWriter writer : this.getFiles() )
+        {
+            writer.close();
+        }
+    }
+
+
+    /**
+     * Get the list of underlying files this writer is responsible for.
+     * It is not anticipated to become public or protected or package-private.
+     * @return the netcdf files
+     */
+
+    private List<NetcdfFileWriter> getFiles()
+    {
+        return this.files;
+    }
+
+
+    /**
+     * Create and set up dimensions of the NetCDF files for a given project.
+     * @param config the project to set up files for
+     * @param featureCount the count of features to write (netCDF lib needs it)
+     * @param timeStepCount the count of times to write (netCDF lib needs it)
+     * @param leadCount the count of leads to write (netCDF lib needs it),
+     * @param thresholdCount the count of thresholds to write (netCDF lib needs it)
+     * @param metrics the metric names to write (netCDF lib needs it)
+     * @return the list of files (possibly empty)
+     * @throws IOException when something goes wrong when creating or writing
+     * @throws NullPointerException when ProjectConfig is null
+     */
+
+    private static List<NetcdfFileWriter> initializeFiles( ProjectConfig config,
+                                                           int featureCount,
+                                                           int timeStepCount,
+                                                           int leadCount,
+                                                           int thresholdCount,
+                                                           List<String> metrics )
+            throws IOException
+    {
+        Objects.requireNonNull( config );
+        int count = NetcdfDoubleScoreWriter.countNetcdfOutputFiles( config );
+        List<NetcdfFileWriter> fileWriters = new ArrayList<>( count );
+
+        if ( config.getOutputs() == null
+             || config.getOutputs().getDestination() == null )
+        {
+            // Return an empty list when there are no destinations.
+            return Collections.unmodifiableList( fileWriters );
+        }
+
+        String projectName = config.getName();
+
+        if ( projectName == null )
+        {
+            projectName = NetcdfDoubleScoreWriter.DEFAULT_FILE_NAME;
+        }
+
+        for ( DestinationConfig destinationConfig : config.getOutputs().getDestination() )
+        {
+            if ( destinationConfig.getType().equals( DestinationType.NETCDF ) )
+            {
+                NetcdfFileWriter writer =
+                        NetcdfFileWriter.createNew( NetcdfFileWriter.Version.netcdf3,
+                                                    destinationConfig.getPath()
+                                                    + "/" + projectName + ".nc" );
+                NetcdfDoubleScoreWriter.setDimensionsAndVariables( config,
+                                                                   writer,
+                                                                   featureCount,
+                                                                   timeStepCount,
+                                                                   leadCount,
+                                                                   thresholdCount,
+                                                                   metrics );
+                writer.create();
+                // Flush may not be needed, but helpful for debugging.
+                writer.flush();
+                fileWriters.add( writer );
+            }
+        }
+
+        return Collections.unmodifiableList( fileWriters );
+    }
+
+
+    /**
+     * Given a project config and the writer to use, set up the dimensions of
+     * the netcdf file.
+     * @param config the project config, not null
+     * @param writer the writer to use to mutate the netcdf file, in define mode
+     * @param featureCount the count of features to write (netCDF lib needs it)
+     * @param timeStepCount the count of times to write (netCDF lib needs it)
+     * @param leadCount the count of leads to write (netCDF lib needs it)
+     * @param thresholdCount the count of thresholds to write (netCDF lib needs it)
+     * @param metrics the metric names to write (netCDF lib needs it)
+     * @throws IllegalStateException when writer is not in define mode
+     * @throws NullPointerException when any arg is null
+     */
+
+    private static void setDimensionsAndVariables( ProjectConfig config,
+                                                   NetcdfFileWriter writer,
+                                                   int featureCount,
+                                                   int timeStepCount,
+                                                   int leadCount,
+                                                   int thresholdCount,
+                                                   List<String> metrics )
+    {
+        Objects.requireNonNull( config );
+        Objects.requireNonNull( writer );
+
+        if ( !writer.isDefineMode() )
+        {
+            throw new IllegalStateException( "The writer must be in define mode" );
+        }
+
+        Dimension poolDimension = writer.addUnlimitedDimension( "pool" );
+        Dimension featureDimension = writer.addDimension( null,
+                                                          "station",
+                                                          featureCount );
+        Dimension timeDimension = writer.addDimension( null,
+                                                       "time",
+                                                       timeStepCount );
+        Dimension leadSecondsDimension = writer.addDimension( null,
+                                                              "lead_seconds",
+                                                              leadCount );
+        Dimension thresholdDimension = writer.addDimension( null,
+                                                            "threshold",
+                                                            thresholdCount );
+
+        List<Dimension> featureDimensions = new ArrayList<>( 1 );
+        featureDimensions.add( featureDimension );
+        List<Dimension> shareableFeatureDimensions =
+                Collections.unmodifiableList( featureDimensions );
+        Variable featureVariable = writer.addVariable( null,
+                                                       "station_id",
+                                                       DataType.INT,
+                                                       shareableFeatureDimensions );
+        Attribute featureNameAttribute = new Attribute( "long_name", "Station id" );
+        featureVariable.addAttribute( featureNameAttribute );
+
+        List<Dimension> timeDimensions = new ArrayList<>( 1 );
+        timeDimensions.add( timeDimension );
+        List<Dimension> shareableTimeDimensions =
+                Collections.unmodifiableList( timeDimensions );
+        Variable timeVariable = writer.addVariable( null,
+                                                    "time",
+                                                    DataType.INT,
+                                                    shareableTimeDimensions );
+        Attribute timeUnitsAttribute = new Attribute( "units", "seconds since 1970-01-01T00:00:00Z" );
+        timeVariable.addAttribute( timeUnitsAttribute );
+
+        List<Dimension> leadSecondsDimensions = new ArrayList<>( 1 );
+        leadSecondsDimensions.add( leadSecondsDimension );
+        List<Dimension> shareableLeadSecondsDimensions =
+                Collections.unmodifiableList( leadSecondsDimensions );
+        Variable leadSecondsVariable = writer.addVariable( null,
+                                                           "lead_seconds",
+                                                           DataType.INT,
+                                                           shareableLeadSecondsDimensions );
+        Attribute leadSecondsUnits = new Attribute( "units", "seconds" );
+        timeVariable.addAttribute( leadSecondsUnits );
+
+        List<Dimension> poolDimensions = new ArrayList<>( 6 );
+        poolDimensions.add( poolDimension );
+        poolDimensions.add( timeDimension );
+        poolDimensions.add( timeDimension );
+        poolDimensions.add( thresholdDimension );
+        poolDimensions.add( leadSecondsDimension );
+        poolDimensions.add( leadSecondsDimension );
+        List<Dimension> shareablePoolDimensions =
+                Collections.unmodifiableList( poolDimensions );
+        Variable poolVariable = writer.addVariable( null,
+                                                    "pool_id",
+                                                    DataType.INT,
+                                                    shareablePoolDimensions );
+
+        List<Dimension> scoreDimensions = new ArrayList<>( 2 );
+        scoreDimensions.add( poolDimension );
+        scoreDimensions.add( featureDimension );
+        List<Dimension> shareableScoreDimensions =
+                Collections.unmodifiableList( scoreDimensions );
+
+        // The actual values we care about
+        for ( String metricName : metrics )
+        {
+            Variable metricVariable = writer.addVariable( null,
+                                                          metricName,
+                                                          DataType.DOUBLE,
+                                                          shareableScoreDimensions );
         }
 
     }
 
+
     /**
-     * Hidden constructor.
-     * 
-     * @param projectConfig the project configuration
-     * @throws ProjectConfigException if the project configuration is not valid for writing
+     * Returns the count of output files required.
+     *
+     * @param config the project configuration
+     * @return the number of files required
      */
 
-    private NetcdfDoubleScoreWriter( ProjectConfig projectConfig ) throws ProjectConfigException
+    private static int countNetcdfOutputFiles( ProjectConfig config )
     {
-        super( projectConfig );
+        Objects.requireNonNull( config );
+
+
+        int countOfNetcdfOutputs = 0;
+
+        for ( DestinationConfig destinationConfig : config.getOutputs().getDestination() )
+        {
+            if ( destinationConfig.getType().equals( DestinationType.NETCDF ) )
+            {
+                countOfNetcdfOutputs++;
+            }
+        }
+
+        return countOfNetcdfOutputs;
     }
 
+    /**
+     * Write the values for a collected metric to a netcdf file
+     * @param writer the writer to write with
+     * @param id the name/id of the metric
+     * @param output the metric output map of double score outputs
+     * of the writer.
+     */
+    private static void writeMetric( NetcdfFileWriter writer,
+                                     MetricConstants id,
+                                     MetricOutputMapByTimeAndThreshold<DoubleScoreOutput> output )
+    {
+        Variable ncVariable = writer.findVariable( id.toString() );
+
+        if ( ncVariable == null )
+        {
+            throw new IllegalArgumentException( "Must set up NetCDF file to have variable " + id.toString() );
+        }
+
+    }
 }
