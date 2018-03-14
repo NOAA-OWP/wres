9ed62c2a2d0014009ef96790a2a45dbb27a6f64e
Jesse Bickel <jesse.bickel@noreply.gov>
2018-03-14 13:20:40 -0500

    A resolved project could become a simple representation of a project
    The evolution went from:
    1) only ProjectConfig
    to
    2) ProjectConfigPlus (to solve configuration of graphgen)
    to
    3) ProjectConfigPlus with a human-specified name tag as identifier
    to
    4) ProjectConfigPlus with a machine-generated code as identifier
    to
    5) ProjectConfigPlus with a machine-generated code as identifier and also a
    machine-decomposed set of features
    
    ResolvedProject might simplify this into a single representation of the idea
    that we go from raw ProjectConfig + user inputs -> ProjectConfig, id, features,
    tags, and the system uses these resolved things from then on. Most of the time,
    hopefully the system can still get what is needed directly from the
    ProjectConfig. But for these other things, it is nice to have a single hook.
    
    Refs #46739
    Refs #45347

---
+++ src/wres/control/ResolvedProject.java
@@ -0,0 +1,118 @@
+package wres.control;
+
+import java.util.Map;
+import java.util.Set;
+
+import wres.config.generated.DestinationConfig;
+import wres.config.generated.Feature;
+import wres.config.generated.ProjectConfig;
+import wres.io.config.ProjectConfigPlus;
+
+/**
+ * Represents a project that has been "resolved", i.e. any kind of translation
+ * from the ProjectConfig into WRES that has happened can be captured here.
+ * The existence of this class is a possible solution to these discomforts:
+ * 1) decomposed features which are disconnected from the original config
+ * 2) project identity in the database (discovered post-ingest)
+ *
+ * Both the decomposed features and project identity are resolved outside of
+ * the original project config, and need a home for sharing. This class might
+ * be that home.
+ *
+ */
+class ResolvedProject
+{
+    private final ProjectConfigPlus projectConfigPlus;
+    private final Set<Feature> decomposedFeatures;
+    private final String projectIdentifier;
+
+    private ResolvedProject( ProjectConfigPlus projectConfigPlus,
+                             Set<Feature> decomposedFeatures,
+                             String projectIdentifier )
+    {
+        this.projectConfigPlus = projectConfigPlus;
+        this.decomposedFeatures = decomposedFeatures;
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    ResolvedProject of( ProjectConfigPlus projectConfigPlus,
+                        Set<Feature> decomposedFeatures,
+                        String projectIdentifier )
+    {
+        return new ResolvedProject( projectConfigPlus,
+                                    decomposedFeatures,
+                                    projectIdentifier );
+    }
+
+    /**
+     * This is just here to emphasize that we intentionally do not directly
+     * expose ProjectConfigPlus
+     * @return the ProjectConfigPlus
+     */
+    private ProjectConfigPlus getProjectConfigPlus()
+    {
+        return this.projectConfigPlus;
+    }
+
+    /**
+     * Get the ProjectConfig. ProjectConfig is used by most classes.
+     * @return the original project config as specified by the user
+     */
+
+    ProjectConfig getProjectConfig()
+    {
+        return this.getProjectConfigPlus()
+                   .getProjectConfig();
+    }
+
+
+    /**
+     * Get the decomposed features that the system calculated from the given
+     * project config.
+     * In the future, hopefully we can figure out a different representation for
+     * feature besides the object that implies something came from a config.
+     * Once we figure that out, the return type will change.
+     * @return a Set of features resolved from the project
+     */
+
+    Set<Feature> getDecomposedFeatures()
+    {
+        return this.decomposedFeatures;
+    }
+
+
+    /**
+     * Get the wres identifier generated for this project.
+     * As of 2018-03-14 this is anticipated to be the md5sum generated at the
+     * end of ingest that incorporates both the project configuration and the
+     * sources that were actually ingested for that project configuration.
+     * This identifier is a hook that the database can use to figure out which
+     * actual, full project is being referred to. There will be many identifiers
+     * possible for a given project config:
+     * 1) A source could change
+     * 2) A source could be present on one run and absent on another
+     * @return the wres-generated project identifier
+     */
+
+    String getProjectIdentifier()
+    {
+        if ( projectIdentifier == null )
+        {
+            throw new UnsupportedOperationException( "Might not be implemented" );
+        }
+        return this.projectIdentifier;
+    }
+
+
+    /**
+     * The graphics module needs to get its specific xml snippets from the
+     * project config.
+     * @return a Map from a DestinationConfig to a String
+     */
+
+    Map<DestinationConfig, String> getGraphicsStrings()
+    {
+        return this.getProjectConfigPlus()
+                   .getGraphicsStrings();
+    }
+}
