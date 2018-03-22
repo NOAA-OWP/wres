762d34aeb25d1258e10958d093aae258ab8b7243
Jesse Bickel <jesse.bickel@noreply.gov>
2018-03-22 14:07:24 -0500

    Simplify SQL task runner
    Leaves it to the caller to decide what to do with any SQLExceptions.
    
    Refs #39057

--- wres-io/src/wres/io/concurrency/SQLExecutor.java
+++ wres-io/src/wres/io/concurrency/SQLExecutor.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import wres.io.utilities.Database;
-import wres.util.Strings;
 
 /**
  * A thread that will execute a passed in SQL script
@@ -23,58 +22,22 @@ public class SQLExecutor extends WRESRunnable
 	 */
 	public SQLExecutor(String script) {
 		this.script = script;
-		this.displayErrors = true;
 		this.forceTransaction = false;
 	}
 
-	/**
-	 * Creates the thread with the passed in SQL script with the option to
-	 * display log errors in the database or to log them without
-	 * necessarily displaying them
-	 * @param script The script to execute
-	 * @param displayErrors Determines if errors occuring in the database
-	 *                      should be logged/displayed as errors or logged as
-	 *                      debug records
-	 */
-	public SQLExecutor(String script, boolean displayErrors)
-	{
-		this.script = script;
-		this.displayErrors = displayErrors;
-		this.forceTransaction = false;
-	}
-
-	public SQLExecutor(String script, boolean displayErrors, boolean forceTransaction)
+    public SQLExecutor( String script, boolean forceTransaction )
     {
         this.script = script;
-        this.displayErrors = displayErrors;
         this.forceTransaction = forceTransaction;
     }
 
 	@Override
     public void execute() throws SQLException
     {
-		try
-		{
-            Database.execute( this.script, forceTransaction );
-		}
-		catch (SQLException e)
-		{
-			// We allow the hiding of errors on the screen on the rare
-			// occasion where failing in the database is acceptable
-			if (this.displayErrors)
-			{
-				this.getLogger().error( Strings.getStackTrace( e ) );
-				throw e;
-			}
-			else
-			{
-				this.getLogger().debug( Strings.getStackTrace( e ) );
-			}
-		}
+        Database.execute( this.script, forceTransaction );
 	}
 
 	private final String script;
-	private final boolean displayErrors;
 	private final boolean forceTransaction;
 
 	@Override
