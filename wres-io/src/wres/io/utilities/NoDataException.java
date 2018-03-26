2ff9dbd555c10c52fe27fff88c7d57b623a64ffb
Jesse Bickel <jesse.bickel@noreply.gov>
2018-03-26 17:24:22 -0500

    Document NoDataException
    Refs #39057

--- wres-io/src/wres/io/utilities/NoDataException.java
+++ wres-io/src/wres/io/utilities/NoDataException.java
@@ -2,6 +2,12 @@ package wres.io.utilities;
 
 import java.io.IOException;
 
+/**
+ * A temporary placeholder exception to be thrown when data is not found.
+ * As distinct from a SQLException which the database client library throws.
+ * Long run, since no data is a common every day occurrence, we should handle
+ * no data without having any exceptions thrown.
+ */
 public class NoDataException extends IOException
 {
     public NoDataException( String message )
