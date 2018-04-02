8f059b293653ac34ce5eb1f7ff43e5fe5dc90258
Jesse Bickel <jesse.bickel@noreply.gov>
2018-04-02 16:46:57 -0500

    Emit system information on start of the application
    This is somewhat pruned already, but could probably be pruned more. It is nice
    to have the -D options displayed, which vary based on the libraries we use. For
    example, logback can use a -D, or wres can use a -D, so it seems like we would
    want to be selectively removing rather than selectively adding.
    
    The most important things are the java runtime versions, operating system
    versions and the -D options.
    
    The idea is to be able to distinguish some of the platforms we run on, e.g.
    virtual machines, docker containers, bare metal, etc.
    
    Refs #45854

--- src/wres/Version.java
+++ src/wres/Version.java
@@ -1,5 +1,9 @@
 package wres;
 
+import java.util.SortedSet;
+import java.util.StringJoiner;
+import java.util.TreeSet;
+
 /**
  * Used to retrieve the version of the WRES software (top-level, wres jar)
  */
@@ -56,5 +60,47 @@ public class Version
     private Package getRawPackage()
     {
         return this.rawPackage;
+    }
+
+
+    /**
+     * Get a lot of information about the system, including processors, jvm ram,
+     * jvm system properties. Omit passphrases and classpaths.
+     * @return lots of runtime information
+     */
+
+    public String getVerboseRuntimeDescription()
+    {
+        StringJoiner s = new StringJoiner( "; " );
+
+        Runtime runtime = Runtime.getRuntime();
+
+        final String MIB = "MiB";
+        final long MEGABYTE = 1024 * 1024;
+        s.add( "Processors: " + runtime.availableProcessors() );
+        s.add( "Max Memory: " + ( runtime.maxMemory() / MEGABYTE ) + MIB );
+        s.add( "Free Memory: " + ( runtime.freeMemory() / MEGABYTE ) + MIB );
+        s.add( "Total Memory: " +  ( runtime.totalMemory() / MEGABYTE ) + MIB );
+
+        // Order the property names for consistency.
+        SortedSet<String> sortedPropertyNames =
+                new TreeSet<>( System.getProperties().stringPropertyNames() );
+
+        for ( String propertyName : sortedPropertyNames )
+        {
+            String lowerCaseName = propertyName.toLowerCase();
+
+            // Avoid printing passphrases or passwords or passes of any kind,
+            // avoid printing full classpath, ignore separators, ignore printers
+            if ( !lowerCaseName.contains( "pass" )
+                 && !lowerCaseName.contains( "class.path" )
+                 && !lowerCaseName.contains( "separator" )
+                 && !lowerCaseName.contains( "printer" ) )
+            {
+                s.add( propertyName + ": " + System.getProperty( propertyName ) );
+            }
+        }
+
+        return s.toString();
     }
 }
