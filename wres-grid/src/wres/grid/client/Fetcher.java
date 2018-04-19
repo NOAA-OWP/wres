9c83fafcb93d414ff507970d1a609bf61ad5a533
Chris Tubbs <christopher.tubbs@noreply.gov>
2018-04-19 15:09:00 -0500

    Added the grid module to the IO layer and added a few clarifications to the grid module additions (they are only stand-ins)

--- wres-grid/src/wres/grid/client/Fetcher.java
+++ wres-grid/src/wres/grid/client/Fetcher.java
@@ -2,6 +2,9 @@ package wres.grid.client;
 
 import java.io.IOException;
 
+/**
+ * Stand in module for integration development between IO and the Grid module
+ */
 public class Fetcher
 {
     public static Response getData(Request request) throws IOException
@@ -12,6 +15,11 @@ public class Fetcher
         return response;
     }
 
+    /**
+     * Stand-in function for creating fake response data
+     * @param request
+     * @return A response object with fake data
+     */
     private static TimeSeriesResponse fakeOutResponseData(Request request)
     {
         TimeSeriesResponse response = new TimeSeriesResponse();
