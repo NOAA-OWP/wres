36f455e69d19972e6d5d824e0324ea95378ec5db
Jesse Bickel <jesse.bickel@noreply.gov>
2018-04-23 13:07:52 -0500

    Remove unused imports

--- wres-tasker/src/wres/tasker/WresJob.java
+++ wres-tasker/src/wres/tasker/WresJob.java
@@ -1,7 +1,6 @@
 package wres.tasker;
 
 import java.io.IOException;
-import java.sql.Time;
 import java.util.Random;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
@@ -9,7 +8,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
