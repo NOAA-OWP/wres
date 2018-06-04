6b8feb7624315b2274b241c106351a65775c6c1c
Sanian Gaffar <sgaffar@noreply.gov>
2018-06-04 13:22:17 -0500

    Updated griddedReader to loop over list of files

--- wres-grid/src/wres/io/griddedReader/GriddedReader.java
+++ wres-grid/src/wres/io/griddedReader/GriddedReader.java
@@ -1,8 +1,14 @@
 package wres.io.griddedReader;
 
+import org.apache.commons.lang3.tuple.Pair;
+import ucar.nc2.Attribute;
+import ucar.nc2.NetcdfFile;
 import wres.config.generated.*;
+import wres.grid.client.*;
+import wres.config.FeaturePlus;
 
 import java.io.IOException;
+import java.util.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,18 +20,26 @@ import ucar.nc2.dt.GridCoordSystem;
 
 public class GriddedReader
 {
-    private GriddedPath filePath;
+
     private int x_index;
     private int y_index;
     private GriddedCoordinate coordinate;
     private String variable_name;
     private String fileName;
 
+    private List<Pair<Integer, Integer>> indices = new ArrayList<>();
+    private List<String> paths = new ArrayList<>();
+    private List<Feature> features = new ArrayList<>();
+
+    protected TimeSeriesResponse timeSeriesResponse;
+
+
+
     private static final Logger LOGGER = LoggerFactory.getLogger (GriddedReader.class);
 
-    public GriddedReader ( GriddedPath filePath, int x_index, int y_index, GriddedCoordinate coordinate, String variable_name, String fileName)
+    public GriddedReader ( int x_index, int y_index, GriddedCoordinate coordinate, String variable_name, String fileName)
     {
-        this.filePath= filePath;
+
         this.x_index = x_index;
         this.y_index= y_index;
         this.coordinate= coordinate;
@@ -39,8 +53,14 @@ public class GriddedReader
             this.fileName= fileName;
     }
 
-    public GriddedPath getFilePath() {
-        return filePath;
+    public List<String> getPaths () {return paths;}
+
+    public List<Pair<Integer, Integer>> getIndices() {
+        return indices;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
     }
 
     public int getX_index()
@@ -63,43 +83,83 @@ public class GriddedReader
         return variable_name;
     }
 
-    public void getData(String fileName )
-    {
-        GridDataset gds= null;
-        try
-        {
-            gds = ucar.nc2.dt.grid.GridDataset.open( fileName );
-        }
-        catch (IOException ioe)
-        {
-            LOGGER.info(ioe.toString());
-        }
+    NetcdfFile ncfile = null;
+    Attribute timeAtt1, timeAtt2;
+    String issuenceTime, validTime;
 
-        // -----------------------------------------------------------------------
+    public TimeSeriesResponse getData( List<String> paths )
+    {
+        for (String filePath:paths)
+        {
+
+            try
+            {
+                ncfile = NetcdfFile.open(filePath);
+            }
+            catch (IOException ioe)
+            {
+                LOGGER.info(ioe.toString());
+            }
+
+            timeAtt1 = ncfile.findGlobalAttribute("model_initialization_time");
+            issuenceTime = timeAtt1.getStringValue();
+            //timeSeriesResponse.issuedDate.add (issuenceTime);
+
+            timeAtt2 = ncfile.findGlobalAttribute("model_output_valid_time");
+            validTime = timeAtt2.getStringValue();
+            //timeSeriesResponse.lastLead.add (validTime);
+
+            try {
+                ncfile.close();
+            }
+
+            catch (IOException ioe)
+            {
+                LOGGER.info(ioe.toString());
+            }
+
+            GridDataset gds = null;
+            try
+            {
+                gds = ucar.nc2.dt.grid.GridDataset.open(filePath);
+            }
+            catch (IOException ioe)
+            {
+                LOGGER.info(ioe.toString());
+            }
+
+            // -----------------------------------------------------------------------
 // GridCoordSystem to find the value of a grid a a specific lat, lon point
 
-        GridDatatype grid = gds.findGridDatatype( variable_name );
-        GridCoordSystem gcs = null;
-        gcs= grid.getCoordinateSystem();
+            GridDatatype grid = gds.findGridDatatype(variable_name);
+            GridCoordSystem gcs = null;
+            gcs = grid.getCoordinateSystem();
 
-        // find the x,y index for a specific lat/lon position
-        int[] xy = gcs.findXYindexFromLatLon( x_index, y_index, null ); // xy[0] = x, xy[1] = y
+            // find the x,y index for a specific lat/lon position
+
+            Array data = null;
+
+            int[] xy = gcs.findXYindexFromLatLon(x_index, y_index, null); // xy[0] = x, xy[1] = y
+
 
 // read the data at that lat, lon and the first time and z level (if any)
-        Array data= null;
-        try
-        {
-            data = grid.readDataSlice(0, 0, xy[1], xy[0]); // note order is t, z, y, x
+
+            try
+            {
+                data = grid.readDataSlice(0, 0, xy[1], xy[0]); // note order is t, z, y, x
+            }
+            catch (IOException ioe)
+            {
+                LOGGER.info(ioe.toString());
+            }
+
+            double val = data.getDouble(0); // we know its a scalar
+            //timeSeriesResponse.value.add ( val );
+
+            //LOGGER.info("Value at %f %f == %f%n", x_index, y_index, val);
         }
-        catch (IOException ioe)
-        {
-            LOGGER.info(ioe.toString());
-        }
 
-        double val = data.getDouble(0 ); // we know its a scalar
-        LOGGER.info ( "Value at %f %f == %f%n", x_index, y_index, val );
-
-
+        return timeSeriesResponse;
     }
 
 
