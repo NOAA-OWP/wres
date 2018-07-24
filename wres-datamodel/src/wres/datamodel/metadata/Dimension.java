6f24b156fc6d503c77878d850e7e7c762b5c3f09
James Brown <james.d.brown@noreply.gov>
2018-07-24 14:36:24 +0100

    Continues to remove unnecessary abstractions from the wres-datamodel.
    Refs #52859
    Refs #52939

--- wres-datamodel/src/wres/datamodel/metadata/Dimension.java
+++ wres-datamodel/src/wres/datamodel/metadata/Dimension.java
@@ -1,18 +1,28 @@
 package wres.datamodel.metadata;
 
+import java.util.Objects;
+
 import wres.datamodel.inputs.MetricInput;
 import wres.datamodel.outputs.MetricOutput;
 
 /**
- * A class that stores the dimension associated with a {@link MetricInput} or a {@link MetricOutput}.
+ * A class that stores the dimension associated with a {@link MetricInput} or a {@link MetricOutput}. A dimension 
+ * corresponds to the unit of measurement associated with a constant or a variable. Examples of measurement units
+ * include "millimeter", and "cubic feet per second".
+ * 
+ * TODO: replace this simplistic abstraction with a more sophisticated representation, probably based on javax.measure,
+ * which allows for a full description, as well as transformations between units.
  * 
  * @author james.brown@hydrosolved.com
- * @version 0.1
- * @since 0.1
  */
 
-public interface Dimension extends Comparable<Dimension>
+public class Dimension implements Comparable<Dimension>
 {
+
+    /**
+     * The dimension.
+     */
+    private final String dimension; 
 
     /**
      * Returns true if the metric data has an explicit dimension, false if it is dimensionless.
@@ -20,7 +30,10 @@ public interface Dimension extends Comparable<Dimension>
      * @return true if the metric data has an explicit dimension, false otherwise
      */
     
-    public boolean hasDimension();
+    public boolean hasDimension()
+    {
+        return !"DIMENSIONLESS".equals( dimension );
+    }
     
     /**
      * Returns the named dimension.
@@ -28,5 +41,62 @@ public interface Dimension extends Comparable<Dimension>
      * @return the named dimension
      */
 
-    public String getDimension();
+    public String getDimension()
+    {
+        return dimension;
+    }
+
+    /**
+     * Returns an instance from the input.
+     * 
+     * @param dimension the dimension
+     * @throws NullPointerException if the input is null
+     */
+
+    public static Dimension of( String dimension )
+    {
+        return new Dimension( dimension );
+    }
+    
+    @Override
+    public boolean equals( final Object o )
+    {
+        return o instanceof Dimension && ( (Dimension) o ).hasDimension() == hasDimension()
+               && ( (Dimension) o ).getDimension().equals( getDimension() );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( hasDimension(), dimension );
+    }
+
+    @Override
+    public String toString()
+    {
+        return getDimension();
+    }
+
+    @Override
+    public int compareTo( Dimension o )
+    {
+        Objects.requireNonNull( o, "Specify a non-null dimension to compare with this dimension." );
+
+        return dimension.compareTo( o.getDimension() );
+    }
+
+    /**
+     * Hidden constructor.
+     * 
+     * @param dimension the dimension
+     * @throws NullPointerException if the input is null
+     */
+
+    private Dimension( final String dimension )
+    {
+        Objects.requireNonNull( dimension, "Specify a non-null dimension string." );
+
+        this.dimension = dimension;
+    }
+    
 }
--- wres-datamodel/src/wres/datamodel/metadata/MetricOutputMetadata.java
+++ wres-datamodel/src/wres/datamodel/metadata/MetricOutputMetadata.java
@@ -6,31 +6,88 @@ import wres.datamodel.MetricConstants;
 import wres.datamodel.outputs.MetricOutput;
 
 /**
- * A class that stores the metadata associated with a {@link MetricOutput}.
+ * An immutable store of metadata associated with a {@link MetricOutput}.
  * 
  * @author james.brown@hydrosolved.com
  */
-public interface MetricOutputMetadata extends Metadata
+public class MetricOutputMetadata extends Metadata
 {
-  
+    /**
+     * The sample size.
+     */
+
+    private final int sampleSize;
+
+    /**
+     * The dimension associated with the input data from which the output was computed.
+     */
+
+    private final Dimension inputDim;
+
+    /**
+     * The metric identifier.
+     */
+
+    private final MetricConstants metricID;
+
+    /**
+     * The metric component identifier.
+     */
+
+    private final MetricConstants componentID;
+
+    /**
+     * Returns an instance from the inputs.
+     * 
+     * @param sampleSize the sample size
+     * @param outputDim the required output dimension
+     * @param inputDim the optional input dimension
+     * @param metricID the optional metric identifier
+     * @param componentID the optional metric component identifier or decomposition template
+     * @param identifier the optional dataset identifier
+     * @param timeWindow the optional time window
+     * @return a {@link MetricOutputMetadata} object
+     * @throws NullPointerException if the output dimension is null
+     */
+
+    public static MetricOutputMetadata of( int sampleSize,
+                                           Dimension outputDim,
+                                           Dimension inputDim,
+                                           MetricConstants metricID,
+                                           MetricConstants componentID,
+                                           DatasetIdentifier identifier,
+                                           TimeWindow timeWindow )
+    {
+        return new MetricOutputMetadata( sampleSize,
+                                         outputDim,
+                                         inputDim,
+                                         metricID,
+                                         componentID,
+                                         identifier,
+                                         timeWindow );
+    }
+
     /**
      * Returns <code>true</code> if the {@link #getMetricComponentID()} has been set, otherwise <code>false</code>.
      * 
      * @return true if the metric component identifier is defined, otherwise false
      */
 
-    default boolean hasMetricComponentID()
+    public boolean hasMetricComponentID()
     {
         return Objects.nonNull( getMetricComponentID() );
     }
-    
+
     /**
      * Returns an identifier associated with the metric that produced the output.
      * 
      * @return the metric identifier
      */
 
-    MetricConstants getMetricID();
+    public MetricConstants getMetricID()
+    {
+        return metricID;
+    }
 
     /**
      * Returns an optional identifier associated with the component of the metric to which the output corresponds or 
@@ -40,8 +97,11 @@ public interface MetricOutputMetadata extends Metadata
      * @return the component identifier or null
      */
 
-    MetricConstants getMetricComponentID();
-    
+    public MetricConstants getMetricComponentID()
+    {
+        return componentID;
+    }
+
     /**
      * Returns the dimension associated with the metric input, which may differ from the output. The output dimension
      * is returned by {@link #getDimension()}.
@@ -49,15 +109,21 @@ public interface MetricOutputMetadata extends Metadata
      * @return the dimension
      */
 
-    Dimension getInputDimension();
-    
+    public Dimension getInputDimension()
+    {
+        return inputDim;
+    }
+
     /**
      * Returns the sample size associated with the {@link MetricOutput}.
      * 
      * @return the sample size
      */
 
-    int getSampleSize();    
+    public int getSampleSize()
+    {
+        return sampleSize;
+    }
 
     /**
      * <p>
@@ -76,6 +142,84 @@ public interface MetricOutputMetadata extends Metadata
      * @return true if the mandatory elements match, false otherwise
      */
 
-    boolean minimumEquals(MetricOutputMetadata meta);
+    public boolean minimumEquals( MetricOutputMetadata meta )
+    {
+        return meta.getMetricID() == getMetricID()
+               && meta.getMetricComponentID() == getMetricComponentID()
+               && meta.getDimension().equals( getDimension() )
+               && meta.getInputDimension().equals( getInputDimension() );
+    }
+
+    @Override
+    public boolean equals( final Object o )
+    {
+        if ( ! ( o instanceof MetricOutputMetadata ) )
+        {
+            return false;
+        }
+        final MetricOutputMetadata p = ( (MetricOutputMetadata) o );
+        boolean returnMe = super.equals( o ) && p.getSampleSize() == getSampleSize()
+                           && p.getInputDimension().equals( getInputDimension() );
+        return returnMe && p.getMetricID() == getMetricID()
+               && p.getMetricComponentID() == getMetricComponentID();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(),
+                             getSampleSize(),
+                             getMetricID(),
+                             getMetricComponentID(),
+                             getInputDimension() );
+    }
+
+    @Override
+    public String toString()
+    {
+        String start = super.toString();
+        start = start.substring( 0, start.length() - 1 ); // Remove bookend char, ']'
+        final StringBuilder b = new StringBuilder( start );
+        b.append( "," )
+         .append( inputDim )
+         .append( "," )
+         .append( sampleSize )
+         .append( "," )
+         .append( metricID )
+         .append( "," )
+         .append( componentID )
+         .append( "]" );
+        return b.toString();
+    }
+
+    /**
+     * Hidden constructor.
+     * 
+     * @param sampleSize the sample size
+     * @param outputDim the required output dimension
+     * @param inputDim the optional input dimension
+     * @param metricID the optional metric identifier
+     * @param componentID the optional metric component identifier or decomposition template
+     * @param identifier the optional dataset identifier
+     * @param timeWindow the optional time window
+     * @return a {@link MetricOutputMetadata} object
+     * @throws NullPointerException if the output dimension is null
+     */
+
+    private MetricOutputMetadata( int sampleSize,
+                                  Dimension outputDim,
+                                  Dimension inputDim,
+                                  MetricConstants metricID,
+                                  MetricConstants componentID,
+                                  DatasetIdentifier identifier,
+                                  TimeWindow timeWindow )
+    {
+        super( outputDim, identifier, timeWindow );
+
+        this.sampleSize = sampleSize;
+        this.inputDim = inputDim;
+        this.componentID = componentID;
+        this.metricID = metricID;
+    }
 
 }
