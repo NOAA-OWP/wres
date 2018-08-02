70b8de409fe8d108f01375eeb6bb817e7b51e1d4
Chris Tubbs <christopher.tubbs@noreply.gov>
2018-08-02 13:48:33 -0500

    Removed obsolete sql scripts.

--- wres-io/dist/SQL/operating_member.sql
+++
@@ -1,8 +0,0 @@
--- Enumeration: Operating Member
-
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'operating_member') THEN
-	CREATE TYPE operating_member AS ENUM ('left', 'right', 'baseline');
-    END IF;
-END $$;
