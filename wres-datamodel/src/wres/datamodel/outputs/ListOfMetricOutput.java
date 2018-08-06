b569f5a74e989eabbe0768a4deec6855d282cf32
James Brown <james.d.brown@noreply.gov>
2018-08-06 16:07:18 +0100

    Allow for a ListOfMetricOutput to be built incrementally using a thread-safe builder.
    Refs #51796

--- wres-datamodel/src/wres/datamodel/outputs/ListOfMetricOutput.java
+++ wres-datamodel/src/wres/datamodel/outputs/ListOfMetricOutput.java
@@ -1,14 +1,17 @@
 package wres.datamodel.outputs;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import wres.datamodel.metadata.MetricOutputMetadata;
 
 /**
- * An immutable list of {@link MetricOutput}.
+ * An immutable list of {@link MetricOutput}. A thread-safe builder is included to build a {@link ListOfMetricOutput}
+ * incrementally.
  * 
  * @param <T> the metric output type
  * @author james.brown@hydrosolved.com
@@ -36,6 +39,8 @@ public class ListOfMetricOutput<T extends MetricOutput<?>> implements Iterable<T
      * @param outputs the outputs used to populate the list
      * @param metadata the output metadata
      * @return an instance of the container
+     * @throws NullPointerException if either input is null
+     * @throws MetricOutputException if the outputs contain one or more null entries
      */
 
     public static <T extends MetricOutput<?>> ListOfMetricOutput<T> of( List<T> outputs, MetricOutputMetadata metadata )
@@ -120,16 +125,88 @@ public class ListOfMetricOutput<T extends MetricOutput<?>> implements Iterable<T
         
         return b.toString();
     }
+    
+    /**
+     * A thread-safe builder that allows for the incremental construction of a {@link ListOfMetricOutput} using one or 
+     * more threads.
+     */
+    
+    public static class ListOfMetricOutputBuilder<T extends MetricOutput<?>>
+    {
+        
+        /**
+         * The metadata.
+         */
+        
+        private MetricOutputMetadata metadata;
+        
+        /**
+         * The thread-safe queue of metric outputs.
+         */
+        
+        private ConcurrentLinkedQueue<T> outputs = new ConcurrentLinkedQueue<>(); 
+        
+        /**
+         * Sets the metadata.
+         * 
+         * @param metadata the metadata
+         * @return the builder
+         */
+        
+        public ListOfMetricOutputBuilder<T> setMetadata( MetricOutputMetadata metadata )
+        {
+            this.metadata = metadata;
+            
+            return this;
+        }
+        
+        /**
+         * Adds an output to the list.
+         * 
+         * @param output the output to add
+         * @return the builder
+         */
+        
+        public ListOfMetricOutputBuilder<T> addOutput( T output )
+        {
+            outputs.add( output );
+
+            return this;
+        }
+        
+        /**
+         * Return the container.
+         * 
+         * @return the list of outputs
+         */
+        
+        public ListOfMetricOutput<T> build()
+        {
+            return ListOfMetricOutput.of( Collections.unmodifiableList( new ArrayList<>( outputs ) ), metadata );
+        }
+        
+    }
 
     /**
      * Hidden constructor.
      * 
      * @param outputs the outputs used to populate the list
      * @param metadata the output metadata
+     * @throws NullPointerException if either input is null
+     * @throws MetricOutputException if the outputs contain one or more null entries
      */
 
     private ListOfMetricOutput( List<T> outputs, MetricOutputMetadata metadata )
     {
+        Objects.requireNonNull( outputs, "Specify a non-null list of outputs." );
+        
+        Objects.requireNonNull( metadata, "Specify non-null metadata for the outputs." );
+        
+        if( outputs.contains( null ) )
+        {
+            throw new MetricOutputException( "Cannot build a list of outputs with one or more null entries." );
+        }
+        
         this.outputs = Collections.unmodifiableList( outputs );
         this.metadata = metadata;
     }
