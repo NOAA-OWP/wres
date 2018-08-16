04afd6da2c3d4d2dd3ded6c869f74190daa56a82
Jesse Bickel <jesse.bickel@noreply.gov>
2018-08-16 14:25:32 -0500

    Run a WRES project when posted to the project service endpoint
    Refs #54143

--- src/wres/server/ProjectService.java
+++ src/wres/server/ProjectService.java
@@ -9,6 +9,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import wres.config.ProjectConfigPlus;
+import wres.control.Control;
+import wres.control.InternalWresException;
+import wres.control.UserInputException;
 
 /**
  * Accepts projects in the body of http request and executes them one at a time
@@ -26,16 +29,27 @@ public class ProjectService
     {
         try
         {
-            ProjectConfigPlus.from( rawProjectConfig, "a web request" );
+            ProjectConfigPlus projectPlus = ProjectConfigPlus.from( rawProjectConfig,
+                                                                    "a web request" );
+            Control control = new Control();
+            control.accept( projectPlus );
         }
-        catch ( IOException ioe )
+        catch ( IOException | UserInputException e )
         {
             return Response.status( 400 )
                            .entity( "I received something I could not parse. The top-level exception was: "
-                                    + ioe.getMessage() )
+                                    + e.getMessage() )
                            .build();
         }
-        return Response.ok( "I received some xml and successfully parsed it. Xml looked like this:"
+        catch ( InternalWresException iwe )
+        {
+            return Response.status( 500 )
+                           .entity( "WRES experienced an internal issue. The top-level exception was: "
+                             + iwe.getMessage() )
+                           .build();
+        }
+
+        return Response.ok( "I received a project, and successfully ran it. The config looked like this:"
                             + System.lineSeparator()
                             + rawProjectConfig )
                        .build();
