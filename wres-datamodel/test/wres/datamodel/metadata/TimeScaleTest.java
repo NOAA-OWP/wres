7b272a807938433bbae1157e83e49b4481c8e214
James Brown <james.d.brown@noreply.gov>
2018-08-30 19:37:16 +0100

    Qualifies the PNG products with information about the time scale of the data used to produce the statistics.
    Refs #44544

--- wres-datamodel/test/wres/datamodel/metadata/TimeScaleTest.java
+++ wres-datamodel/test/wres/datamodel/metadata/TimeScaleTest.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.assertTrue;
 import java.time.Duration;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import wres.config.generated.DurationUnit;
 import wres.config.generated.TimeScaleConfig;
@@ -23,6 +25,9 @@ import wres.datamodel.metadata.TimeScale.TimeScaleFunction;
 public final class TimeScaleTest
 {
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
     /**
      * Common instance for use in multiple tests.
      */
@@ -32,7 +37,7 @@ public final class TimeScaleTest
     @BeforeClass
     public static void setupBeforeAllTests()
     {
-        timeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.AVG );
+        timeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MEAN );
     }
 
     /**
@@ -52,7 +57,9 @@ public final class TimeScaleTest
     @Test
     public void testGetFunctionReturnsExpectedFunction()
     {
-        assertEquals( timeScale.getFunction(), TimeScaleFunction.AVG );
+        assertEquals( timeScale.getFunction(), TimeScaleFunction.MEAN );
+        
+        assertEquals( TimeScale.of( Duration.ofSeconds( 1 ) ).getFunction(), TimeScaleFunction.UNKNOWN );        
     }
 
     /**
@@ -63,14 +70,14 @@ public final class TimeScaleTest
     public void testConstructionWithConfigProducesExpectedTimeScale()
     {
         TimeScaleConfig first =
-                new TimeScaleConfig( wres.config.generated.TimeScaleFunction.AVG, 1, null, DurationUnit.DAYS, null );
+                new TimeScaleConfig( wres.config.generated.TimeScaleFunction.MEAN, 1, null, DurationUnit.DAYS, null );
 
-        assertEquals( TimeScale.of( first ), TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.AVG ) );
+        assertEquals( TimeScale.of( first ), TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MEAN ) );
 
         TimeScaleConfig second =
-                new TimeScaleConfig( wres.config.generated.TimeScaleFunction.MAX, 1, null, DurationUnit.HOURS, null );
+                new TimeScaleConfig( wres.config.generated.TimeScaleFunction.MAXIMUM, 1, null, DurationUnit.HOURS, null );
 
-        assertEquals( TimeScale.of( second ), TimeScale.of( Duration.ofHours( 1 ), TimeScaleFunction.MAX ) );
+        assertEquals( TimeScale.of( second ), TimeScale.of( Duration.ofHours( 1 ), TimeScaleFunction.MAXIMUM ) );
 
         // Null function produces TimeScaleFunction.UNKNOWN
         TimeScaleConfig third =
@@ -90,11 +97,11 @@ public final class TimeScaleTest
         assertEquals( timeScale, timeScale );
 
         // Symmetric
-        TimeScale otherTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.AVG );
+        TimeScale otherTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MEAN );
         assertEquals( timeScale, otherTimeScale );
 
         // Transitive, noting that timeScale and otherTimeScale are equal above
-        TimeScale oneMoreTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.AVG );
+        TimeScale oneMoreTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MEAN );
         assertEquals( otherTimeScale, oneMoreTimeScale );
         assertEquals( timeScale, oneMoreTimeScale );
 
@@ -108,11 +115,11 @@ public final class TimeScaleTest
         assertThat( timeScale, not( equalTo( null ) ) );
 
         // Unequal on period
-        TimeScale unequalOnPeriod = TimeScale.of( Duration.ofDays( 3 ), TimeScaleFunction.AVG );
+        TimeScale unequalOnPeriod = TimeScale.of( Duration.ofDays( 3 ), TimeScaleFunction.MEAN );
         assertThat( timeScale, not( equalTo( unequalOnPeriod ) ) );
 
         // Unequal on function
-        TimeScale unequalOnFunction = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MIN );
+        TimeScale unequalOnFunction = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MINIMUM );
         assertThat( timeScale, not( equalTo( unequalOnFunction ) ) );
     }
 
@@ -124,7 +131,7 @@ public final class TimeScaleTest
     public void testHashcode()
     {
         // Consistent with equals 
-        TimeScale otherTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.AVG );
+        TimeScale otherTimeScale = TimeScale.of( Duration.ofDays( 1 ), TimeScaleFunction.MEAN );
 
         assertEquals( timeScale.hashCode(), otherTimeScale.hashCode() );
 
@@ -141,6 +148,35 @@ public final class TimeScaleTest
     @Test
     public void testToString()
     {
-        assertEquals( timeScale.toString(), "[PT24H,AVG]" );
+        assertEquals( timeScale.toString(), "[PT24H,MEAN]" );
     }
+    
+    /**
+     * Tests for an expected exception when attempting to build a {@link TimeScale} that has a zero period.
+     */
+    
+    @Test
+    public void testConstructionThrowsExceptionWhenPeriodIsZero()
+    {
+        exception.expect( IllegalArgumentException.class );
+        
+        exception.expectMessage( "Cannot build a time scale with a period of zero." );
+        
+        TimeScale.of( Duration.ZERO );
+    }
+    
+    /**
+     * Tests for an expected exception when attempting to build a {@link TimeScale} that has a negative period.
+     */
+    
+    @Test
+    public void testConstructionThrowsExceptionWhenPeriodIsNegative()
+    {
+        exception.expect( IllegalArgumentException.class );
+        
+        exception.expectMessage( "Cannot build a time scale with a negative period." );
+        
+        TimeScale.of( Duration.ofSeconds( -100 ) );
+    }
+    
 }
