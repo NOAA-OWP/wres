6f9f66a1b4db9cab14c175135d660b53b5eda12f
Raymond Chui <raymond.chui@noreply.gov>
2018-11-21 08:42:46 -0500

    created new JUnit test directory

---
+++ systests/JUnit_Test/src/test/java/Scenario003.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.nio.file.*;
+
+public class Scenario003 {
+    @Test public void runtest() {
+	AllScenarios classUnderTest = new AllScenarios();
+	classUnderTest.runTest("scenario003");
+    }
+}
