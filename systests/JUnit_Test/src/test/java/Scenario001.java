0b88ae09e069c60e05c38b240899c00fc0bc46a9
Raymond Chui <raymond.chui@noreply.gov>
2018-12-14 16:42:50 +0000

    renamed the runTest method

--- systests/JUnit_Test/src/test/java/Scenario001.java
+++ systests/JUnit_Test/src/test/java/Scenario001.java
@@ -4,7 +4,6 @@ import java.nio.file.*;
 
 public class Scenario001 {
     @Test public void runtest() {
-	//AllScenarios scenario001 = new AllScenarios().runTest("scenario001");
-	new AllScenarios().runTest("scenario001");
+	new AllScenarios().runScenarioTest("scenario001");
     }
 }
