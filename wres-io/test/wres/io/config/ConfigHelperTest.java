a3eeed6faf4a7a300d9a592099f5ec31a176cae8
James Brown <james.d.brown@noreply.gov>
2018-12-28 11:55:43 +0000

    Asserts an expected set of time windows equal to those in system test scenario403 and increments the corresponding helper to achieve that expectation.
    Refs #56213

--- wres-io/test/wres/io/config/ConfigHelperTest.java
+++ wres-io/test/wres/io/config/ConfigHelperTest.java
@@ -72,16 +72,17 @@ public class ConfigHelperTest
     /**
      * <p>Tests the {@link ConfigHelper#getTimeWindowsFromProjectConfig(wres.config.generated.ProjectConfig)}
      * where the project declaration includes a <code>leadHours</code> and a 
-     * <code>leadTimesPoolingWindow</code>.
+     * <code>leadTimesPoolingWindow</code>. Expects twenty-four time windows with
+     * prescribed characteristics.
      * 
      * <p>This test scenario is analogous to system test scenario017 as of commit 
      * 766c6d0b4ad96f191bcafb8f2a357c0f2e6a2d3c
      */
 
     @Test
-    public void testGetTimeWindowsWithLeadHoursAndLeadTimesPoolingWindow()
+    public void testGetTimeWindowsWithLeadHoursAndLeadTimesPoolingWindowReturnsTwentyFourWindows()
     {
-        // Mock the PairConfig, which is sufficient for this test
+        // Mock the sufficient elements of the ProjectConfig
         IntBoundsType leadBoundsConfig = new IntBoundsType( 1, 24 );
         PoolingWindowConfig leadTimesPoolingWindowConfig =
                 new PoolingWindowConfig( 1, null, DurationUnit.HOURS );
@@ -120,4 +121,55 @@ public class ConfigHelperTest
         assertEquals( expectedTimeWindows, actualTimeWindows );
     }
 
+    /**
+     * <p>Tests the {@link ConfigHelper#getTimeWindowsFromProjectConfig(wres.config.generated.ProjectConfig)}
+     * where the project declaration includes a <code>leadHours</code> and a 
+     * <code>leadTimesPoolingWindow</code>. Expects two time windows with
+     * prescribed characteristics.
+     * 
+     * <p>This test scenario is analogous to system test scenario403 as of commit 
+     * 766c6d0b4ad96f191bcafb8f2a357c0f2e6a2d3c
+     */
+
+    @Test
+    public void testGetTimeWindowsWithLeadHoursAndLeadTimesPoolingWindowReturnsTwoWindows()
+    {
+        // Mock the sufficient elements of the ProjectConfig
+        IntBoundsType leadBoundsConfig = new IntBoundsType( 1, 48 );
+        PoolingWindowConfig leadTimesPoolingWindowConfig =
+                new PoolingWindowConfig( 24, null, DurationUnit.HOURS );
+        PairConfig pairsConfig = new PairConfig( null,
+                                                 null,
+                                                 null,
+                                                 leadBoundsConfig,
+                                                 null,
+                                                 null,
+                                                 null,
+                                                 null,
+                                                 null,
+                                                 null,
+                                                 leadTimesPoolingWindowConfig,
+                                                 null,
+                                                 null );
+        ProjectConfig mockedConfig =
+                new ProjectConfig( null,
+                                   pairsConfig,
+                                   null,
+                                   null,
+                                   null,
+                                   null );
+
+        // Generate the expected windows
+        Set<TimeWindow> expectedTimeWindows = new HashSet<>( 2 );
+        expectedTimeWindows.add( TimeWindow.of( Duration.ofHours( 0 ), Duration.ofHours( 24 ) ) );
+        expectedTimeWindows.add( TimeWindow.of( Duration.ofHours( 24 ), Duration.ofHours( 48 ) ) );
+        
+        // Generate the actual windows
+        Set<TimeWindow> actualTimeWindows = ConfigHelper.getTimeWindowsFromProjectConfig( mockedConfig );
+
+        // Assert that the expected and actual are equal
+        assertEquals( expectedTimeWindows, actualTimeWindows );
+    }
+
+    
 }
