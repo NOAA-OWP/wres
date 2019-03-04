d16838e67dc115d058c05270ea3258cee5a57e47
Jesse Bickel <jesse.bickel@noreply.gov>
2019-03-04 22:39:49 +0000

    Only use environment variables when system properties not set
    Refs #51654

--- systests/dist/src/test/java/wres/systests/SystemTestScenario001.java
+++ systests/dist/src/test/java/wres/systests/SystemTestScenario001.java
@@ -1,21 +1,35 @@
 package wres.systests;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import wres.io.Operations;
 
 public class SystemTestScenario001
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger( SystemTestScenario001.class );
+    private static final String SCENARIO_NAME = "scenario001";
+    private static final String NEWLINE = System.lineSeparator();
+
+    @Before
+    public void beforeEachIndividualTest() throws IOException, SQLException
+    {
+        LOGGER.info( "{}{}{}{}", NEWLINE, "########################################################## EXECUTION ", SCENARIO_NAME, NEWLINE );
+        Operations.cleanDatabase();
+        SystestsScenarioRunner.deleteOldOutputDirectories( Paths.get( SCENARIO_NAME ) );
+    }
+
     @Test
     public void testScenario()
     {
-        String scenarioName = "scenario001";
-        System.out.println( );
-        System.out.println("########################################################## EXECUTION " + scenarioName);
-        System.out.println( );
-
-        SystestsScenarioRunner classUnderTest = new SystestsScenarioRunner( scenarioName );
-
-        SystestsScenarioRunner.assertCleanDatabase();
-        classUnderTest.assertDeletionOfOldOutputDirectories();
+        SystestsScenarioRunner classUnderTest = new SystestsScenarioRunner( SCENARIO_NAME );
         classUnderTest.assertProjectExecution();
         classUnderTest.assertOutputsMatchBenchmarks();
     }
