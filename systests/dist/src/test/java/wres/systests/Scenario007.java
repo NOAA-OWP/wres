c40f1c4fd9543e6d51ad724372ac5caee63f623c
Raymond Chui <raymond.chui@noreply.gov>
2019-03-26 15:56:45 +0000

    remove dirListing.txt from comparison

--- systests/dist/src/test/java/wres/systests/Scenario007.java
+++ systests/dist/src/test/java/wres/systests/Scenario007.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import wres.io.Operations;
-
+import wres.control.Control;
 public class Scenario007
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( Scenario007.class );
@@ -29,29 +29,23 @@ public class Scenario007
                                               .getSimpleName()
                                               .toLowerCase(),
                                           baseDirectory );
-        LOGGER.info( "####>> Cleaning the database..." );
+		LOGGER.info( "####>> Cleaning the database..." );
         Operations.cleanDatabase();
-        
-        LOGGER.info( "####>> Removing any existing old output directories..." );
-        ScenarioHelper.deleteOldOutputDirectories( scenarioInfo.getScenarioDirectory() );
-        
-        LOGGER.info( "####>> Setting properties for run based on user settings..." );
+        //ScenarioHelper.deleteOldOutputDirectories( scenarioInfo.getScenarioDirectory() );
         ScenarioHelper.setAllPropertiesFromEnvVars( scenarioInfo );
     }
 
     @Test
     public void testScenario()
     {
-        LOGGER.info( "####>> Beginning test execution..." );
-        ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
         
         //This method does it based on a file listing of the output directory.
         //The other choice can work if you have a Control available, in which case
         //you can get the output paths from the Control via its get method.
-        LOGGER.info( "####>> Assert outputs match benchmarks..." );
-        ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo );
+        //LOGGER.info( "####>> Assert outputs match benchmarks..." );
+        ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
         LOGGER.info( "########################################################## COMPLETED "
                 + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
     }
 }
-
