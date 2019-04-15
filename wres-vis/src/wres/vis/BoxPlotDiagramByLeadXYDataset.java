33ed1ccc7da37819e63445afc07bde950bea80ac
James Brown <james.d.brown@noreply.gov>
2019-04-15 21:56:31 +0100

    Adds a missing source file.
    Refs #62374

---
+++ wres-vis/src/wres/vis/BoxPlotDiagramByLeadXYDataset.java
@@ -0,0 +1,59 @@
+package wres.vis;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import org.jfree.data.xy.XYDataset;
+
+import wres.datamodel.statistics.BoxPlotStatistics;
+import wres.util.TimeHelper;
+
+/**
+ * The {@link XYDataset} for use in building the reliability diagram portion of the reliability diagram plot (the other
+ * being the sample size portion).
+ * 
+ * @author Hank.Herr
+ */
+public class BoxPlotDiagramByLeadXYDataset extends BoxPlotDiagramXYDataset
+{
+
+    private static final long serialVersionUID = 2818742289181492762L;
+    
+    /**
+     * The duration units.
+     */
+    
+    private final ChronoUnit durationUnits;    
+
+    /**
+     * Build the data type with an input source and the type of duration units.
+     * 
+     * @param input the input data
+     * @param durationUnits the duration units
+     * @throws NullPointerException if either input is null
+     */
+    
+    public BoxPlotDiagramByLeadXYDataset( BoxPlotStatistics input, ChronoUnit durationUnits )
+    {
+        super( input );
+        
+        Objects.requireNonNull( durationUnits );
+        
+        this.durationUnits = durationUnits;
+    }
+
+    @Override
+    public Number getX( final int series, final int item )
+    {
+        // Time in prescribed units for the specified item number of the single dataset        
+        return TimeHelper.durationToLongUnits( this.getPlotData()
+                                               .getData()
+                                               .get( item )
+                                               .getMetadata()
+                                               .getSampleMetadata()
+                                               .getTimeWindow()
+                                               .getLatestLeadDuration(),
+                                           this.durationUnits );
+    }
+
+}
