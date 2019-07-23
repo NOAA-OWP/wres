22264ede7f67ece007f60bedca8f56435fb135e3
Jesse Bickel <jesse.bickel@noreply.gov>
2019-07-23 10:31:40 -0500

    Use a mock server with a fixed CA rather than 3rd party live servers
    Fixes #66282

--- wres-system/test/wres/system/SSLStuffThatTrustsOneCertificateTest.java
+++ wres-system/test/wres/system/SSLStuffThatTrustsOneCertificateTest.java
@@ -12,26 +12,57 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.Ignore;
-import org.junit.Rule;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.mockserver.integration.ClientAndServer;
 
 import static junit.framework.TestCase.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 
 public class SSLStuffThatTrustsOneCertificateTest
 {
 
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
+    private static ClientAndServer mockServer;
+
+    /**
+     * From https://github.com/jamesdbloom/mockserver/blob/master/mockserver-core/src/main/resources/org/mockserver/socket/CertificateAuthorityCertificate.pem
+     * which was mentioned in https://www.mock-server.com/ as automatically
+     * being set up for any SSL connections to the server.
+     */
+
+    private static final String MOCK_SERVER_CA_PEM_2019 =
+            "-----BEGIN CERTIFICATE-----\n"
+            + "MIIDqDCCApCgAwIBAgIEPhwe6TANBgkqhkiG9w0BAQsFADBiMRswGQYDVQQDDBJ3\n"
+            + "d3cubW9ja3NlcnZlci5jb20xEzARBgNVBAoMCk1vY2tTZXJ2ZXIxDzANBgNVBAcM\n"
+            + "BkxvbmRvbjEQMA4GA1UECAwHRW5nbGFuZDELMAkGA1UEBhMCVUswIBcNMTYwNjIw\n"
+            + "MTYzNDE0WhgPMjExNzA1MjcxNjM0MTRaMGIxGzAZBgNVBAMMEnd3dy5tb2Nrc2Vy\n"
+            + "dmVyLmNvbTETMBEGA1UECgwKTW9ja1NlcnZlcjEPMA0GA1UEBwwGTG9uZG9uMRAw\n"
+            + "DgYDVQQIDAdFbmdsYW5kMQswCQYDVQQGEwJVSzCCASIwDQYJKoZIhvcNAQEBBQAD\n"
+            + "ggEPADCCAQoCggEBAPGORrdkwTY1H1dvQPYaA+RpD+pSbsvHTtUSU6H7NQS2qu1p\n"
+            + "sE6TEG2fE+Vb0QIXkeH+jjKzcfzHGCpIU/0qQCu4RVycrIW4CCdXjl+T3L4C0I3R\n"
+            + "mIMciTig5qcAvY9P5bQAdWDkU36YGrCjGaX3QlndGxD9M974JdpVK4cqFyc6N4gA\n"
+            + "Onys3uS8MMmSHTjTFAgR/WFeJiciQnal+Zy4ZF2x66CdjN+hP8ch2yH/CBwrSBc0\n"
+            + "ZeH2flbYGgkh3PwKEqATqhVa+mft4dCrvqBwGhBTnzEGWK/qrl9xB4mTs4GQ/Z5E\n"
+            + "8rXzlvpKzVJbfDHfqVzgFw4fQFGV0XMLTKyvOX0CAwEAAaNkMGIwHQYDVR0OBBYE\n"
+            + "FH3W3sL4XRDM/VnRayaSamVLISndMA8GA1UdEwEB/wQFMAMBAf8wCwYDVR0PBAQD\n"
+            + "AgG2MCMGA1UdJQQcMBoGCCsGAQUFBwMBBggrBgEFBQcDAgYEVR0lADANBgkqhkiG\n"
+            + "9w0BAQsFAAOCAQEAecfgKuMxCBe/NxVqoc4kzacf9rjgz2houvXdZU2UDBY3hCs4\n"
+            + "MBbM7U9Oi/3nAoU1zsA8Rg2nBwc76T8kSsfG1TK3iJkfGIOVjcwOoIjy3Z8zLM2V\n"
+            + "YjYbOUyAQdO/s2uShAmzzjh9SV2NKtcNNdoE9e6udvwDV8s3NGMTUpY5d7BHYQqV\n"
+            + "sqaPGlsKi8dN+gdLcRbtQo29bY8EYR5QJm7QJFDI1njODEnrUjjMvWw2yjFlje59\n"
+            + "j/7LBRe2wfNmjXFYm5GqWft10UJ7Ypb3XYoGwcDac+IUvrgmgTHD+E3klV3SUi8i\n"
+            + "Gm5MBedhPkXrLWmwuoMJd7tzARRHHT6PBH/ZGw==\n"
+            + "-----END CERTIFICATE-----";
 
     /**
      * The intermediate certificate found that signed hc.apache.org certificate
-     * as of 2018-11-15, will expire 2024-07-03. They will probably change certs
-     * before then. Feel free to ignore the test if it starts failing due to
-     * either a certificate change on https://hc.apache.org or you are reading
-     * this in 2024-07.
+     * as of 2018-11-15, will expire 2024-07-03. In 2024 it would be wise to
+     * auto-generate a certificate here instead of using this old apache cert
+     * otherwise the test will not be testing what is intended, instead it will
+     * test that an expired certificate throws an SSLHandShakeException which
+     * should always be true. What we are looking for is that exactly one cert
+     * (intermediate or CA) is trusted, the exact one specified.
      */
     private static final String APACHE_INTERMEDIATE_PEM_2018 =
             "-----BEGIN CERTIFICATE-----\n"
@@ -69,12 +100,17 @@ public class SSLStuffThatTrustsOneCertificateTest
             + "+E3jECsl+ke/Q3dfswLjNl+as1U10Ltgx7/IJanbDEQzVr6Sqgs=\n"
             + "-----END CERTIFICATE-----\n";
 
+    @BeforeClass
+    public static void beforeAllTests()
+    {
+        SSLStuffThatTrustsOneCertificateTest.mockServer = ClientAndServer.startClientAndServer( 0 );
+    }
+
     @Test
-    @Ignore // #66282: update PEM or refactor to mock server with mock cert
-    public void attemptToReachApacheDotOrg() throws IOException
+    public void connectToServerWithCorrectCertificateTrustedSucceeds() throws IOException
     {
         byte[] certificateBytes =
-                APACHE_INTERMEDIATE_PEM_2018.getBytes( StandardCharsets.US_ASCII );
+                MOCK_SERVER_CA_PEM_2019.getBytes( StandardCharsets.US_ASCII );
         InputStream certificateStream =
                 new ByteArrayInputStream( certificateBytes );
 
@@ -93,7 +129,8 @@ public class SSLStuffThatTrustsOneCertificateTest
                                  .setSSLSocketFactory( connectionSocketFactory )
                                  .build() )
         {
-            HttpGet httpGet = new HttpGet( "https://hc.apache.org" );
+            HttpGet httpGet = new HttpGet( "https://localhost:"
+                                           + SSLStuffThatTrustsOneCertificateTest.mockServer.getLocalPort() );
             response = httpClient.execute( httpGet );
         }
 
@@ -102,7 +139,7 @@ public class SSLStuffThatTrustsOneCertificateTest
 
 
     @Test
-    public void attemptToReachGoogleWithApachesCertificate() throws IOException
+    public void connectToServerWithIncorrectCertificateTrustedThrowsHandshakeException() throws IOException
     {
         byte[] certificateBytes =
                 APACHE_INTERMEDIATE_PEM_2018.getBytes( StandardCharsets.US_ASCII );
@@ -117,21 +154,25 @@ public class SSLStuffThatTrustsOneCertificateTest
                                                 new String[] { "TLSv1.2" },
                                                 null,
                                                 SSLConnectionSocketFactory.getDefaultHostnameVerifier() );
-        HttpResponse response;
 
         try ( CloseableHttpClient httpClient =
                       HttpClients.custom()
                                  .setSSLSocketFactory( connectionSocketFactory )
                                  .build() )
         {
-            HttpGet httpGet = new HttpGet( "https://www.google.com" );
+            HttpGet httpGet = new HttpGet( "https://localhost:"
+                                           + SSLStuffThatTrustsOneCertificateTest.mockServer.getLocalPort() );
 
-            // Because the certificate is for apache's server, not google's,
+            // Because the certificate is for mockserver, not an old apache one,
             // we should expect a handshake failure.
-            exception.expect( SSLHandshakeException.class );
-            response = httpClient.execute( httpGet );
+            assertThrows( SSLHandshakeException.class, () -> { httpClient.execute( httpGet ); } );
         }
+    }
 
-        assertNull( "Response should not have been set because an exception should have occurred.", response );
+
+    @AfterClass
+    public static void afterAllTests()
+    {
+        SSLStuffThatTrustsOneCertificateTest.mockServer.stop();
     }
 }
