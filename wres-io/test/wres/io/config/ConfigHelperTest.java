03cd5b52f214ecfba4c9c417beaba9d199a2f2eb
James Brown <james.d.brown@noreply.gov>
2019-08-28 16:45:05 +0100

    Removing a toy/stub test committed by mistake.
    Refs #67774

--- wres-io/test/wres/io/config/ConfigHelperTest.java
+++ wres-io/test/wres/io/config/ConfigHelperTest.java
@@ -2,20 +2,12 @@ package wres.io.config;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import wres.config.generated.DataSourceConfig;
-import wres.config.generated.DataSourceConfig.Source;
-import wres.config.generated.DataSourceConfig.Variable;
-import wres.config.generated.DatasourceType;
 import wres.config.generated.DurationUnit;
-import wres.config.generated.Format;
-import wres.config.generated.ProjectConfig;
-import wres.config.generated.ProjectConfig.Inputs;
 
 public class ConfigHelperTest
 {
@@ -60,62 +52,6 @@ public class ConfigHelperTest
         Assert.assertEquals( "A null timeshift was not created.", null, timeshift );
 
     }
-    
-    @Test
-    public void testGetLeftOrRightOrBaselineWhenLeftAndRightDataSourceConfigAreEqual()
-    {
-        
-        DataSourceConfig left = new DataSourceConfig( DatasourceType.OBSERVATIONS,
-                                                      List.of( new Source( null,
-                                                                           Format.USGS,
-                                                                           null,
-                                                                           null,
-                                                                           null,
-                                                                           null,
-                                                                           false,
-                                                                           null ) ),
-                                                      new Variable( null, null, "ft3/s" ),
-                                                      null,
-                                                      null,
-                                                      null,
-                                                      null,
-                                                      null,
-                                                      "USGS" );
-
-        DataSourceConfig right = new DataSourceConfig( DatasourceType.OBSERVATIONS,
-                                                       List.of( new Source( null,
-                                                                            Format.USGS,
-                                                                            null,
-                                                                            null,
-                                                                            null,
-                                                                            null,
-                                                                            false,
-                                                                            null ) ),
-                                                       new Variable( null, null, "ft3/s" ),
-                                                       null,
-                                                       null,
-                                                       null,
-                                                       null,
-                                                       null,
-                                                       "USGS obs" );
-
-        Inputs inputs = new Inputs( left, right, null );
-
-        ProjectConfig mockedConfig = new ProjectConfig( inputs,
-                                                        null,
-                                                        null,
-                                                        null,
-                                                        null,
-                                                        null );
-
-        System.out.println( left.hashCode());
-        System.out.println( right.hashCode());
-        
-        
-        LeftOrRightOrBaseline expected = ConfigHelper.getLeftOrRightOrBaseline( mockedConfig, right );
-        
-    }
-    
     
 
 }
