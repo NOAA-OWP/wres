37c6f5a50aad1313a410808642eb295d3f501c40
Hank Herr <hank.herr@noreply.gov>
2019-09-23 20:21:42 +0000

    Implemented system test suite with random ordering; refs #69087

--- systests/dist/src/test/java/wres/systests/SystemTestSuite.java
+++ systests/dist/src/test/java/wres/systests/SystemTestSuite.java
@@ -3,12 +3,15 @@ package wres.systests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//@RunWith( Suite.class )
-//
-//@Suite.SuiteClasses( {
-//                       Scenario001.class,
-//                       Scenario003.class
-//} )
+@RunWith( SystemTestsSuiteRunner.class )
+
+@Suite.SuiteClasses( {
+                       Scenario001.class,
+                       Scenario003.class,
+                       Scenario100.class,
+                       Scenario200.class
+} )
+
 /**
  * Specify the suite of JUnit tests above.  The suite is executed in order.
  * @author Hank.Herr
