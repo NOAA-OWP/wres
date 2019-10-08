e8ec69ec0680f0f8371caadc31a472c8a8d108c2
Raymond Chui <raymond.chui@noreply.gov>
2019-10-08 19:33:54 +0000

    updated scripts/JUnit_systests.bash

--- systests/scripts/JUnit_systests.bash
+++ systests/scripts/JUnit_systests.bash
@@ -57,7 +57,6 @@ fi
 #echo "WRES_DB_PASSWORD = $WRES_DB_PASSWORD"
 #echo "TIMESTAMP = $TIMESTAMP"
 #$echo "LOGFILE = $LOGFILE"
-exit
 
 ./gradlew distZip 2>&1 | /usr/bin/tee --append $LOGFILE 
 REVISION=`/usr/bin/find build/distributions | /usr/bin/tail -1 | /usr/bin/gawk -F / '{print($NF)}'  | /usr/bin/sed -e s/wres-// | /usr/bin/tr -d '.zip'`
@@ -74,3 +73,17 @@ cd build/install/systests
 
 ./gradlew cleanTest test -PversionToTest=$REVISION -PwresZipDirectory=../../../../build/distributions -PtestJvmSystemProperties="-Dwres.useSSL=true -Dwres.username=$WRES_DB_USERNAME -Dwres.url=$WRES_DB_HOSTNAME -Dwres.databaseName=$WRES_DB_NAME -Dwres.dataDirectory=../../.. -Djava.io.tmpdir=./outputs" --tests="SystemTestSuite" -$WRES_LOG_LEVEL 2>&1 | /usr/bin/tee --append $LOGFILE
 #./gradlew cleanTest test -PversionToTest=$REVISION -PwresZipDirectory=../../../../build/distributions -PtestJvmSystemProperties="-Dwres.useSSL=true -Dwres.username=$WRES_DB_USERNAME -Dwres.url=$WRES_DB_HOSTNAME -Dwres.databaseName=$WRES_DB_NAME -Dwres.dataDirectory=../../.. -Dwres.password=$WRES_DB_PASSWORD -Djava.io.tmpdir=./outputs" --tests="SystemTestSuite" -$WRES_LOG_LEVEL 2>&1 | /usr/bin/tee --append $LOGFILE
+
+/usr/bin/grep FAILED $LOGFILE | /usr/bin/grep testScenario > failures.txt 
+/usr/bin/grep PASSED $LOGFILE | /usr/bin/grep testScenario > passes.txt 
+if [ -s failures.txt ]
+then
+	failure_nums=`/usr/bin/cat failures.txt | /usr/bin/wc -l`
+	/usr/bin/mailx -F -S smtp=140.90.91.135 -s "JUnit Test $REVISION $failure_nums FAILED" -a $LOGFILE Raymond.Chui@***REMOVED*** < failures.txt
+	rm -v failures.txt passes.txt
+elif [ -s passes.txt ]
+then
+	pass_nums=`/usr/bin/cat passes.txt | /usr/bin/wc -l`
+	/usr/bin/mailx -F -S smtp=140.90.91.135 -s "JUnit Test $REVISION $pass_nums PASSED" -a $LOGFILE Raymond.Chui@***REMOVED*** < passes.txt
+	rm -v passes.txt failures.txt
+fi
