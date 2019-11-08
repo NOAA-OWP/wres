a02bcd07866e15f7485fd7378bbff23317ee5f03
James Brown <james.d.brown@noreply.gov>
2019-11-08 12:29:05 +0000

    Turns on the retrieval of pools that maintain a time-series shape for the pairs they contain.
    Refs #56214

--- wres-io/src/wres/io/Operations.java
+++ wres-io/src/wres/io/Operations.java
@@ -147,7 +147,7 @@ public final class Operations {
 
         try
         {
-            project.prepareForExecution();
+            project.prepareForExecutionNew();
         }
         catch (CalculationException | SQLException exception)
         {
