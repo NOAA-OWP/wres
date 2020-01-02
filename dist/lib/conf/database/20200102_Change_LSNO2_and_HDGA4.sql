703df3f578604b1409bf785f10c3647f7deb6163
Jesse Bickel <jesse.bickel@noreply.gov>
2020-01-02 17:14:17 -0600

    Improve updates to LSNO2 and HDGA4
    Removes previously-orphaned (but invisible to projects) data.
    Less likely to fail in future changes due to new precondition.
    
    Refs #72786
    Refs #65216

--- dist/lib/conf/database/20200102_Change_LSNO2_and_HDGA4.sql
+++ dist/lib/conf/database/20200102_Change_LSNO2_and_HDGA4.sql
@@ -1,40 +1,40 @@
 -- First delete associated project and source rows (and cascade).
 
-delete from wres.project
+delete from wres.Project
 where project_id in
 (
     select project_id
-    from wres.projectsource
+    from wres.ProjectSource
     where source_id in
     (
         select distinct( source_id )
-        from wres.observation
+        from wres.Observation
         where variablefeature_id in
         (
             select distinct( variablefeature_id )
-            from wres.variablefeature
+            from wres.VariableFeature
             where feature_id in
             (
                 select feature_id
-                from wres.feature
+                from wres.Feature
                 where lid in ( 'HDGA4', 'LSNO2' )
             )
         )
         union
         select distinct( source_id )
-        from wres.timeseriessource
+        from wres.TimeSeriesSource
         where timeseries_id in
         (
             select timeseries_id
-            from wres.timeseries
+            from wres.TimeSeries
             where variablefeature_id in
             (
                 select variablefeature_id
-                from wres.variablefeature
+                from wres.VariableFeature
                 where feature_id in
                 (
                     select feature_id
-                    from wres.feature
+                    from wres.Feature
                     where lid in ( 'HDGA4', 'LSNO2' )
                 )
             )
@@ -43,37 +43,37 @@ where project_id in
 );
 
 delete
-from wres.source
+from wres.Source
 where source_id in
 (
     select distinct( source_id )
-    from wres.observation
+    from wres.Observation
     where variablefeature_id in
     (
         select distinct( variablefeature_id )
-        from wres.variablefeature
+        from wres.VariableFeature
         where feature_id in
         (
             select feature_id
-            from wres.feature
+            from wres.Feature
             where lid in ( 'HDGA4', 'LSNO2' )
         )
     )
     union
     select distinct( source_id )
-    from wres.timeseriessource
+    from wres.TimeSeriesSource
     where timeseries_id in
     (
         select timeseries_id
-        from wres.timeseries
+        from wres.TimeSeries
         where variablefeature_id in
         (
             select variablefeature_id
-            from wres.variablefeature
+            from wres.VariableFeature
             where feature_id in
             (
                 select feature_id
-                from wres.feature
+                from wres.Feature
                 where lid in ( 'HDGA4', 'LSNO2' )
             )
         )
@@ -81,13 +81,9 @@ where source_id in
 );
 
 -- Second, delete the feature rows (and cascade).
-delete from wres.feature
-where feature_id in
-(
-    select feature_id
-    from wres.feature
-    where lid in ( 'HDGA4', 'LSNO2' )
-);
+delete from wres.Feature
+where lid in ( 'HDGA4', 'LSNO2' );
+
 
 -- Third: create new feature rows.
 insert into wres.Feature
@@ -95,3 +91,19 @@ insert into wres.Feature
 values
 ( 21773045, 'LSNO2', '07190400', 'ABRFC', 'OK', '11070209', 'Neosho River (spillway) near Langley, OK', 36.4375, -95.0455555555555555555556 ),
 ( 8588002, 'HDGA4', '07049000', 'LMRFC', 'AR', '11010001', 'War Eagle Creek near Hindsville, AR', 36.2, -93.855 );
+
+
+-- Fourth: clean up orphaned data from removed sources
+delete from wres.TimeSeries
+where timeseries_id not in
+(
+    select timeseries_id
+    from wres.TimeSeriesSource
+);
+
+delete from wres.TimeSeriesValue
+where timeseries_id not in
+(
+    select timeseries_id
+    from wres.TimeSeries
+);
