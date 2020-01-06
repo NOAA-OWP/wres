237d3fbd5dbd0a2cd4d7430685e94ef1f21cd8cf
James Brown <james.d.brown@noreply.gov>
2020-01-06 12:49:41 +0000

    Assert that the expected file names match the actual file names (that exist) for system tests scenario4**.
    Refs #69423

--- systests/dist/src/test/java/wres/systests/Scenario404.java
+++ systests/dist/src/test/java/wres/systests/Scenario404.java
@@ -1,7 +1,13 @@
 package wres.systests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +22,20 @@ public class Scenario404
 
     private ScenarioInformation scenarioInfo;
 
+    /**
+     * Expected paths as file names.
+     */
+
+    private static final Set<Path> EXPECTED_FILE_NAMES =
+            Set.of( Path.of( "baseline_pairs.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.png" ),
+                    Path.of( "pairs.csv" ) );
+    
     @Before
     public void beforeIndividualTest() throws IOException, SQLException
     {
@@ -33,6 +53,24 @@ public class Scenario404
     public void testScenario()
     {
         Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        
+        // Collect the file names actually written and that exist
+        Set<Path> pathsWritten = control.get();
+        Set<Path> actualFileNamesThatExist = pathsWritten.stream()
+                                                         .filter( Files::exists )
+                                                         .map( Path::getFileName )
+                                                         .collect( Collectors.toSet() );
+
+        // Expected file-name paths equals actual
+        LOGGER.info( "Checking expected file names against actual file names that exist for {} files...",
+                     EXPECTED_FILE_NAMES.size() );
+
+        assertEquals( "The actual set of file names does not match the expected set of file names.",
+                      EXPECTED_FILE_NAMES,
+                      actualFileNamesThatExist );
+        
+        LOGGER.info( "Finished checking file names. The actual file names match the expected file names." );
+        
         ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
         LOGGER.info( "########################################################## COMPLETED "
                 + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
