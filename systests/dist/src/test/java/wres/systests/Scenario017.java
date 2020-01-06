e70e2e367bd932d85dbf500b10528d1be539b348
James Brown <james.d.brown@noreply.gov>
2020-01-06 12:00:21 +0000

    Assert that the expected file names match the actual file names (that exist) for system test scenario0**.
    Refs #69423

--- systests/dist/src/test/java/wres/systests/Scenario017.java
+++ systests/dist/src/test/java/wres/systests/Scenario017.java
@@ -1,7 +1,13 @@
 package wres.systests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +21,67 @@ public class Scenario017
     private static final String NEWLINE = System.lineSeparator();
 
     private ScenarioInformation scenarioInfo;
+    
+    /**
+     * Expected paths as file names.
+     */
+
+    private static final Set<Path> EXPECTED_FILE_NAMES =
+            Set.of( Path.of( "DRRC2_QINE_HEFS_BRIER_SCORE.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_BRIER_SCORE.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_MEAN_ERROR.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_MEAN_ERROR.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_10800_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_10800_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_14400_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_14400_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_18000_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_18000_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_21600_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_21600_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_25200_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_25200_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_28800_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_28800_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_32400_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_32400_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_36000_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_36000_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_3600_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_3600_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_39600_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_39600_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_43200_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_43200_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_46800_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_46800_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_50400_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_50400_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_54000_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_54000_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_57600_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_57600_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_61200_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_61200_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_64800_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_64800_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_68400_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_68400_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_72000_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_72000_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_7200_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_7200_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_75600_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_75600_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_79200_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_79200_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_82800_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_82800_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_86400_SECONDS.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_RELIABILITY_DIAGRAM_86400_SECONDS.png" ),
+                    Path.of( "DRRC2_QINE_HEFS_SAMPLE_SIZE.csv" ),
+                    Path.of( "DRRC2_QINE_HEFS_SAMPLE_SIZE.png" ),
+                    Path.of( "pairs.csv" ) );
 
     @Before
     public void beforeIndividualTest() throws IOException, SQLException
@@ -33,6 +100,24 @@ public class Scenario017
     public void testScenario()
     {
         Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        
+        // Collect the file names actually written and that exist
+        Set<Path> pathsWritten = control.get();
+        Set<Path> actualFileNamesThatExist = pathsWritten.stream()
+                                                         .filter( Files::exists )
+                                                         .map( Path::getFileName )
+                                                         .collect( Collectors.toSet() );
+
+        // Expected file-name paths equals actual
+        LOGGER.info( "Checking expected file names against actual file names that exist for {} files...",
+                     EXPECTED_FILE_NAMES.size() );
+
+        assertEquals( "The actual set of file names does not match the expected set of file names.",
+                      EXPECTED_FILE_NAMES,
+                      actualFileNamesThatExist );
+        
+        LOGGER.info( "Finished checking file names. The actual file names match the expected file names." );
+        
         ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
         LOGGER.info( "########################################################## COMPLETED "
                 + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
