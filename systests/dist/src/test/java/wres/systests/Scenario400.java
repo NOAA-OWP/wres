3a20e9f57b792b2b7324fe84d2d5457a8db14d95
James Brown <james.d.brown@noreply.gov>
2020-01-06 12:49:41 +0000

    Assert that the expected file names match the actual file names (that exist) for system tests scenario4**.
    Refs #69423

--- systests/dist/src/test/java/wres/systests/Scenario400.java
+++ systests/dist/src/test/java/wres/systests/Scenario400.java
@@ -1,7 +1,13 @@
 package wres.systests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +21,73 @@ public class Scenario400
     private static final String NEWLINE = System.lineSeparator();
 
     private ScenarioInformation scenarioInfo;
+    
+    /**
+     * Expected paths as file names.
+     */
+
+    private static final Set<Path> EXPECTED_FILE_NAMES =
+            Set.of( Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BIAS_FRACTION.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BIAS_FRACTION.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BOX_PLOT_OF_ERRORS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BOX_PLOT_OF_ERRORS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BOX_PLOT_OF_PERCENTAGE_ERRORS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_BOX_PLOT_OF_PERCENTAGE_ERRORS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_COEFFICIENT_OF_DETERMINATION.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_COEFFICIENT_OF_DETERMINATION.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_CONTINGENCY_TABLE_21600_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_CONTINGENCY_TABLE_43200_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_CONTINGENCY_TABLE_64800_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_CONTINGENCY_TABLE_86400_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_EQUITABLE_THREAT_SCORE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_EQUITABLE_THREAT_SCORE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_FREQUENCY_BIAS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_FREQUENCY_BIAS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_INDEX_OF_AGREEMENT.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_INDEX_OF_AGREEMENT.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_KLING_GUPTA_EFFICIENCY.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_KLING_GUPTA_EFFICIENCY.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ABSOLUTE_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ABSOLUTE_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE_NORMALIZED.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE_NORMALIZED.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_SQUARE_ERROR_SKILL_SCORE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEDIAN_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEDIAN_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PEARSON_CORRELATION_COEFFICIENT.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PEARSON_CORRELATION_COEFFICIENT.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PEIRCE_SKILL_SCORE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PEIRCE_SKILL_SCORE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PROBABILITY_OF_DETECTION.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PROBABILITY_OF_DETECTION.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PROBABILITY_OF_FALSE_DETECTION.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_PROBABILITY_OF_FALSE_DETECTION.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_21600_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_21600_SECONDS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_43200_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_43200_SECONDS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_64800_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_64800_SECONDS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_86400_SECONDS.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_QUANTILE_QUANTILE_DIAGRAM_86400_SECONDS.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_ROOT_MEAN_SQUARE_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_ROOT_MEAN_SQUARE_ERROR_NORMALIZED.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_ROOT_MEAN_SQUARE_ERROR_NORMALIZED.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_ROOT_MEAN_SQUARE_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SUM_OF_SQUARE_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SUM_OF_SQUARE_ERROR.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_THREAT_SCORE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_THREAT_SCORE.png" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_VOLUMETRIC_EFFICIENCY.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_VOLUMETRIC_EFFICIENCY.png" ),
+                    Path.of( "pairs.csv" ) );
 
     @Before
     public void beforeIndividualTest() throws IOException, SQLException
@@ -33,6 +106,24 @@ public class Scenario400
     public void testScenario()
     {
         Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        
+        // Collect the file names actually written and that exist
+        Set<Path> pathsWritten = control.get();
+        Set<Path> actualFileNamesThatExist = pathsWritten.stream()
+                                                         .filter( Files::exists )
+                                                         .map( Path::getFileName )
+                                                         .collect( Collectors.toSet() );
+
+        // Expected file-name paths equals actual
+        LOGGER.info( "Checking expected file names against actual file names that exist for {} files...",
+                     EXPECTED_FILE_NAMES.size() );
+
+        assertEquals( "The actual set of file names does not match the expected set of file names.",
+                      EXPECTED_FILE_NAMES,
+                      actualFileNamesThatExist );
+        
+        LOGGER.info( "Finished checking file names. The actual file names match the expected file names." );
+        
         ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
         LOGGER.info( "########################################################## COMPLETED "
                 + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
--- systests/dist/src/test/java/wres/systests/Scenario407.java
+++ systests/dist/src/test/java/wres/systests/Scenario407.java
@@ -1,7 +1,13 @@
 package wres.systests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +22,21 @@ public class Scenario407
 
     private ScenarioInformation scenarioInfo;
 
+    /**
+     * Expected paths as file names.
+     */
+
+    private static final Set<Path> EXPECTED_FILE_NAMES =
+            Set.of( Path.of( "ANTO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "ANTO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "ARCT2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "ARCT2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "BLUO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "BLUO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_MEAN_ERROR.csv" ),
+                    Path.of( "GLOO2_QINE_Operational_Single-Valued_Forecasts_SAMPLE_SIZE.csv" ),
+                    Path.of( "pairs.csv" ) ); 
+    
     @Before
     public void beforeIndividualTest() throws IOException, SQLException
     {
@@ -33,6 +54,24 @@ public class Scenario407
     public void testScenario()
     {
         Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        
+        // Collect the file names actually written and that exist
+        Set<Path> pathsWritten = control.get();
+        Set<Path> actualFileNamesThatExist = pathsWritten.stream()
+                                                         .filter( Files::exists )
+                                                         .map( Path::getFileName )
+                                                         .collect( Collectors.toSet() );
+
+        // Expected file-name paths equals actual
+        LOGGER.info( "Checking expected file names against actual file names that exist for {} files...",
+                     EXPECTED_FILE_NAMES.size() );
+
+        assertEquals( "The actual set of file names does not match the expected set of file names.",
+                      EXPECTED_FILE_NAMES,
+                      actualFileNamesThatExist );
+        
+        LOGGER.info( "Finished checking file names. The actual file names match the expected file names." );
+        
         ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
         LOGGER.info( "########################################################## COMPLETED "
                 + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
