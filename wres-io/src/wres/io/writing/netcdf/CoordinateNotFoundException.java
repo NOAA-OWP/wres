104c67dc083b39b03d25fd85dabfbe40ee0b43fb
James Brown <james.d.brown@noreply.gov>
2020-04-02 20:56:23 +0100

    Improves the exception message when failing to identify a coordinate during netcdf writing.
    Refs #76011

--- wres-io/src/wres/io/writing/netcdf/CoordinateNotFoundException.java
+++ wres-io/src/wres/io/writing/netcdf/CoordinateNotFoundException.java
@@ -5,8 +5,15 @@ public class CoordinateNotFoundException extends Exception
 
     private static final long serialVersionUID = 194816125215945933L;
 
-    public CoordinateNotFoundException(final String message)
+    public CoordinateNotFoundException( final String message )
     {
-        super(message);
+        super( message );
     }
+
+    public CoordinateNotFoundException( final String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+
+
 }
