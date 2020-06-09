bfd3f6c8288d463ff7835e28e977a47cf7f04c22
James Brown <james.d.brown@noreply.gov>
2020-06-09 12:10:46 +0100

    Adds a package for brokering messages connected to evaluation events.
    Refs #79280

---
+++ wres-eventsbroker/test/wres/eventsbroker/EmbeddedBrokerTest.java
@@ -0,0 +1,36 @@
+package wres.eventsbroker;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.apache.qpid.client.AMQConnection;
+
+/**
+ * Tests the {@link EmbeddedBroker}.
+ * 
+ * @author james.brown@hydrosolved.com
+ */
+
+public class EmbeddedBrokerTest
+{
+
+    @Test
+    public void testConnectionSucceeds() throws Exception
+    {
+        boolean connectionMade = false;
+
+        // Create and start the broker, clean up on completion
+        try ( EmbeddedBroker embeddedBroker = EmbeddedBroker.of(); )
+        {
+            embeddedBroker.start();
+
+            try ( AMQConnection connection =
+                    new AMQConnection( "amqp://guest:guest@test/?brokerlist='tcp://localhost:5673'" ); )
+            {
+                connectionMade = connection.isConnected();
+            }
+        }
+
+        assertTrue( connectionMade );
+    }
+
+}
