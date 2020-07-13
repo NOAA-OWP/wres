18ad6ef1f94fb49a0112ee166b9b4a0427bc8d4f
Chris Tubbs <christopher.tubbs@noreply.gov>
2020-07-13 10:07:26 -0500

    Added Scenario802 to the testing process
    refs #78628

---
+++ systests/dist/src/test/java/wres/systests/Scenario802.java
@@ -0,0 +1,90 @@
+package wres.systests;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wres.control.Control;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class Scenario802
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger( Scenario802.class );
+    private static final String NEWLINE = System.lineSeparator();
+
+    /**
+     * Expected paths as file names.
+     */
+
+    private static final Set<Path> EXPECTED_FILE_NAMES =
+            Set.of( Path.of( "CKLN6_STAGE_HEFS_CONTINGENCY_TABLE.csv" ),
+                    Path.of( "pairs.csv" ) );
+    
+    private ScenarioInformation scenarioInfo;
+    
+    /**
+     * Watch for any failed assertions and log them.
+     */
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher()
+    {
+        @Override
+        protected void failed( Throwable e, Description description )
+        {
+            LOGGER.error( description.toString(), e );
+        }
+    };
+
+    @Before
+    public void beforeIndividualTest() throws IOException, SQLException
+    {
+        LOGGER.info( "########################################################## EXECUTING "
+                     + this.getClass().getSimpleName().toLowerCase()
+                     + NEWLINE );
+        this.scenarioInfo = new ScenarioInformation( this.getClass()
+                                              .getSimpleName()
+                                              .toLowerCase(),
+                                              ScenarioHelper.getBaseDirectory() );
+        ScenarioHelper.logUsedSystemProperties( scenarioInfo );
+    }
+
+    @Test
+    public void testScenario()
+    {
+        Control control = ScenarioHelper.assertExecuteScenario( scenarioInfo );
+        
+        // Collect the file names actually written and that exist
+        Set<Path> pathsWritten = control.get();
+        Set<Path> actualFileNamesThatExist = pathsWritten.stream()
+                                                         .filter( Files::exists )
+                                                         .map( Path::getFileName )
+                                                         .collect( Collectors.toSet() );
+
+        // Expected file-name paths equals actual
+        LOGGER.info( "Checking expected file names against actual file names that exist for {} files...",
+                     EXPECTED_FILE_NAMES.size() );
+
+        assertEquals( "The actual set of file names does not match the expected set of file names.",
+                      EXPECTED_FILE_NAMES,
+                      actualFileNamesThatExist );
+        
+        LOGGER.info( "Finished checking file names. The actual file names match the expected file names." );
+        
+        ScenarioHelper.assertOutputsMatchBenchmarks( scenarioInfo, control );
+        LOGGER.info( "########################################################## COMPLETED "
+                + this.getClass().getSimpleName().toLowerCase() + NEWLINE);
+    }
+}
+
