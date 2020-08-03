a1efb92cc2899dd1a85f1ab2b4e0993589a7c2d3
Jesse Bickel <jesse.bickel@noreply.gov>
2020-08-03 16:58:34 -0500

    Let WRES read NWM 2.1 Puerto Rico short_range channel_rt data
    Refs #81454

--- wres-io/src/wres/io/reading/nwm/NWMProfiles.java
+++ wres-io/src/wres/io/reading/nwm/NWMProfiles.java
@@ -39,6 +39,46 @@ import wres.config.generated.InterfaceShortHand;
  * short_range_hawaii/            23-Oct-2019 14:58    -
  * usgs_timeslices/               23-Oct-2019 18:31    -
  *
+ * Survey of directories on para 2020-08-03
+ * Index of /pub/data/nccf/com/nwm/para/nwm.20200802
+ *
+ * Name                               Last modified      Size  Parent Directory                                        -
+ * analysis_assim/                    02-Aug-2020 23:48    -
+ * analysis_assim_extend/             03-Aug-2020 21:01    -
+ * analysis_assim_extend_no_da/       03-Aug-2020 21:00    -
+ * analysis_assim_hawaii/             03-Aug-2020 21:01    -
+ * analysis_assim_hawaii_no_da/       03-Aug-2020 00:40    -
+ * analysis_assim_long/               03-Aug-2020 21:02    -
+ * analysis_assim_long_no_da/         03-Aug-2020 21:00    -
+ * analysis_assim_no_da/              03-Aug-2020 21:00    -
+ * analysis_assim_puertorico/         03-Aug-2020 21:02    -
+ * analysis_assim_puertorico_no_da/   02-Aug-2020 23:58    -
+ * forcing_analysis_assim/            02-Aug-2020 23:33    -
+ * forcing_analysis_assim_extend/     02-Aug-2020 18:22    -
+ * forcing_analysis_assim_hawaii/     03-Aug-2020 00:25    -
+ * forcing_analysis_assim_puertorico/ 02-Aug-2020 23:45    -
+ * forcing_medium_range/              03-Aug-2020 21:17    -
+ * forcing_short_range/               03-Aug-2020 21:17    -
+ * forcing_short_range_hawaii/        02-Aug-2020 15:02    -
+ * forcing_short_range_puertorico/    03-Aug-2020 21:17    -
+ * long_range_mem1/                   03-Aug-2020 14:27    -
+ * long_range_mem2/                   03-Aug-2020 04:26    -
+ * long_range_mem3/                   03-Aug-2020 21:00    -
+ * long_range_mem4/                   03-Aug-2020 04:27    -
+ * medium_range_mem1/                 03-Aug-2020 21:17    -
+ * medium_range_mem2/                 03-Aug-2020 21:02    -
+ * medium_range_mem3/                 03-Aug-2020 01:40    -
+ * medium_range_mem4/                 03-Aug-2020 21:17    -
+ * medium_range_mem5/                 03-Aug-2020 21:03    -
+ * medium_range_mem6/                 03-Aug-2020 21:17    -
+ * medium_range_mem7/                 03-Aug-2020 21:17    -
+ * medium_range_no_da/                03-Aug-2020 02:39    -
+ * short_range/                       03-Aug-2020 00:59    -
+ * short_range_hawaii/                02-Aug-2020 15:37    -
+ * short_range_hawaii_no_da/          02-Aug-2020 15:37    -
+ * short_range_puertorico/            02-Aug-2020 21:05    -
+ * short_range_puertorico_no_da/      02-Aug-2020 21:11    -
+ * usgs_timeslices/                   03-Aug-2020 20:40    -
  */
 
 class NWMProfiles
@@ -69,6 +109,10 @@ class NWMProfiles
         else if ( shortHand.equals( InterfaceShortHand.NWM_LONG_RANGE_CHANNEL_RT_CONUS ) )
         {
             return NWMProfiles.getLongRangeChannelRtConus();
+        }
+        if ( shortHand.equals( InterfaceShortHand.NWM_SHORT_RANGE_CHANNEL_RT_PUERTORICO ) )
+        {
+            return NWMProfiles.getShortRangeChannelRtPuertoRico();
         }
         else
         {
@@ -153,4 +197,20 @@ class NWMProfiles
                                true );
     }
 
+
+    private static NWMProfile getShortRangeChannelRtPuertoRico()
+    {
+        return new NWMProfile( 48,
+                               1,
+                               Duration.ofHours( 1 ),
+                               true,
+                               "short_range",
+                               "channel_rt",
+                               NWMProfile.TimeLabel.f,
+                               "short_range_puertorico",
+                               "puertorico",
+                               // Actually, T06Z and T18Z
+                               Duration.ofHours( 6 ),
+                               false );
+    }
 }
