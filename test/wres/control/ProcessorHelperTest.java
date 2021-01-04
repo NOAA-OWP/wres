e17a88a0da1d262afd56032d91fcb30a1e0c7bb9
Jesse Bickel <jesse.bickel@noreply.gov>
2021-01-04 13:43:52 -0600

    Add tests for ProcessorHelper.doAllOrException
    Two happy-path tests and one that verifies an exception on a shorter
    task propagates prior to completion of a longer task.
    
    Refs #66545

--- test/wres/control/ProcessorHelperTest.java
+++ test/wres/control/ProcessorHelperTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import static org.junit.Assert.assertTrue;
 public class ProcessorHelperTest
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( ProcessorHelperTest.class );
+    private static final String TASK_RESULT_STRING_PREFIX = "I took a little more than ";
 
     @Test
     public void testDoAllOrExceptionResultHasException()
@@ -27,6 +29,73 @@ public class ProcessorHelperTest
         CompletableFuture<Object> result = ProcessorHelper.doAllOrException( List.of( f ) );
         Throwable cause = assertThrows( CompletionException.class, result::join ).getCause();
         assertTrue( cause instanceof DummyException );
+    }
+
+
+    @Test
+    public void testDoAllOrExceptionResultHasNoException()
+            throws InterruptedException, ExecutionException
+    {
+        DummyTask succeedingTask = new DummyTask( Duration.ofMillis( 1 ),
+                                                  false );
+        CompletableFuture<String> f = CompletableFuture.supplyAsync( succeedingTask::call );
+        ProcessorHelper.doAllOrException( List.of( f ) )
+                       .join();
+        assertTrue( f.isDone() );
+        assertTrue( f.get()
+                     .startsWith( TASK_RESULT_STRING_PREFIX ) );
+    }
+
+
+    @Test
+    public void testDoAllOrExceptionCompletesMultipleTasks()
+            throws InterruptedException, ExecutionException
+    {
+        DummyTask taskOne = new DummyTask( Duration.ofMillis( 1 ),
+                                           false );
+        DummyTask taskTwo = new DummyTask( Duration.ofMillis( 1 ),
+                                           false );
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync( taskOne::call );
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync( taskTwo::call );
+        ProcessorHelper.doAllOrException( List.of( f1, f2 ) )
+                       .join();
+        assertTrue( f1.isDone() );
+        assertTrue( f2.isDone() );
+        assertTrue( f1.get()
+                      .startsWith( TASK_RESULT_STRING_PREFIX ) );
+        assertTrue( f2.get()
+                      .startsWith( TASK_RESULT_STRING_PREFIX ) );
+    }
+
+
+    /**
+     * Verify that when the shorter task is after the first (longer) task, while
+     * the longer task does not throw an exception, and the shorter task does
+     * throw an exception, that the exception propagates up within the expected
+     * timeframe of "between duration of shorter task and longer task."
+     */
+    @Test
+    public void testDoAllOrExceptionThrowsShorterTaskException()
+    {
+        Duration longerDuration = Duration.ofMillis( 10_000 );
+        Duration shorterDuration = Duration.ofMillis( 10 );
+        DummyTask firstLongerSucceedingTask = new DummyTask( longerDuration,
+                                                             false );
+        DummyTask secondShorterFailingTask = new DummyTask( shorterDuration,
+                                                            true );
+        Instant start = Instant.now();
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync( firstLongerSucceedingTask::call );
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync( secondShorterFailingTask::call );
+        CompletableFuture<Object> result = ProcessorHelper.doAllOrException( List.of( f1, f2 ) );
+        Throwable cause = assertThrows( CompletionException.class, result::join ).getCause();
+        Instant end = Instant.now();
+        assertTrue( cause instanceof DummyException );
+        Duration executionDuration = Duration.between( start, end );
+        assertTrue( "Expected execution duration " + executionDuration
+                    + " to be shorter than " + longerDuration
+                    + " and longer than " + shorterDuration,
+                    executionDuration.toMillis() > shorterDuration.toMillis()
+                    && executionDuration.toMillis() < longerDuration.toMillis() );
     }
 
 
@@ -76,7 +145,7 @@ public class ProcessorHelperTest
             }
 
             Instant end = Instant.now();
-            String result = "I took a little more than "
+            String result = TASK_RESULT_STRING_PREFIX
                             + Duration.between( start, end );
 
             if ( this.mustFail )
