cb4e6cefe8f44cc2bcf85f86813aa61c736867b8
Jesse Bickel <jesse.bickel@noreply.gov>
2021-02-01 12:06:54 -0600

    Add basic connectivity-to-NWIS test to external tests distribution
    Put the jars in the lib directory, other stuff in the root directory.
    Include the gradle stuff needed.
    
    Refs #87148

---
+++ wres-external-services-tests/dist/src/test/java/wres/testing/NwisTest.java
@@ -0,0 +1,37 @@
+package wres.testing.nwis;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import wres.io.utilities.WebClient;
+
+public class NwisTest
+{
+    private static final URI NWIS_URI = URI.create( "https://nwis.waterservices.usgs.gov/nwis/iv/" );
+    private final WebClient webClient = new WebClient();
+
+    @Test
+    void canGetMinimalResponseFromNwisWithWebClient() throws IOException
+    {
+        List<Integer> retryOnThese = Collections.emptyList();
+
+        try ( WebClient.ClientResponse response = webClient.getFromWeb( NWIS_URI,
+                                                                        retryOnThese ) )
+        {
+            assertAll( () -> assertTrue( response.getStatusCode() >= 200
+                                         && response.getStatusCode() < 300,
+                                         "Expected HTTP 2XX response." ),
+                       () -> assertNotNull( response.getResponse(),
+                                            "Expected an InputStream" )
+            );
+        }
+    }
+}
+
