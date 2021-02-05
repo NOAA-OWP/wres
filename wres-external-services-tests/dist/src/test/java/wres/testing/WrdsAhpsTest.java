24a47d643ccd6302186648d7f56499cbf9ed7580
Jesse Bickel <jesse.bickel@noreply.gov>
2021-02-05 15:40:49 -0600

    Add a WebClient-can-reach-WRDS test, enable same test for NWIS
    Include WRDS-used DoD CA pem in the external services test zip, and on
    the classpath at test-time within that distribution.
    
    Refs #87148

--- wres-external-services-tests/dist/src/test/java/wres/testing/WrdsAhpsTest.java
+++ wres-external-services-tests/dist/src/test/java/wres/testing/WrdsAhpsTest.java
@@ -4,25 +4,31 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import wres.io.reading.wrds.ReadValueManager;
 import wres.io.utilities.WebClient;
 
-public class NwisTest
+public class WrdsAhpsTest
 {
-    private static final URI NWIS_URI = URI.create( "https://nwis.waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=09165000&parameterCd=00060&startDT=2018-10-01T00:00:00Z&endDT=2018-10-07T23:59:59Z" );
-    private final WebClient webClient = new WebClient();
+    private static final Pair<SSLContext, X509TrustManager> SSL_CONTEXT =
+            ReadValueManager.getSslContextTrustingDodSigner();
+    private static final URI WRDS_AHPS_URI = URI.create( "https://***REMOVED***.***REMOVED***.***REMOVED***/api/v1/forecasts/streamflow/ahps/nwsLocations/DRRC2?issuedTime=(2018-10-01T00%3A00%3A00Z%2C2018-10-07T23%3A23%3A59Z]&validTime=all&excludePast=none&groupInterval=none&groups=9999&groupStatistic=none&minForecastStatus=no_flooding&groupsRefTime=basisTime" );
+    private final WebClient webClient = new WebClient( SSL_CONTEXT, true );
 
     @Test
-    void canGetMinimalResponseFromNwisWithWebClient() throws IOException
+    void canGetMinimalResponseFromWrdsAhpsWithWebClient() throws IOException
     {
         List<Integer> retryOnThese = Collections.emptyList();
 
-        try ( WebClient.ClientResponse response = webClient.getFromWeb( NWIS_URI,
+        try ( WebClient.ClientResponse response = webClient.getFromWeb( WRDS_AHPS_URI,
                                                                         retryOnThese ) )
         {
             assertAll( () -> assertTrue( response.getStatusCode() >= 200
