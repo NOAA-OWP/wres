d472be786982b1acc19a2710ad83ae7234e37b30
James Brown <james.d.brown@noreply.gov>
2021-02-08 23:33:05 +0000

    Adds an entrypoint script to the eventsbroker container.
    Adds a HOST_NAME environment variable to use when creating a host-specific directory for qpid message overflow.
    
    Refs #87362

---
+++ wres-eventsbroker/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Set a lenient umask for files written by the qpid process to allow for their removal
+umask 0002
+
+# For any files not written using the default umask, make them group-writable on exit
+trap stop TERM INT
+
+start() {
+    # Run qpid in background and await it so that this script traps signals properly
+    # https://github.com/moby/moby/issues/33319#issuecomment-457914349
+    echo "Starting the wres-eventsbroker..."    
+    qpid-server --initial-config-path ${QPID_HOME}/etc/initial-config.json -prop qpid.amqp_port=5673 -prop qpid.http_port=15673 &
+    wait $!
+}
+
+# Stop the broker only once, regardless of how many stop signals are received
+stopped="false"
+
+stop() {
+    if [ "$stopped" = "false" ]
+    then
+        echo "Stopping the wres-eventsbroker. Bye bye..."
+        # Make the qpid files group writable
+        chmod -R 764 ${QPID_WORK}
+        stopped="true"
+        exit 0
+    fi
+}
+
+start
