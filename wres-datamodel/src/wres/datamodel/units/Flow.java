29f649b1e6a02d073728692d24e7ac8c35c7ebad
Jesse Bickel <jesse.bickel@noreply.gov>
2021-02-16 12:05:51 -0600

    Add javax.measure cubic foot per second progress
    Include the US Customary measures from systems-common library.
    
    See diagram at https://github.com/unitsofmeasurement/uom-systems
    for the four levels of libraries.
    
    Refs #44819

--- wres-datamodel/src/wres/datamodel/units/Flow.java
+++ wres-datamodel/src/wres/datamodel/units/Flow.java
@@ -7,13 +7,18 @@ import tech.units.indriya.unit.ProductUnit;
 
 import static tech.units.indriya.unit.Units.CUBIC_METRE;
 import static tech.units.indriya.unit.Units.SECOND;
+import static systems.uom.common.USCustomary.CUBIC_FOOT;
 
 /**
  * Experimental class to build flow units using javax.measure. When it is used,
+ * visibility will likely need to be public and the library declarations updated
+ * in the build.gradle to be "api" instead of "implementation."
  */
 
 class Flow
 {
     static final Unit<VolumetricFlowRate> CUBIC_METRE_PER_SECOND =
             new ProductUnit<VolumetricFlowRate>( CUBIC_METRE.divide( SECOND ) );
+    static final Unit<VolumetricFlowRate> CUBIC_FOOT_PER_SECOND =
+            new ProductUnit<VolumetricFlowRate>( CUBIC_FOOT.divide( SECOND ) );
 }
