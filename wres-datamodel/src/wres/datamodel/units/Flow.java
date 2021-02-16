09a2298e22450a126a7efca9dee9c909e70f6f6c
Jesse Bickel <jesse.bickel@noreply.gov>
2021-02-16 11:33:12 -0600

    Add javax.measure cubic meters per second progress
    Refs #44819

--- wres-datamodel/src/wres/datamodel/units/Flow.java
+++ wres-datamodel/src/wres/datamodel/units/Flow.java
@@ -1,5 +1,19 @@
 package wres.datamodel.units;
 
-public class Flow
+import javax.measure.Unit;
+
+import si.uom.quantity.VolumetricFlowRate;
+import tech.units.indriya.unit.ProductUnit;
+
+import static tech.units.indriya.unit.Units.CUBIC_METRE;
+import static tech.units.indriya.unit.Units.SECOND;
+
+/**
+ * Experimental class to build flow units using javax.measure. When it is used,
+ */
+
+class Flow
 {
+    static final Unit<VolumetricFlowRate> CUBIC_METRE_PER_SECOND =
+            new ProductUnit<VolumetricFlowRate>( CUBIC_METRE.divide( SECOND ) );
 }
