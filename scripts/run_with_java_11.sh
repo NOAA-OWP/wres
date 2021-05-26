c1fca2e685c7eb87ca4162fd25bacf47cbcf38e5
Jesse Bickel <jesse.bickel@noreply.gov>
2021-05-26 18:48:54 +0000

    Update jdk that runs on build server
    Refs #92167

--- scripts/run_with_java_11.sh
+++ scripts/run_with_java_11.sh
@@ -8,8 +8,8 @@
 # The sha256sum is published alongside releases visible at
 # https://www.azul.com/downloads/zulu-community/
 
-zulu_java_sha256sum="6c79bfe8bb06c82b72ef2f293a14becef56b3078d298dc75fda4225cbb2d3d0c"
-zulu_java_version="zulu11.43.55-ca-jdk11.0.9.1-linux_x64"
+zulu_java_sha256sum="172449e998afbb59a961598330a3438dbe9f188910cdeb14d0a6457421541134"
+zulu_java_version="zulu11.48.21-ca-jdk11.0.11-linux_x64"
 zulu_java_tarball="${zulu_java_version}.tar.gz"
 zulu_java_url="https://cdn.azul.com/zulu/bin/${zulu_java_tarball}"
 
@@ -26,7 +26,7 @@ else
     echo "Found existing zulu java at ${zulu_java_version}"
 fi
 
-old_zulu_java_version="zulu11.39.15-ca-jdk11.0.7-linux_x64"
+old_zulu_java_version="zulu11.43.55-ca-jdk11.0.9.1-linux_x64"
 
 if [ -d ${old_zulu_java_version} ]
 then
