3eb90772e8a34879e718b86fe0553f3dfab311e2
Hank.Herr <Hank.Herr@noreply.gov>
2021-06-04 14:24:21 -0400

    Adding ignoreUnknown to the members reader for the AHPS API; refs #91778

--- wres-io/src/wres/io/reading/wrds/Member.java
+++ wres-io/src/wres/io/reading/wrds/Member.java
@@ -2,6 +2,9 @@ package wres.io.reading.wrds;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties( ignoreUnknown = true )
 public class Member
 {
     String identifier;
