8a71d9491602963070be9bb179efbc12a1e0fb39
James Brown <james.d.brown@noreply.gov>
2021-06-11 13:52:26 +0100

    Adds an in-memory store of time-series for experimentation. Also adds a retriever factory to utilize the store.
    Refs #92522

---
+++ wres-datamodel/src/wres/datamodel/time/TimeSeriesStore.java
@@ -0,0 +1,312 @@
+package wres.datamodel.time;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.jcip.annotations.Immutable;
+import wres.config.generated.LeftOrRightOrBaseline;
+import wres.datamodel.Ensemble;
+import wres.datamodel.FeatureKey;
+
+/**
+ * Store of {@link TimeSeries} that is built incrementally with a {@link Builder}.
+ * @author james.brown@hydrosolved.com
+ */
+
+@Immutable
+public class TimeSeriesStore
+{
+    /** Logger. */
+    private final Logger LOGGER = LoggerFactory.getLogger( TimeSeriesStore.class );
+
+    /** Left-ish time-series of {@link Double}. **/
+    private final List<TimeSeries<Double>> leftSingleValuedSeries;
+
+    /** Right-ish time-series of {@link Double}. **/
+    private final List<TimeSeries<Double>> rightSingleValuedSeries;
+
+    /** Baseline-ish time-series of {@link Double}. **/
+    private final List<TimeSeries<Double>> baselineSingleValuedSeries;
+
+    /** Left-ish time-series of {@link Ensemble}. **/
+    private final List<TimeSeries<Ensemble>> leftEnsembleSeries;
+
+    /** Right-ish time-series of {@link Ensemble}. **/
+    private final List<TimeSeries<Ensemble>> rightEnsembleSeries;
+
+    /** Baseline-ish time-series of {@link Ensemble}. **/
+    private final List<TimeSeries<Ensemble>> baselineEnsembleSeries;
+
+    /**
+     * Returns all single-valued series by feature.
+     * @param context the context
+     * @param feature the feature
+     * @return the filtered series
+     * @throws NullPointerException if any input is null
+     * @throws IllegalArgumentException if the context is unrecognized
+     */
+
+    public Stream<TimeSeries<Double>> getSingleValuedSeries( LeftOrRightOrBaseline context, FeatureKey feature )
+    {
+        Objects.requireNonNull( context );
+        Objects.requireNonNull( feature );
+
+        return TimeSeriesStore.getSingleValuedStore( this.leftSingleValuedSeries,
+                                                     this.rightSingleValuedSeries,
+                                                     this.baselineSingleValuedSeries,
+                                                     context )
+                              .stream()
+                              .filter( next -> feature.equals( next.getMetadata().getFeature() ) );
+    }
+
+    /**
+     * Filters the single-valued series by time window and feature.
+     * @param timeWindow the time window
+     * @param context the context
+     * @param feature the feature
+     * @return the filtered series
+     * @throws NullPointerException if any input is null
+     * @throws IllegalArgumentException if the context is unrecognized
+     */
+
+    public Stream<TimeSeries<Double>> getSingleValuedSeries( TimeWindowOuter timeWindow,
+                                                             LeftOrRightOrBaseline context,
+                                                             FeatureKey feature )
+    {
+        Objects.requireNonNull( timeWindow );
+        Objects.requireNonNull( context );
+        Objects.requireNonNull( feature );
+
+        return TimeSeriesStore.getSingleValuedStore( this.leftSingleValuedSeries,
+                                                     this.rightSingleValuedSeries,
+                                                     this.baselineSingleValuedSeries,
+                                                     context )
+                              .stream()
+                              .filter( next -> feature.equals( next.getMetadata().getFeature() ) )
+                              .map( next -> TimeSeriesSlicer.filter( next, timeWindow ) );
+    }
+
+    /**
+     * Filters the ensemble series by time window and feature.
+     * @param timeWindow the time window
+     * @param context the context
+     * @param feature the feature
+     * @return the filtered series
+     * @throws NullPointerException if any input is null
+     * @throws IllegalArgumentException if the context is unrecognized
+     */
+
+    public Stream<TimeSeries<Ensemble>> getEnsembleSeries( TimeWindowOuter timeWindow,
+                                                           LeftOrRightOrBaseline context,
+                                                           FeatureKey feature )
+    {
+        Objects.requireNonNull( timeWindow );
+        Objects.requireNonNull( context );
+        Objects.requireNonNull( feature );
+
+        return TimeSeriesStore.getEnsembleStore( this.leftEnsembleSeries,
+                                                 this.rightEnsembleSeries,
+                                                 this.baselineEnsembleSeries,
+                                                 context )
+                              .stream()
+                              .filter( next -> feature.equals( next.getMetadata().getFeature() ) )
+                              .map( next -> TimeSeriesSlicer.filter( next,
+                                                                     timeWindow ) );
+    }
+
+    /**
+     * Builder to create the store incrementally.
+     */
+
+    public static class Builder
+    {
+        /** Left-ish time-series of {@link Double}. **/
+        private final Queue<TimeSeries<Double>> leftSingleValuedSeries = new ConcurrentLinkedQueue<>();
+
+        /** Right-ish time-series of {@link Double}. **/
+        private final Queue<TimeSeries<Double>> rightSingleValuedSeries = new ConcurrentLinkedQueue<>();
+
+        /** Baseline-ish time-series of {@link Double}. **/
+        private final Queue<TimeSeries<Double>> baselineSingleValuedSeries = new ConcurrentLinkedQueue<>();
+
+        /** Left-ish time-series of {@link Ensemble}. **/
+        private final Queue<TimeSeries<Ensemble>> leftEnsembleSeries = new ConcurrentLinkedQueue<>();
+
+        /** Right-ish time-series of {@link Ensemble}. **/
+        private final Queue<TimeSeries<Ensemble>> rightEnsembleSeries = new ConcurrentLinkedQueue<>();
+
+        /** Baseline-ish time-series of {@link Ensemble}. **/
+        private final Queue<TimeSeries<Ensemble>> baselineEnsembleSeries = new ConcurrentLinkedQueue<>();
+
+        /**
+         * Adds a single-valued time-series to the store with given context
+         * @param series the time-series
+         * @param context the context
+         * @return the builder
+         * @throws NullPointerException if either input is null
+         * @throws IllegalArgumentException if the context is unrecognized
+         */
+
+        public Builder addSingleValuedSeries( TimeSeries<Double> series, LeftOrRightOrBaseline context )
+        {
+            Objects.requireNonNull( series );
+            Objects.requireNonNull( context );
+
+            TimeSeriesStore.getSingleValuedStore( this.leftSingleValuedSeries,
+                                                  this.rightSingleValuedSeries,
+                                                  this.baselineSingleValuedSeries,
+                                                  context )
+                           .add( series );
+
+            return this;
+        }
+
+        /**
+         * Adds an ensemble time-series to the store with given context
+         * @param series the time-series
+         * @param context the context
+         * @return the builder
+         * @throws NullPointerException if either input is null
+         * @throws IllegalArgumentException if the context is unrecognized
+         */
+
+        public Builder addEnsembleSeries( TimeSeries<Ensemble> series, LeftOrRightOrBaseline context )
+        {
+            Objects.requireNonNull( series );
+            Objects.requireNonNull( context );
+
+            TimeSeriesStore.getEnsembleStore( this.leftEnsembleSeries,
+                                              this.rightEnsembleSeries,
+                                              this.baselineEnsembleSeries,
+                                              context )
+                           .add( series );
+
+            return this;
+        }
+
+        /**
+         * @return a {@link TimeSeriesStore}.
+         */
+
+        public TimeSeriesStore build()
+        {
+            return new TimeSeriesStore( this );
+        }
+    }
+
+    /**
+     * Helper that selects the store based on the context.
+     * @param leftSingleValuedSeries the store of left single-valued series
+     * @param rightSingleValuedSeries the store of right single-valued series
+     * @param baselineSingleValuedSeries the store of baseline single-valued series
+     * @param context the context
+     * @return a single-valued store with the prescribed context
+     * @throws IllegalArgumentException if the context is unrecognized
+     */
+
+    private static Collection<TimeSeries<Double>>
+            getSingleValuedStore( Collection<TimeSeries<Double>> leftSingleValuedSeries,
+                                  Collection<TimeSeries<Double>> rightSingleValuedSeries,
+                                  Collection<TimeSeries<Double>> baselineSingleValuedSeries,
+                                  LeftOrRightOrBaseline context )
+    {
+        switch ( context )
+        {
+            case LEFT:
+                return leftSingleValuedSeries;
+            case RIGHT:
+                return rightSingleValuedSeries;
+            case BASELINE:
+                return baselineSingleValuedSeries;
+            default:
+                throw new IllegalArgumentException( "Unexpected context '" + context
+                                                    + "'. Expected LEFT or RIGHT or BASELINE." );
+        }
+    }
+
+    /**
+     * Helper that selects the store based on the context.
+     * @param leftEnsembleSeries the store of left single-valued series
+     * @param rightEnsembleSeries the store of right single-valued series
+     * @param baselineEnsembleSeries the store of baseline single-valued series
+     * @param context the context
+     * @return an ensemble store with the prescribed context
+     * @throws IllegalArgumentException if the context is unrecognized
+     */
+
+    private static Collection<TimeSeries<Ensemble>>
+            getEnsembleStore( Collection<TimeSeries<Ensemble>> leftEnsembleSeries,
+                              Collection<TimeSeries<Ensemble>> rightEnsembleSeries,
+                              Collection<TimeSeries<Ensemble>> baselineEnsembleSeries,
+                              LeftOrRightOrBaseline context )
+    {
+        switch ( context )
+        {
+            case LEFT:
+                return leftEnsembleSeries;
+            case RIGHT:
+                return rightEnsembleSeries;
+            case BASELINE:
+                return baselineEnsembleSeries;
+            default:
+                throw new IllegalArgumentException( "Unexpected context '" + context
+                                                    + "'. Expected LEFT or RIGHT or BASELINE." );
+        }
+    }
+
+    /**
+     * Creates an instance.
+     * @param builder the builder
+     */
+
+    private TimeSeriesStore( Builder builder )
+    {
+        // No longer need a concurrent collection type because writing is complete and the collection is now immutable
+        this.leftSingleValuedSeries = Collections.unmodifiableList( new ArrayList<>( builder.leftSingleValuedSeries ) );
+        this.rightSingleValuedSeries =
+                Collections.unmodifiableList( new ArrayList<>( builder.rightSingleValuedSeries ) );
+        this.baselineSingleValuedSeries =
+                Collections.unmodifiableList( new ArrayList<>( builder.baselineSingleValuedSeries ) );
+        this.leftEnsembleSeries = Collections.unmodifiableList( new ArrayList<>( builder.leftEnsembleSeries ) );
+        this.rightEnsembleSeries = Collections.unmodifiableList( new ArrayList<>( builder.rightEnsembleSeries ) );
+        this.baselineEnsembleSeries = Collections.unmodifiableList( new ArrayList<>( builder.baselineEnsembleSeries ) );
+
+        if ( LOGGER.isInfoEnabled() )
+        {
+            int size = this.leftSingleValuedSeries.size() + this.rightSingleValuedSeries.size()
+                       + this.baselineSingleValuedSeries.size()
+                       + this.leftEnsembleSeries.size()
+                       + this.rightEnsembleSeries.size()
+                       + this.baselineEnsembleSeries.size();
+
+            LOGGER.info( "Created a time-series store that contains {} single-valued time-series with designation {}, "
+                         + "{} single-valued time-series with designation {}, {} single-valued time-series with "
+                         + "designation {}, {} ensemble time-series with designation {}, {} ensemble time-series with "
+                         + "designation {} and {} ensemble time-series with designation {}. There are {} time-series "
+                         + "in the store.",
+                         this.leftSingleValuedSeries.size(),
+                         LeftOrRightOrBaseline.LEFT,
+                         this.rightSingleValuedSeries.size(),
+                         LeftOrRightOrBaseline.RIGHT,
+                         this.baselineSingleValuedSeries.size(),
+                         LeftOrRightOrBaseline.BASELINE,
+                         this.leftEnsembleSeries.size(),
+                         LeftOrRightOrBaseline.LEFT,
+                         this.rightEnsembleSeries.size(),
+                         LeftOrRightOrBaseline.RIGHT,
+                         this.baselineEnsembleSeries.size(),
+                         LeftOrRightOrBaseline.BASELINE,
+                         size );
+        }
+    }
+
+}
---
+++ wres-io/src/wres/io/retrieval/EnsembleRetrieverFactory2.java
@@ -0,0 +1,198 @@
+package wres.io.retrieval;
+
+import java.util.Objects;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import wres.config.generated.LeftOrRightOrBaseline;
+import wres.config.generated.PairConfig;
+import wres.config.generated.ProjectConfig;
+import wres.datamodel.Ensemble;
+import wres.datamodel.FeatureTuple;
+import wres.datamodel.scale.TimeScaleOuter;
+import wres.datamodel.time.TimeSeries;
+import wres.datamodel.time.TimeSeriesMetadata;
+import wres.datamodel.time.TimeSeriesSlicer;
+import wres.datamodel.time.TimeSeriesStore;
+import wres.datamodel.time.TimeWindowOuter;
+import wres.io.config.ConfigHelper;
+
+/**
+ * <p>A factory class that creates retrievers for the single-valued left and ensemble right datasets associated with one 
+ * evaluation. This factory takes a "per-feature-view" of retrieval whereby a feature is supplied on construction. In 
+ * future, other implementations may not take a per-feature view (e.g., a multiple-feature-view or a grid-view). This 
+ * implementation is backed by a {@link TimeSeriesStore} that is supplied on construction.
+ * 
+ * @author james.brown@hydrosolved.com
+ */
+
+public class EnsembleRetrieverFactory2 implements RetrieverFactory<Double, Ensemble>
+{
+    /** Logger. */
+    private static final Logger LOGGER = LoggerFactory.getLogger( EnsembleRetrieverFactory2.class );
+
+    /** Declared <code>desiredTimeScale</code>, if any. */
+    private final TimeScaleOuter desiredTimeScale;
+
+    /** A feature tuple for retrieval. */
+    private final FeatureTuple feature;
+
+    /** A time-series store. */
+    private final TimeSeriesStore timeSeriesStore;
+
+    /**A unit mapper. */
+    private final UnitMapper unitMapper;
+
+    /**
+     * Returns an instance.
+     *
+     * @param projectConfig the project declaration
+     * @param feature a feature to evaluate
+     * @param timeSeriesStore the store of time-series
+     * @param unitMapper the unit mapper
+     * @return a factory instance
+     * @throws NullPointerException if any input is null
+     */
+
+    public static EnsembleRetrieverFactory2 of( ProjectConfig projectConfig,
+                                                FeatureTuple feature,
+                                                TimeSeriesStore timeSeriesStore,
+                                                UnitMapper unitMapper )
+    {
+        return new EnsembleRetrieverFactory2( projectConfig, feature, timeSeriesStore, unitMapper );
+    }
+
+    @Override
+    public Supplier<Stream<TimeSeries<Double>>> getLeftRetriever()
+    {
+        // Wrap in a caching retriever to allow re-use of left-ish data, and map the units here
+        return CachingRetriever.of( () -> this.timeSeriesStore.getSingleValuedSeries( LeftOrRightOrBaseline.LEFT,
+                                                                                      this.feature.getLeft() )
+                                                              .map( this::mapUnits ) );
+    }
+
+    @Override
+    public Supplier<Stream<TimeSeries<Double>>> getLeftRetriever( TimeWindowOuter timeWindow )
+    {
+        // Consider all possible lead durations
+        TimeWindowOuter adjustedWindow = timeWindow.toBuilder()
+                                                   .setEarliestLeadDuration( TimeWindowOuter.DURATION_MIN )
+                                                   .setLatestLeadDuration( TimeWindowOuter.DURATION_MAX )
+                                                   .build();
+
+        // Wrap in a caching retriever to allow re-use of left-ish data
+        return CachingRetriever.of( () -> this.timeSeriesStore.getSingleValuedSeries( adjustedWindow,
+                                                                                      LeftOrRightOrBaseline.LEFT,
+                                                                                      this.feature.getLeft() )
+                                                              .map( this::mapUnits ) );
+    }
+
+    @Override
+    public Supplier<Stream<TimeSeries<Ensemble>>> getRightRetriever( TimeWindowOuter timeWindow )
+    {
+        TimeWindowOuter adjustedWindow = this.adjustByTimeScalePeriod( timeWindow );
+
+        // TODO: allow for unit mapping
+        return () -> this.timeSeriesStore.getEnsembleSeries( adjustedWindow,
+                                                             LeftOrRightOrBaseline.RIGHT,
+                                                             this.feature.getRight() );
+    }
+
+    @Override
+    public Supplier<Stream<TimeSeries<Ensemble>>> getBaselineRetriever()
+    {
+        // TODO: allow for unit mapping
+        return this.getBaselineRetriever( TimeWindowOuter.of() );
+    }
+
+    @Override
+    public Supplier<Stream<TimeSeries<Ensemble>>> getBaselineRetriever( TimeWindowOuter timeWindow )
+    {
+        TimeWindowOuter adjustedWindow = this.adjustByTimeScalePeriod( timeWindow );
+
+        // TODO: allow for unit mapping
+        return () -> this.timeSeriesStore.getEnsembleSeries( adjustedWindow,
+                                                             LeftOrRightOrBaseline.BASELINE,
+                                                             this.feature.getBaseline() );
+    }
+
+    /**
+     * Adjusts the earliest lead duration of the input to account for the period associated with the desired time scale 
+     * in order to capture sufficient data for rescaling.
+     * 
+     * @param timeWindow the time window to adjust
+     * @return the adjusted time window
+     */
+
+    private TimeWindowOuter adjustByTimeScalePeriod( TimeWindowOuter timeWindow )
+    {
+        TimeWindowOuter adjusted = timeWindow.toBuilder()
+                                             .setEarliestLeadDuration( timeWindow.getEarliestLeadDuration()
+                                                                                 .minus( this.desiredTimeScale.getPeriod() ) )
+                                             .build();
+
+        if ( LOGGER.isDebugEnabled() )
+        {
+            LOGGER.debug( "Adjusted the earliest lead duration of {} by {} to {}, in order to select sufficient data "
+                          + "for rescaling.",
+                          timeWindow,
+                          this.desiredTimeScale.getPeriod(),
+                          adjusted );
+        }
+
+        return adjusted;
+    }
+
+    /**
+     * Map the units of a single-valued time-series
+     * @param timeSeries the time-series
+     * @return a time-series with mapped units
+     */
+
+    private final TimeSeries<Double> mapUnits( TimeSeries<Double> timeSeries )
+    {
+        DoubleUnaryOperator mapper = this.unitMapper.getUnitMapper( timeSeries.getMetadata().getUnit() );
+        
+        TimeSeriesMetadata meta =
+                new TimeSeriesMetadata.Builder( timeSeries.getMetadata() ).setUnit( this.unitMapper.getDesiredMeasurementUnitName() )
+                                                                          .build();
+        TimeSeries<Double> mapped = TimeSeriesSlicer.transform( timeSeries, mapper::applyAsDouble );
+        return TimeSeries.of( meta, mapped.getEvents() );
+    }
+
+    /**
+     * Hidden constructor.
+     * 
+     * @param projectConfig the project declaration
+     * @param feature a feature to evaluate
+     * @param timeSeriesStore the time-series store
+     * @param unitMapper the unit mapper
+     * @param timeSeriesStore the store of time-series
+     * @throws NullPointerException if any input is null
+     */
+
+    private EnsembleRetrieverFactory2( ProjectConfig projectConfig,
+                                       FeatureTuple feature,
+                                       TimeSeriesStore timeSeriesStore,
+                                       UnitMapper unitMapper )
+    {
+        Objects.requireNonNull( projectConfig );
+        Objects.requireNonNull( feature );
+        Objects.requireNonNull( timeSeriesStore );
+        Objects.requireNonNull( unitMapper );
+
+        this.feature = feature;
+        this.timeSeriesStore = timeSeriesStore;
+        this.unitMapper = unitMapper;
+
+        PairConfig pairConfig = projectConfig.getPair();
+
+        // Obtain and set the desired time scale. 
+        this.desiredTimeScale = ConfigHelper.getDesiredTimeScale( pairConfig );
+    }
+
+}
