9e12318e2a20dea8c7f56b99c787990b9dc719cb
Jesse Bickel <jesse.bickel@noreply.gov>
2021-06-21 14:46:40 +0000

    Cluster admin scripts: pass host as arg 1, update CA cert, cleanup
    Refs #90836 (probably others I'm missing too)

--- scripts/redmine_66829/wres_http_cleandatabase.sh
+++ scripts/redmine_66829/wres_http_cleandatabase.sh
@@ -4,14 +4,13 @@
 # This script runs a clean database on the single db used in an environment.
 # Intended to be run from a directory one level deeper than wres/scripts.
 
-env_suffix=-dev
-user_name=anonymous
+host=$1
 
-echo "We are using the $env_suffix environment in this program."
+echo "We are using the $host environment in this program."
 
 read -n1 -r -p "Please ctrl-c if that is not correct, any key otherwise..." key
 
-wres_ca_file=../cacerts/wres_ca_x509_cert.pem
+wres_ca_file=../cacerts/dod_root_ca_3_expires_2029-12.pem
 
 if [ -f $wres_ca_file ]
 then
@@ -21,7 +20,7 @@ else
     exit 1
 fi
 
-post_result=$( curl -i -s --cacert $wres_ca_file --data "userName=${user_name}&projectConfig=none&verb=cleandatabase" https://***REMOVED***wres${env_suffix}.***REMOVED***.***REMOVED***/job | tr -d '\r' )
+post_result=$( curl -i -s --cacert $wres_ca_file --data "projectConfig=none&verb=cleandatabase" https://${host}/job | tr -d '\r' )
 post_result_http_code=$( echo -n "$post_result" | grep HTTP | tail -n 1 | cut -d' ' -f2 )
 echo "The last status code in the response was $post_result_http_code"
 
@@ -36,8 +35,6 @@ fi
 job_location=$( echo -n "$post_result" | grep Location | cut -d' ' -f2 )
 echo "The location of the resource created by server was $job_location"
 
-jobs_failed=0
-
 evaluation_status=""
 
 while [ "$evaluation_status" != "COMPLETED_REPORTED_SUCCESS" ] \
@@ -47,17 +44,13 @@ do
     evaluation_status=$( curl --cacert $wres_ca_file $job_location/status -s | tr -d '\r' )
 done
 
+
 if [ "$evaluation_status" == "COMPLETED_REPORTED_SUCCESS" ]
 then
     echo "Clean succeeded for $job_location"
 else
     echo "CLEAN FAILED FOR $job_location"
-fi
-
-if [ "$jobs_failed" -ne "0" ]
-then
-    echo "There were $jobs_failed FAILED MIGRATIONS! See above..."
-    exit $jobs_failed
+    exit 3
 fi
 
 exit 0
