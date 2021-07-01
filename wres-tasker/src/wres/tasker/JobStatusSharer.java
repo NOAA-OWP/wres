940311fb7479e10fbf346a90415b9d55711a75e2
Jesse Bickel <jesse.bickel@noreply.gov>
2021-07-01 14:51:30 -0500

    Improve COWRES job state tracking (and therefore reporting)
    In the tasker:
    1. Wait until all watchers bind to feedback topics before sending job
    2. Allow job to be marked finished before code marks job as in queue
    3. When a job status comes back dead for a couple minutes, mark failed
    
    Refs #93685

--- wres-tasker/src/wres/tasker/JobStatusSharer.java
+++ wres-tasker/src/wres/tasker/JobStatusSharer.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static wres.messages.generated.JobStatus.job_status.Report.*;
+import static wres.tasker.JobMetadata.JobState.COMPLETED_REPORTED_FAILURE;
 import static wres.tasker.JobMetadata.JobState.IN_PROGRESS;
 
 import wres.messages.generated.JobStatus;
@@ -42,23 +43,61 @@ class JobStatusSharer implements Consumer<GeneratedMessageV3>
                 (JobStatus.job_status) mustBeJobStatus;
         JobStatus.job_status.Report report = jobStatus.getReport();
         JobMetadata.JobState existingState = this.jobMetadata.getJobState();
+        String jobId = this.jobMetadata.getId();
 
         if ( report.equals( RECEIVED ) || report.equals( ALIVE ) )
         {
             if ( existingState.equals( JobMetadata.JobState.IN_QUEUE ) )
             {
+                // TODO: this should probably be something like compare-and-set
                 this.jobMetadata.setJobState( IN_PROGRESS );
             }
             else if ( !existingState.equals( IN_PROGRESS ) )
             {
-                LOGGER.warn( "Existing job was not marked {} but worker reported {}",
-                             IN_PROGRESS, report );
+                LOGGER.warn( "Existing job {} was not marked {} but worker reported {}",
+                             jobId, IN_PROGRESS, report );
             }
         }
         else
         {
-            LOGGER.info( "Ignoring report {} because other mechanisms should handle it.",
-                         report );
+            if ( !JobMetadata.COMPLETED_STATES.contains( existingState ) )
+            {
+                // Wait two minutes to double-check state
+                try
+                {
+                    LOGGER.info( "Saw job {} was marked {} but worker reported status {}, waiting two minutes and re-checking",
+                                 jobId, existingState, report );
+                    Thread.sleep( 120000 );
+                }
+                catch ( InterruptedException ie )
+                {
+                    LOGGER.warn( "Interrupted while waiting to double-check state of dead process for {}",
+                                 jobId );
+                }
+
+                existingState = this.jobMetadata.getJobState();
+
+                if ( !JobMetadata.COMPLETED_STATES.contains( existingState ) )
+                {
+                    if ( LOGGER.isWarnEnabled() )
+                    {
+                        LOGGER.warn( "Marking job {} reported failure because it was marked dead but not yet marked in a completed state.",
+                                     this.jobMetadata.getId() );
+                    }
+
+                    this.jobMetadata.setJobState( COMPLETED_REPORTED_FAILURE );
+                }
+                else
+                {
+                    LOGGER.info( "Ignoring report {} for job {} because other mechanisms (eventually) already handled it.",
+                                 report, jobId );
+                }
+            }
+            else
+            {
+                LOGGER.info( "Ignoring report {} for job {} because other mechanisms already handled it.",
+                             report, jobId );
+            }
         }
     }
 }
