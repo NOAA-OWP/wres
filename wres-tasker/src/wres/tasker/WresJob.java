73689a960aeb2166d9f8c6aff8dea55984edd754
Jesse Bickel <jesse.bickel@noreply.gov>
2021-07-02 12:17:32 -0500

    Use a minimal live object to test the live object service at runtime
    Attempt 4 (no straightforward way to test locally)
    
    Refs #50810

--- wres-tasker/src/wres/tasker/WresJob.java
+++ wres-tasker/src/wres/tasker/WresJob.java
@@ -159,32 +159,6 @@ public class WresJob
     private static final Object CONNECTION_LOCK = new Object();
 
 
-    /**
-     * A dummy redisson class to test redisson live object service availability.
-     */
-
-    @REntity
-    private static class DummyLiveObject
-    {
-        @RId
-        private String id;
-
-        public DummyLiveObject( String id )
-        {
-            this.id = id;
-        }
-
-        public String getId()
-        {
-            return this.id;
-        }
-
-        public void setId( String id )
-        {
-            this.id = id;
-        }
-    }
-
     @GET
     @Produces( MediaType.TEXT_PLAIN )
     public String getWresJob()
