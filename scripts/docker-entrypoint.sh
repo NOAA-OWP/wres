5664aded9e30120fcb1daed0b38fe9c24187a095
James Brown <james.d.brown@noreply.gov>
2021-08-04 18:34:22 +0100

    Do not spawn an inner process to run the worker shim because it will not trap signals properly, hence the container will not exit cleanly/promptly on receiving a SIGTERM, and instead requires a SIGKILL.
    Fixes #87813

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,4 +3,5 @@
 # and in order for the java process to successfully set group write permissions,
 # need more lenient umask.
 umask 0002
-bin/wres-worker /usr/bin/wres
+# Re-use the current process so that signals are trapped properly: see #87813
+exec bin/wres-worker /usr/bin/wres
