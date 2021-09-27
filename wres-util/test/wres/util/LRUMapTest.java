774b496b8ab70f843929fab35f7469f4bba45015
Jesse Bickel <jesse.bickel@noreply.gov>
2021-09-27 10:38:59 -0500

    Remove unused code, remove powermock from wres-util
    Refs #96747

--- wres-util/test/wres/util/LRUMapTest.java
+++ wres-util/test/wres/util/LRUMapTest.java
@@ -5,14 +5,7 @@ import java.util.TreeMap;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import wres.util.LRUMap;
-
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.management.*")
 public class LRUMapTest
 {
     @Test
