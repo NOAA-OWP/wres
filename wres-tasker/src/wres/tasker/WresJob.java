fed5291b50d8d31aa00b6a65842725de679a8b7e
James Brown <james.d.brown@noreply.gov>
2022-01-24 16:24:37 +0000

    Superfluous imports etc.
    Refs #100533

--- wres-tasker/src/wres/tasker/WresJob.java
+++ wres-tasker/src/wres/tasker/WresJob.java
@@ -23,7 +23,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.redisson.Redisson;
 import org.redisson.api.RLiveObjectService;
