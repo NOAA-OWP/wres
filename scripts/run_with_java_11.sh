4c5f71688ffe3be3781d6d354bcfa4f5a2c4c875
Jesse Bickel <jesse.bickel@noreply.gov>
2022-03-07 10:22:17 -0600

    Update the JDK used by the build server's build script to 11.0.14
    Refs #101045

--- scripts/run_with_java_11.sh
+++ scripts/run_with_java_11.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# Until system has java 11 available, we can get an azul version
+# Until the build system has java 11 available, we can get an azul version.
 # The risk here is that this version becomes vulnerable and does not receive
 # updates except manually by updating this script. So this is intended to be a
 # temporary measure until NOAA VLAB Jenkins and NWCAL test systems have java 11.
 
 # The sha256sum is published alongside releases visible at
 # https://www.azul.com/downloads/zulu-community/
-
-zulu_java_sha256sum="172449e998afbb59a961598330a3438dbe9f188910cdeb14d0a6457421541134"
-zulu_java_version="zulu11.48.21-ca-jdk11.0.11-linux_x64"
+zulu_java_sha256sum="60e65d32e38876f81ddb623e87ac26c820465b637e263e8bed1acdecb4ca9be2"
+zulu_java_version="zulu11.54.25-ca-jdk11.0.14.1-linux_x64"
 zulu_java_tarball="${zulu_java_version}.tar.gz"
 zulu_java_url="https://cdn.azul.com/zulu/bin/${zulu_java_tarball}"
 
@@ -26,7 +25,7 @@ else
     echo "Found existing zulu java at ${zulu_java_version}"
 fi
 
-old_zulu_java_version="zulu11.43.55-ca-jdk11.0.9.1-linux_x64"
+old_zulu_java_version="zulu11.48.21-ca-jdk11.0.11-linux_x64"
 
 if [ -d ${old_zulu_java_version} ]
 then
