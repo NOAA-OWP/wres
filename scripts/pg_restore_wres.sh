8e011cf3b7ec78dcf02c1f34fa80c4a72f7912ae
Jesse Bickel <jesse.bickel@noreply.gov>
2022-05-10 21:30:43 +0000

    Remove some unused tables from the pg_restore_wres script
    This is a relatively old change, pre-6.0.
    
    Refs #104637

--- scripts/pg_restore_wres.sh
+++ scripts/pg_restore_wres.sh
@@ -3,6 +3,7 @@ database_host='fake.fqdn'
 database_name='wres'
 database_username='wres_user'
 pg_restore_command=$(which pg_restore)
+port=5432
 cpus=$(nproc)
 max_j=8
 min_j=1
@@ -22,11 +23,14 @@ do
         U)
             database_username=$OPTARG
             ;;
-        p)
+        k)
             pg_restore_command=$OPTARG
             ;;
+        p)
+            port=$OPTARG
+            ;;
         \?)
-            echo "Usage: $0 -f dump_file_prefix -h database_host -d database_name -U database_username [ -p pg_restore_command ]"
+            echo "Usage: $0 -f dump_file_prefix -h database_host -d database_name -U database_username [ -k pg_restore_command -p port ]"
             exit 2
             ;;
     esac
@@ -48,10 +52,10 @@ then
 fi
 
 
-list_objects_command="$pg_restore_command -e -l ${dump_file} -f ${list_of_non_schema_objects_file}"
-restore_pre_data_only_command="$pg_restore_command -e -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --section=pre-data"
-restore_data_only_table_command="$pg_restore_command -e -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --data-only --strict-names "
-restore_post_data_only_command="$pg_restore_command -e -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --section=post-data"
+list_objects_command="$pg_restore_command -e -p ${port} -l ${dump_file} -f ${list_of_non_schema_objects_file}"
+restore_pre_data_only_command="$pg_restore_command -e -p ${port} -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --section=pre-data"
+restore_data_only_table_command="$pg_restore_command -e -p ${port} -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --data-only --strict-names "
+restore_post_data_only_command="$pg_restore_command -e -p ${port} -j $j --no-owner -h ${database_host} -d ${database_name} -U ${database_username} --section=post-data"
 
 # Some basic checks before execution.
 dump_file_exists="(does NOT exist!)"
@@ -156,8 +160,8 @@ $list_objects_command \
 && $restore_data_only_table_command -n public -t databasechangelog -t databasechangeloglock $changelog_dump_file \
 && $restore_post_data_only_command $changelog_dump_file \
 && $restore_pre_data_only_command  -L ${list_of_non_schema_objects_file} $dump_file \
-&& $restore_data_only_table_command -t measurementunit -t conversions -t netcdfcoordinate -t unitconversion -t feature -t ensemble -t gridprojection -t source $dump_file \
-&& $restore_data_only_table_command -t measurementunit_measurementunit_id_seq -t ensemble_ensemble_id_seq -t gridprojection_gridprojection_id_seq -t source_source_id_seq $dump_file \
+&& $restore_data_only_table_command -t measurementunit -t feature -t ensemble -t source $dump_file \
+&& $restore_data_only_table_command -t measurementunit_measurementunit_id_seq -t ensemble_ensemble_id_seq -t source_source_id_seq $dump_file \
 && $restore_data_only_table_command -t timeseries $dump_file \
 && $restore_data_only_table_command -t timeseries_timeseries_id_seq -t timeseriesvalue -t timeseriesvalue_lead_0 -t timeseriesvalue_lead_1 -t timeseriesvalue_lead_2 -t timeseriesvalue_lead_3 -t timeseriesvalue_lead_4 -t timeseriesvalue_lead_5 -t timeseriesvalue_lead_6 -t timeseriesvalue_lead_7 -t timeseriesvalue_lead_8 -t timeseriesvalue_lead_9 $dump_file \
 && $restore_data_only_table_command -t timeseriesvalue_lead_10 -t timeseriesvalue_lead_11 -t timeseriesvalue_lead_12 -t timeseriesvalue_lead_13 -t timeseriesvalue_lead_14 -t timeseriesvalue_lead_15 -t timeseriesvalue_lead_16 -t timeseriesvalue_lead_17 -t timeseriesvalue_lead_18 -t timeseriesvalue_lead_19 $dump_file \
