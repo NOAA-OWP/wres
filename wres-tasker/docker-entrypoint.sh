238a44de7718ab010f830f0ae05107a79d6dc1da
James Brown <james.d.brown@noreply.gov>
2023-01-18 17:16:20 +0000

    Works around the lack of command substitution in gradle application scripts for gradle 7.6.
    Refs #104526

--- wres-tasker/docker-entrypoint.sh
+++ wres-tasker/docker-entrypoint.sh
@@ -2,25 +2,15 @@
 
 set -e
 
-# In order for worker-created files to be deleted by tasker, need group write,
-# and in order for the java process to successfully set group write permissions,
-# need more lenient umask.
-umask 0002
-
-set -e
-
 # For gradle >= v6.9.2, the application start script will not perform command substitution or resolve environment
 # variables at container runtime, so that must be done here.
 CON_HOSTNAME=`hostname`
-JFR_FILENAME=`date +%Y%m%dT%H%M%S.%NZ --utc`.jfr
 
 # Replace the environment variables with the shell variables of the same name
 MODIFIED_OPTS=$(echo $JAVA_OPTS | sed 's/$CON_HOSTNAME/'"$CON_HOSTNAME"'/g' | sed 's/$JFR_FILENAME/'"$JFR_FILENAME"'/g')
-MODIFIED_INNER_OPTS=$(echo $INNER_JAVA_OPTS | sed 's/$CON_HOSTNAME/'"$CON_HOSTNAME"'/g' | sed 's/$JFR_FILENAME/'"$JFR_FILENAME"'/g')
 
 # Export them
 export JAVA_OPTS=$MODIFIED_OPTS
-export INNER_JAVA_OPTS=$MODIFIED_INNER_OPTS
 
 # Trap and proxy signals
 trap stop TERM INT
@@ -28,8 +18,8 @@ trap stop TERM INT
 start() {
     # Run the client in the background and await it so that this script traps signals properly
     # https://github.com/moby/moby/issues/33319#issuecomment-457914349
-    echo "Starting the wres-worker client..."    
-    ./bin/wres-worker /usr/bin/wres &
+    echo "Starting the wres-tasker..."    
+    ./bin/wres-tasker &
     wait $!
 }
 
@@ -39,7 +29,7 @@ stopped="false"
 stop() {
     if [ "$stopped" = "false" ]
     then
-        echo "Stopping the wres-worker client. Bye bye..."
+        echo "Stopping the wres-tasker. Bye bye..."
         stopped="true"
         exit 0
     fi
