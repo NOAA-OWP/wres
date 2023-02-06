893cfd65d6f4df767978bd600576faa9db197db4
James Brown <james.d.brown@noreply.gov>
2023-02-06 19:19:18 +0000

    Improves some documentation.
    Refs #111987

---
+++ wres-statistics/src/main/java/wres/statistics/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p>This package provides a canonical representation of statistics for use by other WRES packages and downstream 
+ * applications and services. The Java sources are generated from Protocol Buffers (aka protobufs) whose schema is 
+ * contained in .proto files within this package. The POJOs generated from these bindings should be considered 
+ * "dumb data"; that is, objects without behavior. Indeed, the wres-datamodel wraps these objects and adds behavior.
+ * 
+ * <p>For more information on Protocol Buffers, see: https://protobuf.dev/
+ * 
+ * @author James Brown
+ */
+
+package wres.statistics;
