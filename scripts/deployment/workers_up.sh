fc02868f3ece0d0c037f0fce1a2a79ccc15c17d7
Hank Herr <hank.herr@noreply.gov>
2023-06-16 12:27:48 +0000

    Modified scripts separate image pull from up; refs #117346

--- scripts/deployment/workers_up.sh
+++ scripts/deployment/workers_up.sh
@@ -10,15 +10,45 @@ if [ ! -f "$1" ]; then
     exit 1
 fi
 
-echo "Running the command..."
-echo docker run -e HOST_NAME=$(hostname) -d -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -v "$HOME/.docker/config.json:/root/.docker/config.json" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 up --scale worker=3 --scale eventsbroker=1 --scale graphics=3 
+echo "Creating the image pull container..."
+echo Running ... docker create --rm -e HOST_NAME=$(hostname) -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 pull. 
+container_id=$(docker create --rm -e HOST_NAME=$(hostname) -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 pull)
+
+if [ $? != 0 ]
+then
+    echo "Docker command to create pull container failed.  See logging above."
+    exit 1
+fi
+
+echo "The container id returned for the pull container is $container_id"
+echo "Running ... docker cp ~/.docker $container_id:/root/."
+docker cp ~/.docker $container_id:/root/.
+
+if [ $? != 0 ]
+then
+    echo "Docker command to copy docker registry credentials into container failed.  See logging above."
+    exit 1
+fi
+
+echo "Start the container $container_id to pull images. Observe the logging to see if problems occur."
+echo "Running ... docker container start -a $container_id"
+docker container start -a $container_id
+
+if [ $? != 0 ]
+then
+    echo "Docker command to start the pull image container failed.  See logging above."
+    exit 1
+fi
+
+echo "Bringing up the service using pulled images."
+echo "If any images failed to pull or cannot be found locally, this command will fail."
+echo Running ... docker run -e HOST_NAME=$(hostname) -d -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -v "$HOME/.docker/config.json:/root/.docker/config.json" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 up --scale worker=2 --scale eventsbroker=1 --scale graphics=2
 echo ""
 echo "============== COMMAND RUNNING ========================"
-docker run -e HOST_NAME=$(hostname) -d -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -v "$HOME/.docker/config.json:/root/.docker/config.json" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 up --scale worker=3 --scale eventsbroker=1 --scale graphics=3
+docker run -e HOST_NAME=$(hostname) -d -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" --cap-drop ALL --cpus 2 --memory 512M docker/compose:1.29.2 --file $1 up --scale worker=3 --scale eventsbroker=1 --scale graphics=3
 echo "======================================================="
 echo ""
 
 echo "To view the up status, run 'docker logs [number]' where the [number] is what is displayed above as the return for the executed command."
 echo "When complete, check for the expected containers using 'docker container ls'."
-echo ""
 
