54dc0d28cc1f02158c33b644b781cf322d54c4c4
Arvin Esmailzadeh <UmbraBegins@noreply.gov>
2023-06-22 22:50:41 -0400

    refs #88796 Made it a get Method endpoint

--- wres-tasker/src/wres/tasker/WresJob.java
+++ wres-tasker/src/wres/tasker/WresJob.java
@@ -273,6 +273,22 @@ public class WresJob
         return "Up";
     }
 
+    @GET
+    @Path( "/traffic" )
+    @Produces( "text/html; charset=utf-8" )
+    public Response getEvaluationInQueue()
+    {
+        int inQueueCount = JOB_RESULTS.getJobStatusCount( JobMetadata.JobState.IN_QUEUE );
+        int inProgressCount = JOB_RESULTS.getJobStatusCount( JobMetadata.JobState.IN_PROGRESS );
+
+        String htmlResponse = "<html><body><h1>Evaluations in Queue and In Progress</h1>"
+                              + "<p>IN_QUEUE Count: " + inQueueCount + "</p>"
+                              + "<p>IN_PROGRESS Count: " + inProgressCount + "</p>"
+                              + "</body></html>";
+
+        return Response.ok( htmlResponse ).build();
+    }
+
     /**
      * Post a declaration to start a new WRES job.
      * @param projectConfig The declaration to use in a new job.
@@ -604,18 +620,6 @@ public class WresJob
     @Produces( "text/html; charset=utf-8" )
     public Response getWresJobInfo( @PathParam( "jobId" ) String jobId )
     {
-        if ( jobId.equalsIgnoreCase( "traffic" ) )
-        {
-            int inQueueCount = JOB_RESULTS.getJobStatusCount( JobMetadata.JobState.IN_QUEUE );
-            int inProgressCount = JOB_RESULTS.getJobStatusCount( JobMetadata.JobState.IN_PROGRESS );
-
-            String htmlResponse = "<html><body><h1>Evaluations in Queue and In Progress</h1>"
-                                  + "<p>IN_QUEUE Count: " + inQueueCount + "</p>"
-                                  + "<p>IN_PROGRESS Count: " + inProgressCount + "</p>"
-                                  + "</body></html>";
-
-            return Response.ok( htmlResponse ).build();
-        }
         Integer jobResult = JOB_RESULTS.getJobResultRaw( jobId );
         if ( Objects.isNull( jobResult ) )
         {
