1e31533cab962d08d1aee4ccb1dc964afe6228d2
Evan Pagryzinski <evan.pagryzinski@noreply.gov>
2023-07-14 16:18:04 -0400

    Added new migrate_dbs_v2 script. refs 117531
    New migrate function is able to take in a specific database to migrate.
    Will replace the v1 function once testing is complete

---
+++ scripts/redmine_66113/wres_http_migrate_dbs_V2.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# This bash program is intended for use by admins of a WRES cluster.
+# Migrates the specified (or default) database host
+# Intended to be run from a directory one level deeper than wres/scripts.
+
+environment=$1
+wres_ca_file=${2:-../cacerts/dod_root_ca_3_expires_2029-12.pem}
+
+if [ -f $wres_ca_file ]
+then
+    echo "Found the WRES CA file at $wres_ca_file"
+else
+    echo "Did not find the WRES CA file at $wres_ca_file"
+    exit 1
+fi
+
+if [ $# -eq 2 ]
+then
+    host=$2
+    echo "We are using the $environment environment in this program and migrating the $host database."
+    read -n1 -r -p "Please ctrl-c if that is not correct, any key otherwise..." key
+    post_result=$( curl -i -s --cacert $wres_ca_file --data "additionalArguments=${host}additionalArguments=5432additionalArguments=wres8projectConfig=noProjectConfigOnlyMigratio&verb=migratedatabase" https://${environment}/job | tr -d '\r' )
+else
+    echo "We are using the $environment environment in this program and migrating the current database."
+    read -n1 -r -p "Please ctrl-c if that is not correct, any key otherwise..." key
+    post_result=$( curl -i -s --cacert $wres_ca_file --data "projectConfig=noProjectConfigOnlyMigratio&verb=migratedatabase" https://${environment}/job | tr -d '\r' )
+fi
+
+post_result_http_code=$( echo -n "$post_result" | grep HTTP | tail -n 1 | cut -d' ' -f2 )
+echo "The last status code in the response was $post_result_http_code"
+
+if [ "$post_result_http_code" == "201" ] || [ "$post_result_http_code" == "200" ]
+then
+    echo "The response code was successful: $post_result_http_code"
+else
+    echo "The response code was NOT 201 nor 200, failed to create, exiting..."
+    exit 2
+fi
+
+job_location=$( echo -n "$post_result" | grep Location | cut -d' ' -f2 )
+echo "The location of the resource created by server was $job_location"
+
+evaluation_status=""
+
+while [ "$evaluation_status" != "COMPLETED_REPORTED_SUCCESS" ] \
+    && [ "$evaluation_status" != "COMPLETED_REPORTED_FAILURE" ]
+do
+    sleep 0.5
+    evaluation_status=$( curl --cacert $wres_ca_file $job_location/status -s | tr -d '\r' )
+done
+
+
+if [ "$evaluation_status" == "COMPLETED_REPORTED_SUCCESS" ]
+then
+    echo "Clean succeeded for $job_location"
+else
+    echo "CLEAN FAILED FOR $job_location"
+    exit 3
+fi
+
+exit 0
