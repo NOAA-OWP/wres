57a46a30a2f18a4df853fc1907fbd08634e51188
Evan Pagryzinski <evan.pagryzinski@noreply.gov>
2023-10-10 11:06:09 -0400

    Helper scripts I was using for deploys
    refs: #120666
    
    Created some helper scripts I was using to deploy. Added some rough
    instructions for use as well. Pushing these incase I need to do deploys
    in the future or to possibly help others needing to do the same thing on
    windows

---
+++ scripts/localWindowsDeployScripts/windowsVersions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# CHECK windowsDockerize FOR USE INSTRUCTIONS
+
+function get_ver
+{
+    last_commit_hash=$( git log --format="%h" -n 1 -- build.gradle gradle $@ )
+    last_commit_date=$( git log --date=iso8601 --format="%cd" -n 1 -- build.gradle gradle $@ )
+    last_commit_date_utc=$( date --date "${last_commit_date}" --iso-8601 --utc )
+    last_commit_date_short=$( echo ${last_commit_date_utc} | sed 's/\-//g' - )
+    potential_version=${last_commit_date_short}-${last_commit_hash::-2}
+
+    # Look for the git status of the dockerfile for the directory passed
+    dockerfile_modified=$( git status --porcelain -- $1/Dockerfile | grep "^ M" )
+
+    if [ "$dockerfile_modified" != "" ]
+    then
+        echo ${potential_version}-dev
+    else
+	echo ${potential_version}
+    fi
+}
+
+echo Main version: $( get_ver . )
+
+# To use extended globbing in the for loop that follows, extglob.
+shopt -s extglob
+
+for directory in wres-*([a-z])
+do
+    # For those with zips, we want to change versions when dependencies change.
+    if [[ "$directory" == "wres-tasker" ]]
+    then
+        version=$( get_ver ${directory} "wres-messages" "wres-config" )
+    elif [[ "$directory" == "wres-worker" ]]
+    then
+        version=$( get_ver ${directory} "wres-messages" )
+    elif [[ "$directory" == "wres-vis" ]]
+    then
+        version=$( get_ver ${directory} "wres-datamodel" \
+                           "wres-util" "wres-eventsbroker" "wres-events" )
+    else
+        version=$( get_ver ${directory} )
+    fi
+
+    echo "${directory} version: ${version}"
+done
