1af9e79b31ca441f72c4be4bc34291a00b2997c8
James Brown <64858662+james-d-brown@users.noreply.github.com>
2026-03-24 12:29:30 +0000

    Migrate wres-system from MockServer to WireMock, #755

--- wres-system/test/wres/system/SSLStuffThatTrustsOneCertificateTest.java
+++ wres-system/test/wres/system/SSLStuffThatTrustsOneCertificateTest.java
@@ -7,6 +7,8 @@ import java.nio.charset.StandardCharsets;
 
 import javax.net.ssl.SSLHandshakeException;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -14,29 +16,67 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuil
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.client5.http.ssl.HttpsSupport;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.TlsSocketStrategy;
 import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.ssl.TLS;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockserver.integration.ClientAndServer;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SSLStuffThatTrustsOneCertificateTest
 {
-
-    private static ClientAndServer mockServer;
+    @RegisterExtension
+    private static final WireMockExtension WIREMOCK =
+            WireMockExtension.newInstance()
+                             .options( WireMockConfiguration.wireMockConfig()
+                                                            .dynamicPort()
+                                                            .dynamicHttpsPort()
+                                                            .keystorePath( "testNonSrc/test.jks" )
+                                                            .keystorePassword( "changeme" )
+                                                            .keyManagerPassword( "changeme" ) )
+                             .build();
 
     /**
-     * From <a href="https://github.com/jamesdbloom/mockserver/blob/master/mockserver-core/src/main/resources/org/mockserver/socket/CertificateAuthorityCertificate.pem">...</a>
-     * which was mentioned in <a href="https://www.mock-server.com/">...</a> as automatically
-     * being set up for any SSL connections to the server.
+     * The x509 public server certificate presented to the client at the keystore path in the wiremock server instance
+     * above, but in base64 PEM format. This certificate is valid for 5024 days from 24 March 2026. It would be better
+     * to generate this certificate at runtime to avoid a ticking bomb. When generating an equivalent certificate,
+     * ensure that the Common Name (CN) is set to localhost, i.e., -subj 'CN=localhost' using openSSL.
      */
-    private static final String MOCK_SERVER_CA_PEM_2019 =
+
+    private static final String WIREMOCK_CERTIFICATE =
+            """
+                    -----BEGIN CERTIFICATE-----
+                    MIIDCTCCAfGgAwIBAgIUE3u7asbSgYZUyD07MefhMKqpYw4wDQYJKoZIhvcNAQEL
+                    BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI2MDMyNDEyMDYwMVoXDTM5MTIy
+                    NTEyMDYwMVowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
+                    AAOCAQ8AMIIBCgKCAQEAp//fMVf67MjjPwc8Jk9A+i5JvgjOOPiH6BVWUgYLhyCS
+                    Q1goTlHrt1GjoD2R1kSYKDd45lPXsGFAlH/BRF9teram63UwJbbi52zukhx93drZ
+                    zdFIOVpasD6TV0arO/qrL9PKs99YcJlO/z2SoMd0j+nUnMYfWNk3RTtAtMUFPSlg
+                    mIYFmTtNFfpNfGFEVuoHvbs37cEQzEpsfXUPma33CmoVGKWerMKp9aFk6fKsiGKp
+                    L1Jv+k5q1H/0YoGzdTW+/0PCUB9K5Q8wq4r7JzrRgMwQCdm0DUwZwwJAKHvnasKe
+                    n+/+PPvs/30Iu2DAGl5ZpV0yvh033FNpBZTAIfHTOwIDAQABo1MwUTAdBgNVHQ4E
+                    FgQULnHJCT6DQADwET50vZbv6EFql6EwHwYDVR0jBBgwFoAULnHJCT6DQADwET50
+                    vZbv6EFql6EwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAoJET
+                    ayoG7LHWgOPZDwdf6RHlcDLa+vv4T8QYIUkaqnoNCpAdGpVdgXVWralFduuvnR8L
+                    YH/gqi3sbg2Se0esYxlGOPwP3UkWaX31w2Yw35Vi031VCLClhLlgxAhqHUPKGXzC
+                    0EByOjMmmJM3mkAKgzp4jv0c5FTn1hE5HOm7koIsshGQRzsb+jhF+ioBuOV2zThW
+                    KolGFOZy6nUY99eEUiX+fv6EHfj5V8u3CTw7H1/2pQBUnOSMy1j8i835mfseFKiS
+                    4UDowp5TxafvdSZghgeTvH4ynWC+tT/ih4KQjYFJ7GvhO4KLrbAjeo3IXuaRB6zQ
+                    cWVO69C17BaekFoDxg==
+                    -----END CERTIFICATE-----
+                    """;
+
+    /**
+     * <p>A mockserver x509 certificate, which is not trusted by the wiremock instance. Obtained from:
+     * <a href="https://github.com/mock-server/mockserver/blob/master/mockserver-core/src/main/resources/org/mockserver/socket/CertificateAuthorityCertificate.pem">Mock server certificate.</a>
+     * <p>It would be significantly better to auto-generate a x509 certificate in code to avoid the sad path test
+     * failing with the same handshake exception, but for an unintended reason, such as certificate expiry.
+     */
+    private static final String UNTRUSTED_MOCKSERVER_CERT =
             """
                     -----BEGIN CERTIFICATE-----
                     MIIDqDCCApCgAwIBAgIEPhwe6TANBgkqhkiG9w0BAQsFADBiMRswGQYDVQQDDBJ3
@@ -62,63 +102,42 @@ class SSLStuffThatTrustsOneCertificateTest
                     -----END CERTIFICATE-----
                     """;
 
-    /**
-     * The intermediate certificate found that signed hc.apache.org certificate
-     * as of 2018-11-15, will expire 2024-07-03. In 2024 it would be wise to
-     * auto-generate a certificate here instead of using this old apache cert
-     * otherwise the test will not be testing what is intended, instead it will
-     * test that an expired certificate throws an SSLHandShakeException which
-     * should always be true. What we are looking for is that exactly one cert
-     * (intermediate or CA) is trusted, the exact one specified.
-     */
-    private static final String APACHE_INTERMEDIATE_PEM_2018 =
-            """
-                    -----BEGIN CERTIFICATE-----
-                    MIIF8jCCA9qgAwIBAgIQUWEdv45DMOu87QrYvTIjzTANBgkqhkiG9w0BAQwFADCB
-                    iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl
-                    cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV
-                    BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTQw
-                    NzA0MDAwMDAwWhcNMjQwNzAzMjM1OTU5WjBZMQswCQYDVQQGEwJVUzEQMA4GA1UE
-                    ChMHU1NMLmNvbTEUMBIGA1UECxMLd3d3LnNzbC5jb20xIjAgBgNVBAMTGVNTTC5j
-                    b20gSGlnaCBBc3N1cmFuY2UgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
-                    AoIBAQClFxgJFdeMFUw1Y4GEqcVHDiXJp7ukYtO31KKU+j/llv/DHKhRNO4CtbI0
-                    febjCO2gMzYz3cvj9zgwWiX/oaeI5HWBqN3XqULI+UA84sBKzdsD5oELyfqC6Yyy
-                    yPeXrS7SCQYblUZqmcjksbJgL2XA9nZ+iqiched+z4MWRbiU+DEklXssTPyo1f4A
-                    QT033VxgkF6lNw+Q+Rm4FVQV7FrYQYj6mxSTFXtIvepbsszU6iy7iVXPA4RNb3Iu
-                    REz8tBnce7+fwZkq+NPUlaKDUp82WvGHMwAzBULBZkd2iujWzDawFPVeYONb+P70
-                    e///GTDhDvMe4E27zXoSW6ujZGh5AgMBAAGjggGEMIIBgDAfBgNVHSMEGDAWgBRT
-                    eb9aqitKz1SA4dibwJ3ysgNmyzAdBgNVHQ4EFgQUTFQjZyl09ELqmbtoPBafCC8Q
-                    5kEwDgYDVR0PAQH/BAQDAgGGMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYDVR0lBBYw
-                    FAYIKwYBBQUHAwEGCCsGAQUFBwMCMCEGA1UdIAQaMBgwDAYKKwYBBAGCqTABATAI
-                    BgZngQwBAgIwVQYDVR0fBE4wTDBKoEigRoZEaHR0cDovL2NybC50cnVzdC1wcm92
-                    aWRlci5jb20vVVNFUlRydXN0UlNBQ2VydGlmaWNhdGlvbkF1dGhvcml0eS5jcmww
-                    gYAGCCsGAQUFBwEBBHQwcjBEBggrBgEFBQcwAoY4aHR0cDovL2NydC50cnVzdC1w
-                    cm92aWRlci5jb20vVVNFUlRydXN0UlNBQWRkVHJ1c3RDQS5jcnQwKgYIKwYBBQUH
-                    MAGGHmh0dHA6Ly9vY3NwLnRydXN0LXByb3ZpZGVyLmNvbTANBgkqhkiG9w0BAQwF
-                    AAOCAgEAKBjIAycQLrBawRYWFoRJIo4tP7ZkyEVRgd491E3beayYpTs+wLzbBOve
-                    +v6wGx4ohIOACncEDgVy4bkBgYulsix6xWeVG1ZmF9WXhhFdP+3ZipAttMSh5s7m
-                    U/5hAg5NvZ5h5VN/3EOJD8+0FOfEaUcgQFGorYXeL+/2+E/ixNWtTFl4ipZ7AU2k
-                    +lSPxnsc4+3H1+EEea9xTmanMx+gY/tXEnyQ7iVtry4uUjHF+8ts0vvAlesra8PZ
-                    5CNCSv9sqkG2Ipig2Srrw8zvrMZtJO+veymDDsc5edpjPbSY0A8XhYbhepjdid2V
-                    kisp/RtyAq+CiWea3tH9y5BK0mDoiMhlJ59E52TsuHgZ7TpzoCnEsCWVPctj4e5N
-                    uSs75oT1ap3wMHCvObincNovMaqtX8zy/oRr+NSoDQY2ZQ06viCHAKpudpUg4lLF
-                    RKqW+UTCWDML6PriZsjXf6aSce4lK+UdmmL4VDh1Nf9y7FpSsNIaZfFrATrTfS1o
-                    P8y0ZjapxUVVpiH11vIr8mqk6QT0WelbI4r4s45ZFjOeT69xSpsEIIcxYI1GpIbw
-                    CMDvdMAmwzr2pur5PIKKVH1WkLtaf1aSruBTP/aeavS2P8NMy7tI1LKoNY8XvMFO
-                    +E3jECsl+ke/Q3dfswLjNl+as1U10Ltgx7/IJanbDEQzVr6Sqgs=
-                    -----END CERTIFICATE-----
-                    """;
-
-    @BeforeAll
-    static void beforeAllTests()
-    {
-        SSLStuffThatTrustsOneCertificateTest.mockServer = ClientAndServer.startClientAndServer( 0 );
-    }
-
     @Test
     void connectToServerWithCorrectCertificateTrustedSucceeds() throws IOException
     {
-        byte[] certificateBytes = MOCK_SERVER_CA_PEM_2019.getBytes( StandardCharsets.US_ASCII );
+        byte[] certificateBytes = WIREMOCK_CERTIFICATE.getBytes( StandardCharsets.US_ASCII );
+        InputStream certificateStream = new ByteArrayInputStream( certificateBytes );
+
+        SSLStuffThatTrustsOneCertificate sslStuff =
+                new SSLStuffThatTrustsOneCertificate( certificateStream, "changeme" );
+
+        TlsSocketStrategy tlsStrategy = ClientTlsStrategyBuilder.create()
+                                                                .setSslContext( sslStuff.getSSLContext() )
+                                                                .setTlsVersions( TLS.V_1_2 )
+                                                                .setHostnameVerifier( NoopHostnameVerifier.INSTANCE )
+                                                                .buildClassic();
+
+        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+                                                                                  .setTlsSocketStrategy( tlsStrategy )
+                                                                                  .build();
+
+        Integer statusCode;
+        try ( CloseableHttpClient httpClient = HttpClients.custom()
+                                                          .setConnectionManager( cm )
+                                                          .build() )
+        {
+            ClassicHttpRequest httpGet = new HttpGet( "https://localhost:" + WIREMOCK.getHttpsPort() );
+            statusCode = httpClient.execute( httpGet, HttpResponse::getCode );
+        }
+
+        assertNotNull( statusCode );
+    }
+
+    @Test
+    void connectToServerWithIncorrectCertificateTrustedThrowsHandshakeException() throws IOException
+    {
+        // Use an untrusted certificate (Apache intermediate)
+        byte[] certificateBytes = UNTRUSTED_MOCKSERVER_CERT.getBytes( StandardCharsets.US_ASCII );
         InputStream certificateStream = new ByteArrayInputStream( certificateBytes );
 
         SSLStuffThatTrustsOneCertificate sslStuff =
@@ -134,58 +153,15 @@ class SSLStuffThatTrustsOneCertificateTest
                                                                                   .setTlsSocketStrategy( tlsStrategy )
                                                                                   .build();
 
-        Integer statusCode;
-
-        try ( CloseableHttpClient httpClient = HttpClients.custom()
-                                                          .setConnectionManager( cm )
-                                                          .build() )
-        {
-            ClassicHttpRequest httpGet = new HttpGet( "https://localhost:"
-                                                      + SSLStuffThatTrustsOneCertificateTest.mockServer.getLocalPort() );
-
-            statusCode = httpClient.execute( httpGet, HttpResponse::getCode );
-        }
-
-        assertNotNull( statusCode );
-    }
-
-    @Test
-    void connectToServerWithIncorrectCertificateTrustedThrowsHandshakeException() throws IOException
-    {
-        byte[] certificateBytes =
-                APACHE_INTERMEDIATE_PEM_2018.getBytes( StandardCharsets.US_ASCII );
-        InputStream certificateStream =
-                new ByteArrayInputStream( certificateBytes );
-
-        SSLStuffThatTrustsOneCertificate sslStuff =
-                new SSLStuffThatTrustsOneCertificate( certificateStream, null );
-
-        TlsSocketStrategy tlsStrategy =
-                ClientTlsStrategyBuilder.create()
-                                        .setSslContext( sslStuff.getSSLContext() )
-                                        .setTlsVersions( TLS.V_1_2 )
-                                        .setHostnameVerifier( HttpsSupport.getDefaultHostnameVerifier() )
-                                        .buildClassic();
-
-        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
-                                                                                  .setTlsSocketStrategy( tlsStrategy )
-                                                                                  .build();
-
         try ( CloseableHttpClient httpClient = HttpClients.custom()
                                                           .setConnectionManager( cm )
                                                           .build() )
         {
 
-            HttpGet httpGet = new HttpGet( "https://localhost:"
-                                           + SSLStuffThatTrustsOneCertificateTest.mockServer.getLocalPort() );
+            HttpGet httpGet = new HttpGet( "https://localhost:" + WIREMOCK.getHttpsPort() );
 
+            // Should fail because the trusted cert doesn't match WireMock's CA
             assertThrows( SSLHandshakeException.class, () -> httpClient.execute( httpGet, HttpResponse::getCode ) );
         }
-    }
-
-    @AfterAll
-    static void afterAllTests()
-    {
-        SSLStuffThatTrustsOneCertificateTest.mockServer.stop();
     }
 }
